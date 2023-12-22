66dff55fe48df637faa3d49b5cb8df456d50832d
Derek Ledoux <derek.ledoux@shopify.com>
2023-12-22 11:17:12 -0500

    pushing template repo

---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: './public/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [{
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    },{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+        }
+    },{
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+    },{
+      test: /\.(png|jpe?g|gif|jp2|webp)$/,
+      loader: 'file-loader',
+      options: {
+        name: 'images/[name].[ext]'
+      }
+    }
+  ],
+},
+resolve: {
+  extensions: ['.tsx', '.ts', '.js'],
+},
+devServer: {
+  historyApiFallback: true,
+},
+};
