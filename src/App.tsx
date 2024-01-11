9f208e314a4d5c70a9ab1ef4880b68c1748203c7
bcostas <bcostas@torontomu.ca>
2024-01-11 01:58:51 -0500

    Completed all functional requirements and added CSS styling to the stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Stopwatch from './StopWatch';
+import './App.css'
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (
+    <div className="justify-center">
+        <div className="justify-center">
+        <Stopwatch />
+        </div>
+    </div>
+    
+  );
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleStart = () => {
+    if (isRunning) return;
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  const resetStopwatch = () => {
+    setIsRunning(false);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    setTime(0);
+  };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  return (
+    <>
+    <div className="box">
+      <h1>Stopwatch</h1>
+      <div className="time">{formatTime(time)}</div>
+    </div>
+    <div style={{display: 'flex', justifyContent:'center', alignItems:'center', paddingTop: '2em'}}>
+        <StopWatchButton onClick={handleStart} label="Start" />
+        <StopWatchButton onClick={handlePause} label="Pause" />
+        <StopWatchButton onClick={resetStopwatch} label="Reset" />
+    </div>
+    </>
+  );
 }
