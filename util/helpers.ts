3669dd33e468d44ddc9c578a436ccc49eb488ee6
topanb11 <topanbudiman11@gmail.com>
2024-01-11 16:26:42 -0700

    refactored to display ms

--- util/helpers.ts
+++ util/helpers.ts
@@ -1,11 +1,11 @@
 export const formatTime = (time: number) => {
-	const hours = Math.floor(time / 3600);
-	const minutes = Math.floor((time % 3600) / 60);
-	const seconds = time % 60;
+	const minutes = Math.floor(time / 60000);
+ 	const seconds = Math.floor((time % 60000) / 1000);
+	const milliseconds = Math.floor((time % 1000) / 10)
 
-	const formattedHours = hours.toString().padStart(2, '0')
 	const formattedMinutes = minutes.toString().padStart(2, '0')
 	const formattedSeconds = seconds.toString().padStart(2, '0')
+	const formattedMilliseconds = milliseconds.toString().padStart(2, '0')
 
-	return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+	return `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
 };
