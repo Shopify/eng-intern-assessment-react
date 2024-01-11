d614e5eed7d87922a8698c05b7ee15d81b48188d
williamckha <williamha@outlook.com>
2024-01-11 11:24:03 -0800

    Get tests working

--- __tests__/Stopwatch.test.js
+++ __tests__/Stopwatch.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Stopwatch from '../src/StopWatch';
+import '@testing-library/jest-dom'
 
 describe('Stopwatch', () => {
   test('renders initial state correctly', () => {
