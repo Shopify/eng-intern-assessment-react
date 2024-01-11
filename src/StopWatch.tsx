24460bf1c9d398f9a5a0aa50864860a6458df636
Saif Karnawi <saifstevens@gmail.com>
2024-01-11 09:59:06 -0800

    Implemented most of the front-end

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import './utils/styles.css'
+import StopWatchButton from './StopWatchButton'
+
+interface TimersProps {
+    mainTimer: string;
+    lapTimer: string;
+}
+
+const Timers = ({ mainTimer, lapTimer }: TimersProps) => {
+    return (
+      <div className="timers-container">
+        <div className="main-timer">{mainTimer}</div>
+        <div className="lap-timer">{lapTimer}</div>
+      </div>
+    );
+};
 
 export default function StopWatch() {
     return(
-        <div></div>
+        <div className="main">
+            <div className="thin-gray-circle">
+            <Timers mainTimer="00:00:00" lapTimer="00:00:00" />
+            </div>
+            <StopWatchButton/>
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faUndo, faTrash} from '@fortawesome/free-solid-svg-icons';
+import './utils/styles.css'
 
 export default function StopWatchButton() {
     return(
-        <div></div>
+        <div className="button-group">
+            <FontAwesomeIcon icon={faPlay} className="icon" />
+            <FontAwesomeIcon icon={faPause} className="icon" />
+            <FontAwesomeIcon icon={faUndo} className="icon" />
+            <FontAwesomeIcon icon={faTrash} className="reset-icon" />
+        </div>
     )
 }
