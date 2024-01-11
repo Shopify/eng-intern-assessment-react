bbf77193cbacfd77dfb3f88eefd65bdb7d611c25
Saif Karnawi <saifstevens@gmail.com>
2024-01-11 11:07:22 -0800

    Implemented play/pause functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './utils/styles.css'
-import StopWatchButton from './StopWatchButton'
+import React, { useState, useEffect } from 'react';
+import StopWatchButton from './StopWatchButton';
+import './utils/styles.css';
 
 interface TimersProps {
-    mainTimer: string;
-    lapTimer: string;
+  mainTimer: string;
+  lapTimer: string;
 }
 
 const Timers = ({ mainTimer, lapTimer }: TimersProps) => {
@@ -14,15 +14,59 @@ const Timers = ({ mainTimer, lapTimer }: TimersProps) => {
         <div className="lap-timer">{lapTimer}</div>
       </div>
     );
+  };
+  
+  const formatTime = (milliseconds: number) => {
+    const seconds = Math.floor(milliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+  
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes % 60).padStart(2, '0');
+    const formattedSeconds = String(seconds % 60).padStart(2, '0');
+    const formattedMilliseconds = String(Math.floor((milliseconds % 1000) / 10)).padStart(2, '0');
+  
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
+  };
+  
+  const StopWatch = () => {
+    const [mainTimer, setMainTimer] = useState(0);
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  
+    const startTimer = () => {
+      const newIntervalId = setInterval(() => {
+        setMainTimer((currTime) => currTime + 10);
+      }, 10);
+      setIntervalId(newIntervalId);
+    };
+  
+    const stopTimer = () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+        setIntervalId(null);
+      }
+    };
+  
+    const handlePlayOrPauseClick = () => {
+      if (intervalId) {
+        stopTimer();
+      } else {
+        startTimer();
+      }
+    };
+  
+    useEffect(() => {
+      return () => stopTimer();
+    }, []);
+
+  return (
+    <div className="main">
+      <div className="thin-gray-circle">
+        <Timers mainTimer={formatTime(mainTimer)} lapTimer="00:00:00" />
+      </div>
+      <StopWatchButton onToggleRun={handlePlayOrPauseClick} />
+    </div>
+  );
 };
 
-export default function StopWatch() {
-    return(
-        <div className="main">
-            <div className="thin-gray-circle">
-            <Timers mainTimer="00:00:00" lapTimer="00:00:00" />
-            </div>
-            <StopWatchButton/>
-        </div>
-    )
-}
+export default StopWatch;
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faUndo, faTrash} from '@fortawesome/free-solid-svg-icons';
 import './utils/styles.css'
 
-export default function StopWatchButton() {
-    return(
-        <div className="button-group">
-            <FontAwesomeIcon icon={faPlay} className="icon" />
-            <FontAwesomeIcon icon={faPause} className="icon" />
-            <FontAwesomeIcon icon={faUndo} className="icon" />
-            <FontAwesomeIcon icon={faTrash} className="reset-icon" />
-        </div>
-    )
+interface StopWatchButtonProps {
+    onToggleRun: () => void;
 }
+
+const StopWatchButton = ({onToggleRun} : StopWatchButtonProps) => {
+    const [isRunning, setIsRunning] = useState(false);
+  
+
+    const handlePlayOrPauseClick = () => {
+        setIsRunning(!isRunning);
+        onToggleRun();
+    };
+
+    const handleLapCick = () => {
+        setIsRunning((prevIsRunning) => !prevIsRunning);
+    };
+
+    const handleTrashClick = () => {
+        setIsRunning((prevIsRunning) => !prevIsRunning);
+    };
+
+    return (
+        <div className="button-group">
+        <FontAwesomeIcon icon={isRunning ? faPause : faPlay} className="icon" onClick={handlePlayOrPauseClick} />
+        <FontAwesomeIcon icon={faUndo} className="icon" onClick={handleLapCick} />
+        <FontAwesomeIcon icon={faTrash} className="reset-icon" onClick={handleTrashClick} />
+        </div>
+    );
+};
+
+export default StopWatchButton;
