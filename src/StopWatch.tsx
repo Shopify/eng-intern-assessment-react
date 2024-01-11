159f7b352fa969d5c205378de3b9926d5a7e138e
topanb11 <topanbudiman11@gmail.com>
2024-01-11 15:14:18 -0700

    add stopwatch display

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import '../util/styles.css'
 
-export default function StopWatch() {
+interface StopWatchProps {
+	time: number
+}
+
+const StopWatch: React.FC<StopWatchProps> = ({
+	time,
+}) => {
+
+	const formatTime = (time: number) => {
+		const hours = Math.floor(time / 3600);
+		const minutes = Math.floor((time % 3600) / 60);
+		const seconds = time % 60;
+
+		const formattedHours = hours.toString().padStart(2, '0')
+		const formattedMinutes = minutes.toString().padStart(2, '0')
+		const formattedSeconds = seconds.toString().padStart(2, '0')
+
+		return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+	};
+
     return(
-        <div></div>
+        <h2 className='stopWatchDisplay'>
+			{formatTime(time)}
+		</h2>
     )
 }
+
+export default StopWatch
