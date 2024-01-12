3ca0fd8bf1bd8db903e15d35c8be810c64cdba9f
Teo Cristante <teo.cristante@gmail.com>
2024-01-12 11:23:25 -0500

    Added buttons and functionality for Stop, Reset and Lap

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -3,13 +3,27 @@ import StopWatchButton from './StopWatchButton'
 
 export default function StopWatch() {
     
+    // Variables for the time
     let sec = 0
     let min = 0
     let hour = 0
 
+    // String variables for the time
     let sec_str: string
     let min_str: string
     let hour_str: string
+
+    // Variables for the lap time
+    let sec_lap = 0
+    let min_lap = 0
+    let hour_lap = 0
+
+    // String variables for the lap time
+    let sec_lap_str: string
+    let min_lap_str: string
+    let hour_lap_str: string
+
+    // Variables for the timer
     let time: any
     let timer: any
 
@@ -33,11 +47,17 @@ export default function StopWatch() {
                 hour++
             }
 
-    
-            hour_str = hour.toString()
-            min_str = min.toString()
-            sec_str = sec.toString()
-        
+            // Increments the lap time
+            sec_lap++
+            if(sec_lap == 60) {
+                sec_lap = 0
+                min_lap++
+            }
+            if(min_lap == 60) {
+                min_lap = 0
+                hour_lap++
+            }
+            
             // Adds a 0 to the front of the number if it is less than 10
             if (sec <= 9) {
                 sec_str = "0" + sec
@@ -55,12 +75,74 @@ export default function StopWatch() {
         }, 1000)
     }
 
+    // Stops the timer
+    function stopTime() {
+        clearInterval(timer)
+    }
+
+    // Resets the timer
+    function resetTime() {
+        // Stops the timer
+        clearInterval(timer)
+
+        // Resets the time
+        sec = 0
+        min = 0
+        hour = 0
+        
+        //Displays the reseted time
+        time = document.getElementById("time")
+        time.innerHTML = "00:00:00"
+
+        // Removes all the laps
+        let lapList = document.getElementById("lapList")
+        while (lapList.firstChild) {
+            lapList.removeChild(lapList.firstChild)
+        }
+    }
+    
+    // Displays the lap time
+    function lapTime() {
+        // Creates list item
+        let lapList = document.getElementById("lapList")
+        let lap = document.createElement("li")
+        
+        // Adds a 0 to the front of the number if it is less than 10
+        if (sec_lap <= 9) {
+            sec_lap_str = "0" + sec_lap
+        }
+        if (min_lap <= 9) {
+            min_lap_str = "0" + min_lap
+        }
+        if (hour_lap <= 9) {
+            hour_lap_str = "0" + hour_lap
+        }
+        
+        // Displays the lap time
+        let currentLap = hour_lap_str + ":" + min_lap_str + ":" + sec_lap_str
+        let currentTime = hour_str + ":" + min_str + ":" + sec_str
+        lap.innerHTML = "Lap: " + currentLap + "   " + "Total: " + currentTime
+        
+        // Resets the lap time
+        hour_lap = 0
+        min_lap = 0
+        sec_lap = 0
+        
+        // Adds the lap to the list
+        lapList.appendChild(lap)
+        
+    }
+
 
     return(
-        <div id="StopWatch">
+        <div>
             <h2>StopWatch</h2>
             <h3 id="time">00:00:00</h3>
             <StopWatchButton name="Start" stopWatchHandler={startTime} />
+            <StopWatchButton name="Stop" stopWatchHandler={stopTime} />
+            <StopWatchButton name="Reset" stopWatchHandler={resetTime} />
+            <StopWatchButton name="Lap" stopWatchHandler={lapTime} />
+            <ol id="lapList"></ol>
         </div>
     )
 }
