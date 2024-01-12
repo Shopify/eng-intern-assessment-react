0a87ccf6f51883314df4161a9ccaf535b378f18b
Sahl Bakshi <saa2baks@uwaterloo.ca>
2024-01-12 14:29:31 -0500

    added lap functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,65 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import StopWatch from './StopWatch'
 import StopWatchButton from './StopWatchButton'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import formatTime from './utils/formatTime'
+import LapTimes from './LapTimes'
 import "./styles.css"
 
 export default function App() {
     const [time, setTime] = useState<number>(0)
     const [stopWatchOn, setStopWatchOn] = useState<boolean>(false)
+    const [lapTimes, setLapTimes] = useState<string[]>([])
 
     useEffect(() => {
         let interval: NodeJS.Timeout
     
+        const updateTimeInterval = () => {
+            setTime((prevTime) => prevTime + 10) // updates time by adding 10ms every 10ms
+        }
+
+        // If the stopwatch is on update time otherwise clear interval
         if (stopWatchOn) {
-            interval = setInterval(() => {
-                setTime((prevTime) => prevTime + 10)
-            }, 10) // adds 10 ms to time every 10 ms
-        } else {
-          clearInterval(interval);
+            interval = setInterval(updateTimeInterval, 10)
         }
         
         return () => clearInterval(interval)
-
     }, [stopWatchOn])
 
+    const handleStopButtonClick = () => {
+        setStopWatchOn(false)
+    }
+
+    const handleStartButtonClick = () => {
+        setStopWatchOn(true)
+    }
+
+    const handleRestartButtonClick = () => {
+        if (stopWatchOn) {
+            setStopWatchOn(false)
+        }
+        setLapTimes([])
+        setTime(0)
+    }
+
+    const handleLapButtonClick = () => {
+        setLapTimes((prevLapTimes) => [...prevLapTimes, formatTime(time)])
+    }
+
     return(
-        <div>
+        <div className='container'>
             <StopWatch time={time}/>
             <div className='buttons'>
+                {/* If stopwatch is on display stop button else the start button */}
                 {stopWatchOn? (
-                    <StopWatchButton label={"STOP"} color={"red"} onButtonClick={() => setStopWatchOn(false)}/>
+                    <StopWatchButton label={"STOP"} color={"red"} onButtonClick={handleStopButtonClick}/>
                 ) : (
-                    <StopWatchButton label={"START"} color={"green"} onButtonClick={() => setStopWatchOn(true)}/>
+                    <StopWatchButton label={"START"} color={"green"} onButtonClick={handleStartButtonClick}/>
                 )}
-                <StopWatchButton label={"RESTART"} color={"blue"} onButtonClick={() => setTime(0)}/>
+                <StopWatchButton label={"LAP"} color={"orange"} onButtonClick={handleLapButtonClick}/>
+                <StopWatchButton label={"RESTART"} color={"blue"} onButtonClick={handleRestartButtonClick}/>
             </div>
+            <LapTimes lapTimes={lapTimes}></LapTimes>
         </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import "./styles.css"
+import formatTime from './utils/formatTime'
 
 interface StopwatchProps {
     time: number
-}
-
-const formatTime = (time: number): string => {
-    const padWithZero = (value: number): string => value.toString().padStart(2, '0') // pads so that all units of time are 2 digits
-    const milliseconds = padWithZero(Math.floor((time % 1000) / 10))
-    const seconds = padWithZero(Math.floor((time / 1000) % 60))
-    const minutes = padWithZero(Math.floor((time / (1000 * 60)) % 60))
-
-    return `${minutes} : ${seconds} : ${milliseconds}`
 }
 
 export default function StopWatch({ time } : StopwatchProps) {
