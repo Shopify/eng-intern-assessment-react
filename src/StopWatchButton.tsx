11d102c88ab01162907035bb785896a0b3be7564
Bello Risikat <oyinkansolashoroye@gmail.com>
2024-01-12 03:56:44 +0100

    Added buttons to control Stopwatch in StopWatchButton component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,47 +1,28 @@
-import React, { useState, useRef } from 'react';
-import StopWatch from './StopWatch';
-import StopWatchButton from './StopWatchButton';
+import React from 'react';
 
+// Props interface for StopWatchButton component
+interface StopWatchButtonProps {
+    isRunning: boolean;
+    onStartStopClick: () => void;
+    onLapClick: () => void;
+    onResetClick: () => void;
+}
 
-export default function StopWatchButton() {
-    const intervalIdRef = useRef<number | null>(null);
-
-    const [isRunning, setIsRunning] = useState(false);
-    const [time, setTime] = useState(0);
-    const [laps, setLaps] = useState<number[]>([]);
-
-    const handleStartStopClick = () => {
-        setIsRunning((prevIsRunning) => !prevIsRunning);
-
-        if (!isRunning) {
-            intervalIdRef.current = window.setInterval(() => {
-                setTime((prevTime) => prevTime + 10);
-            }, 10);
-        } else {
-            window.clearInterval(intervalIdRef.current);
-        }
-    };
-
-    const handleLapClick = () => {
-        setLaps((prevLaps) => [...prevLaps, time]);
-    };
-
-    const handleResetClick = () => {
-        window.clearInterval(intervalIdRef.current);
-        intervalIdRef.current = null;
-        setIsRunning(false);
-        setTime(0);
-        setLaps([]);
-    };
+// StopWatchButton component to render buttons for controlling the stopwatch
+const StopWatchButton: React.FC<StopWatchButtonProps> = ({
+    isRunning,
+    onStartStopClick,
+    onLapClick,
+    onResetClick,
+}) => {
     return (
         <div>
-            <StopWatch time={time} laps={laps} />
-            <StopWatchButton
-                isRunning={isRunning}
-                onStartStopClick={handleStartStopClick}
-                onLapClick={handleLapClick}
-                onResetClick={handleResetClick}
-            />
+            {/* Render buttons for start/stop, lap, and reset */}
+            <button onClick={onStartStopClick}>{isRunning ? 'Stop' : 'Start'}</button>
+            <button onClick={onLapClick}>Lap</button>
+            <button onClick={onResetClick}>Reset</button>
         </div>
-    )
-}
+    );
+};
+
+export default StopWatchButton;
