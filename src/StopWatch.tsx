c5dbc6a20c75935c5234f6abd10f2957c83069d9
ashychiu <ashleychiu.codes@gmail.com>
2024-01-12 09:36:18 -0800

    Setup StopWatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [buttonText, setButtonText] = useState<string>("Start");
+
+  const startWatch = () => {
+    const newButtonText =
+      buttonText === "Start" || buttonText === "Resume" ? "Pause" : "Resume";
+    setButtonText(newButtonText);
+  };
+  const resetWatch = () => {
+    setButtonText("Start");
+  };
+  const showLap = () => {
+    alert("show lap");
+  };
+  return (
+    <>
+      <div>StopWatch</div>
+      <StopWatchButton buttonText={buttonText} onClickHandler={startWatch} />
+      <StopWatchButton buttonText="Reset" onClickHandler={resetWatch} />
+      <StopWatchButton buttonText="Lap" onClickHandler={showLap} />
+    </>
+  );
 }
