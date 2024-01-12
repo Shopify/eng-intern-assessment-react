1c2b7a63e0322ef882250ef8bcd32def71a0e1c2
Bello Risikat <oyinkansolashoroye@gmail.com>
2024-01-12 03:11:28 +0100

    Implemented basic Stopwatch functionality in App component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import StopWatch from './StopWatch';
 import StopWatchButton from './StopWatchButton';
 
 
 export default function StopWatchButton() {
+    const intervalIdRef = useRef<number | null>(null);
+
+    const [isRunning, setIsRunning] = useState(false);
+    const [time, setTime] = useState(0);
+    const [laps, setLaps] = useState<number[]>([]);
+
+    const handleStartStopClick = () => {
+        setIsRunning((prevIsRunning) => !prevIsRunning);
+
+        if (!isRunning) {
+            intervalIdRef.current = window.setInterval(() => {
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else {
+            window.clearInterval(intervalIdRef.current);
+        }
+    };
+
+    const handleLapClick = () => {
+        setLaps((prevLaps) => [...prevLaps, time]);
+    };
+
+    const handleResetClick = () => {
+        window.clearInterval(intervalIdRef.current);
+        intervalIdRef.current = null;
+        setIsRunning(false);
+        setTime(0);
+        setLaps([]);
+    };
     return (
         <div>
-            <StopWatch time={0} laps={[]} />
-            <StopWatchButton isRunning={false} onStartStopClick={() => { }} onLapClick={() => { }} onResetClick={() => { }} />
+            <StopWatch time={time} laps={laps} />
+            <StopWatchButton
+                isRunning={isRunning}
+                onStartStopClick={handleStartStopClick}
+                onLapClick={handleLapClick}
+                onResetClick={handleResetClick}
+            />
         </div>
     )
 }
