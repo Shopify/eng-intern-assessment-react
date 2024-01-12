46a0c11517cb1760d8686aae77acde12fc0a3442
Bello Risikat <oyinkansolashoroye@gmail.com>
2024-01-12 02:58:08 +0100

    Created basic structure for Stopwatch and StopwatchButton components

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import StopWatch from './StopWatch';
+import StopWatchButton from './StopWatchButton';
+
 
 export default function StopWatchButton() {
-    return(
-        <div></div>
+    return (
+        <div>
+            <StopWatch time={0} laps={[]} />
+            <StopWatchButton isRunning={false} onStartStopClick={() => { }} onLapClick={() => { }} onResetClick={() => { }} />
+        </div>
     )
 }
