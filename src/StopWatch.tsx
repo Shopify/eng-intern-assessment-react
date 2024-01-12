3c3f8f5bb95aebbd67a2251ee5469b0bf34f62ef
Moe N <moenumasawa@gmail.com>
2024-01-12 16:38:26 -0700

    Create UI for stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import "./StopWatch.css"
 
-export default function StopWatch() {
+const StopWatch: React.FC = ({}) => {
     return(
-        <div></div>
-    )
-}
+        <div className="stopwatch-container">
+            <div className="stopwatch-item">
+                <span className="number">12</span>
+                <span className="label">hrs</span>
+            </div>
+            <div className="stopwatch-item">
+                <span className="number">34</span>
+                <span className="label">min</span>
+            </div>
+            <div className="stopwatch-item">
+                <span className="number">56</span>
+                <span className="label">sec</span>
+            </div>
+        </div>
+    );
+};
+
+export default StopWatch;
