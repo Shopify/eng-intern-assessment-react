033deee1026ca2063f2b169d9308c2ce87a85f02
ashychiu <ashleychiu.codes@gmail.com>
2024-01-12 10:41:10 -0800

    Add useEffect to run timer

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
   const [buttonText, setButtonText] = useState<string>("Start");
 
   const startWatch = () => {
     const newButtonText =
       buttonText === "Start" || buttonText === "Resume" ? "Pause" : "Resume";
     setButtonText(newButtonText);
+    setIsRunning(!isRunning);
   };
   const resetWatch = () => {
     setButtonText("Start");
+    setTime(0);
+    setIsRunning(false);
   };
   const showLap = () => {
     alert("show lap");
   };
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
   return (
     <>
       <div>StopWatch</div>
+      {time}
       <StopWatchButton buttonText={buttonText} onClickHandler={startWatch} />
       <StopWatchButton buttonText="Reset" onClickHandler={resetWatch} />
       <StopWatchButton buttonText="Lap" onClickHandler={showLap} />
