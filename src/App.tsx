d10b4ccca875cd79f70e5849a9dd5bbca5236cad
Andrew Chen <andrew.chen.anyuan@gmail.com>
2024-01-12 02:14:52 -0500

    feat/stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import {
+  BlockStack,
+  Button,
+  Card,
+  Icon,
+  InlineStack,
+  Page,
+  Text,
+} from "@shopify/polaris";
+import React from "react";
+import StopWatch from "./StopWatch";
+import { ClockMajor } from "@shopify/polaris-icons";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (
+    <Page>
+      <BlockStack gap="300">
+        <Text as="h1" variant="headingXl">
+          Andrew's Stopwatch
+        </Text>
+        <StopWatch></StopWatch>
+      </BlockStack>
+    </Page>
+  );
 }
