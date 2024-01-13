025477bb71bf813a5fd94a0dd7ee851b3cabc471
Will Zhang <wz282@cornell.edu>
2024-01-13 02:19:14 -0500

    Some fun message

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 
 import StopWatch from "./StopWatch";
-import {Page, Text, BlockStack} from "@shopify/polaris";
+import {Page, Text, BlockStack, Link} from "@shopify/polaris";
+
+import "./style.css";
 
 export default function App() {
   return (
-    <Page>
-      <BlockStack gap="1000">
-        <Text variant="heading3xl" as="h1">
-          Stopwatch
-        </Text>
-        <StopWatch />
-      </BlockStack>
-    </Page>
+    <div id="app">
+      <Page>
+        <BlockStack gap="1000">
+          <Text variant="heading3xl" as="h1">
+            Stopwatch
+          </Text>
+          <StopWatch />
+          <Text variant="bodyLg" as="p">
+            Made by{" "}
+            <Link monochrome url="https://wizhaa.com/">
+              Will Zhang
+            </Link>{" "}
+            using Shopify's Polaris UI kit.
+          </Text>
+        </BlockStack>
+      </Page>
+    </div>
   );
 }
