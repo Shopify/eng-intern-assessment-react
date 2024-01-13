ae88b3eb14a95c462d23db4b617e6bacd054717d
Moe N <moenumasawa@gmail.com>
2024-01-12 18:04:46 -0700

    Create UI for buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faStop, faRotateRight } from '@fortawesome/free-solid-svg-icons';
+import "./StopWatchButton.css"
 
-export default function StopWatchButton() {
+const StopWatchButton: React.FC = ({ }) => {
     return(
-        <div></div>
-    )
-}
+        <div className="button-container">
+            <button className="button-start"><FontAwesomeIcon icon={faPlay} /></button>
+            <button className="button-stop"><FontAwesomeIcon icon={faStop} /></button>
+            <button className="button-reset"><FontAwesomeIcon icon={faRotateRight} /></button>
+        </div>
+    );
+};
+
+export default StopWatchButton;
