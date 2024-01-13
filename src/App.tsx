b125e5b05b3bff5fd6c5a33a6b49ac7c702bc352
Will Zhang <wz282@cornell.edu>
2024-01-13 02:24:03 -0500

    No styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,22 @@ import React from "react";
 import StopWatch from "./StopWatch";
 import {Page, Text, BlockStack, Link} from "@shopify/polaris";
 
-import "./style.css";
-
 export default function App() {
   return (
-    <div id="app">
-      <Page>
-        <BlockStack gap="1000">
-          <Text variant="heading3xl" as="h1">
-            Stopwatch
-          </Text>
-          <StopWatch />
-          <Text variant="bodyLg" as="p">
-            Made by{" "}
-            <Link monochrome url="https://wizhaa.com/">
-              Will Zhang
-            </Link>{" "}
-            using Shopify's Polaris UI kit.
-          </Text>
-        </BlockStack>
-      </Page>
-    </div>
+    <Page>
+      <BlockStack gap="1000">
+        <Text variant="heading3xl" as="h1">
+          Stopwatch
+        </Text>
+        <StopWatch />
+        <Text variant="bodyLg" as="p">
+          Made by{" "}
+          <Link monochrome url="https://wizhaa.com/">
+            Will Zhang
+          </Link>{" "}
+          using Shopify's Polaris UI kit.
+        </Text>
+      </BlockStack>
+    </Page>
   );
 }
