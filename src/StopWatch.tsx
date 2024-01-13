b82c337e9241636354ed3f99e8b8328db020656b
Teo Cristante <teo.cristante@gmail.com>
2024-01-13 13:40:51 -0500

    Adjusted Timer and Lap functionality to use states, added proper formatting for time

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import StopWatchButton from './StopWatchButton'
+import { format } from 'path'
 
 export default function StopWatch() {
     
-    // Variables for the time
-    let sec = 0
-    let min = 0
-    let hour = 0
+    // // Variables for the time
+    // let sec = 0
+    // let min = 0
+    // let hour = 0
 
-    // String variables for the time
-    let sec_str: string
-    let min_str: string
-    let hour_str: string
+    // // String variables for the time
+    // let sec_str: string
+    // let min_str: string
+    // let hour_str: string
 
-    // Variables for the lap time
-    let sec_lap = 0
-    let min_lap = 0
-    let hour_lap = 0
+    // // Variables for the lap time
+    // let sec_lap = 0
+    // let min_lap = 0
+    // let hour_lap = 0
 
-    // String variables for the lap time
-    let sec_lap_str: string
-    let min_lap_str: string
-    let hour_lap_str: string
+    // // String variables for the lap time
+    // let sec_lap_str: string
+    // let min_lap_str: string
+    // let hour_lap_str: string
 
-    // Variables for the timer
-    let time: any
+    // // Variables for the timer
+    let clock: any
     let timer: any
+
+    const [time, setTime] = useState(0)
+    const [lap, setLap] = useState(0)
+    const [buttonName, setButtonName] = useState("Pause")
+
+
+    function formatTime(number: number) {
+        let hours = Math.floor(number/3600)
+        let minutes = Math.floor((number%3600)/60)
+        let seconds = Math.floor(number%60)
+
+        return ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2)
+    }
 
     // Calculates the time and displays it
     function startTime() {
@@ -37,48 +51,28 @@ export default function StopWatch() {
         // Starts the timer
         timer = setInterval( () => {
             // Increments the time
-            sec++
-            if(sec == 60) {
-                sec = 0
-                min++
-            }
-            if(min == 60) {
-                min = 0
-                hour++
-            }
-
-            // Increments the lap time
-            sec_lap++
-            if(sec_lap == 60) {
-                sec_lap = 0
-                min_lap++
-            }
-            if(min_lap == 60) {
-                min_lap = 0
-                hour_lap++
-            }
-            
-            // Adds a 0 to the front of the number if it is less than 10
-            if (sec <= 9) {
-                sec_str = "0" + sec
-            }
-            if (min <= 9) {
-                min_str = "0" + min
-            }
-            if (hour <= 9) {
-                hour_str = "0" + hour
-            }
+            setTime((prevTime) => prevTime + 1)
+            setLap((prevLap) => prevLap + 1)
 
             // Displays the time
-            time = document.getElementById("time")
-            time.innerHTML = hour_str + ":" + min_str + ":" + sec_str
+            clock = document.getElementById("time")
+            clock.innerHTML = formatTime(time)
         }, 1000)
     }
 
-    // Stops the timer
-    function stopTime() {
-        clearInterval(timer)
-    }
+    // Pauses and Resumes the timer
+    function pauseTime() {
+        
+        if (buttonName === "Pause") {
+            setButtonName("Resume")
+            clearInterval(timer);
+        } else {
+            setButtonName("Pause")
+            clearInterval(timer)
+            startTime()
+        }
+        
+    }   
 
     // Resets the timer
     function resetTime() {
@@ -86,63 +80,51 @@ export default function StopWatch() {
         clearInterval(timer)
 
         // Resets the time
-        sec = 0
-        min = 0
-        hour = 0
+        setTime(0)
+        setLap(0)
         
         //Displays the reseted time
-        time = document.getElementById("time")
-        time.innerHTML = "00:00:00"
+        clock = document.getElementById("time")
+        clock.innerHTML = "00:00:00"
 
         // Removes all the laps
-        let lapList = document.getElementById("lapList")
-        while (lapList.firstChild) {
-            lapList.removeChild(lapList.firstChild)
+        let lapList = document.getElementById("lap-list")
+        if (lapList.childElementCount > 0) {
+            while (lapList.firstChild) {
+                lapList.removeChild(lapList.firstChild)
+            }
         }
     }
     
     // Displays the lap time
     function lapTime() {
         // Creates list item
-        let lapList = document.getElementById("lapList")
-        let lap = document.createElement("li")
-        
-        // Adds a 0 to the front of the number if it is less than 10
-        if (sec_lap <= 9) {
-            sec_lap_str = "0" + sec_lap
-        }
-        if (min_lap <= 9) {
-            min_lap_str = "0" + min_lap
-        }
-        if (hour_lap <= 9) {
-            hour_lap_str = "0" + hour_lap
-        }
-        
+        let lapList = document.getElementById("lap-list")
+        let lapElement = document.createElement("li")
+
         // Displays the lap time
-        let currentLap = hour_lap_str + ":" + min_lap_str + ":" + sec_lap_str
-        let currentTime = hour_str + ":" + min_str + ":" + sec_str
-        lap.innerHTML = "Lap: " + currentLap + "   " + "Total: " + currentTime
+        let currentLap =  formatTime(lap)
+        let currentTime = formatTime(time)
+        lapElement.innerHTML = "Lap: " + currentLap + "   " + "Total: " + currentTime
         
         // Resets the lap time
-        hour_lap = 0
-        min_lap = 0
-        sec_lap = 0
+        setLap(0)
         
         // Adds the lap to the list
-        lapList.appendChild(lap)
-        
+        lapList.appendChild(lapElement)
     }
+
 
 
     return(
         <div>
             <h2>StopWatch</h2>
-            <h3 id="time">00:00:00</h3>
+            <h3 id="time">{formatTime(time)}</h3>
             <StopWatchButton name="Start" stopWatchHandler={startTime} />
-            <StopWatchButton name="Stop" stopWatchHandler={stopTime} />
+            <StopWatchButton id="pause" name={buttonName} stopWatchHandler={pauseTime} />
             <StopWatchButton name="Reset" stopWatchHandler={resetTime} />
             <StopWatchButton name="Lap" stopWatchHandler={lapTime} />
-            <ol id="lapList"></ol>
+            <ol id="lap-list"></ol>
         </div>
     )
 }
