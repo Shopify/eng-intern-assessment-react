f295d6b9606c2cca60f71d335f03415051a0ec1d
whiffy <bryanzhao168@yahoo.ca>
2024-01-12 18:31:54 -0800

    Commiting current files, need to fix tests next.

---
+++ src/StopWatch/StopWatch.tsx
@@ -0,0 +1,101 @@
+import React, { createContext, useEffect, useState } from "react";
+import "./StopWatch.css";
+import StopWatchButton from "../StopWatchButton/StopWatchButton";
+
+interface StopWatchContextInterface {
+  command: string;
+  setCommand: Function;
+}
+
+export const StopWatchContext = createContext<StopWatchContextInterface>({
+  command: "",
+  setCommand: () => {},
+});
+
+/**
+ * * This Component displays the stopwatch and the laps.
+ * * We used useContext to communicate with the buttons.
+ **/
+
+export default function StopWatch() {
+  const [command, setCommand] = useState<string>("");
+  const [laps, setLaps] = useState<string[]>([]);
+  const [time, setTime] = useState<number>(0);
+  const minutes = Math.floor((time / 60000) % 60);
+  const seconds = Math.floor((time / 1000) % 60);
+  const milliseconds = Math.floor((time / 10) % 100);
+
+  useEffect(() => {
+    let interval: string | number | NodeJS.Timeout;
+
+    switch (command) {
+      case "start":
+        interval = setInterval(() => setTime(time + 10), 10);
+        break;
+      case "stop":
+        clearInterval(interval);
+        break;
+      case "reset":
+        setTime(0);
+        setLaps([]);
+        setCommand("");
+        break;
+      case "resume":
+        interval = setInterval(() => setTime(time + 10), 10);
+        break;
+      case "pause":
+        clearInterval(interval);
+        break;
+    }
+
+    return () => clearInterval(interval);
+  }, [time, command]);
+
+  const CheckForLeadingZero = (value: number) => {
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value;
+    }
+  };
+
+  useEffect(() => {
+    if (command === "lap" && time !== 0) {
+      setLaps([
+        ...laps,
+        `${CheckForLeadingZero(minutes)}:${CheckForLeadingZero(
+          seconds
+        )}:${CheckForLeadingZero(milliseconds)}`,
+      ]);
+
+      setCommand("resume");
+    }
+  }, [command]);
+
+  return (
+    <StopWatchContext.Provider value={{ command, setCommand }}>
+      <div>
+        <div className="stop-watch-main-display" id="stop-watch-time">
+          {command === "stop" ? (
+            "Stopped"
+          ) : (
+            <div>
+              {CheckForLeadingZero(minutes)}:{CheckForLeadingZero(seconds)}:
+              {CheckForLeadingZero(milliseconds)}
+            </div>
+          )}
+        </div>
+
+        <StopWatchButton />
+        <div className="stop-watch-laps-main-display" data-testid="lap-list">
+          {laps.map((lap, index) => (
+            <div key={index} className="stop-watch-laps-inner-display">
+              <div> Lap {index + 1}:</div>
+              <div>{lap}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </StopWatchContext.Provider>
+  );
+}
---
+++ src/StopWatchButton/StopWatchButton.tsx
@@ -0,0 +1,114 @@
+import React, { useContext } from "react";
+import "./StopWatchButton.css";
+import { StopWatchContext } from "../StopWatch/StopWatch";
+
+/**
+ * * This component displays the button and communicates with the stopwatch via useContext.
+ * * I chose to make the Reset button disapper when the stopwatch is stopped, this was based on the Apple stopwatch.
+ **/
+
+export default function StopWatchButton() {
+  const StopWatchContextInButton = useContext(StopWatchContext);
+  const StopWatchContextStopCase = () => {
+    if (
+      StopWatchContextInButton.command === "stop" ||
+      StopWatchContextInButton.command === "pause"
+    ) {
+      return "hidden";
+    } else {
+      return "visible";
+    }
+  };
+
+  const StopWatchContextStartCase = () => {
+    if (StopWatchContextInButton.command === "") {
+      return "";
+    } else {
+      return "none";
+    }
+  };
+
+  const StopWatchContextResumePauseCase = () => {
+    if (StopWatchContextInButton.command === "") {
+      return "none";
+    } else {
+      return "";
+    }
+  };
+
+  return (
+    <div>
+      <div className="stop-watch-button-flex" id="button-list">
+        <button
+          id="start-button"
+          className="stop-watch-button-button"
+          onClick={() => {
+            StopWatchContextInButton.setCommand("start");
+          }}
+          style={{
+            display: `${StopWatchContextStartCase()}`,
+          }}
+        >
+          Start
+        </button>
+        <div
+          className="stop-watch-button-resume-pause-flex"
+          style={{
+            display: `${StopWatchContextResumePauseCase()}`,
+          }}
+        >
+          <button
+            id="resume-button"
+            className="stop-watch-button-button"
+            onClick={() => {
+              StopWatchContextInButton.setCommand("pause");
+            }}
+          >
+            Pause
+          </button>
+          <button
+            id="pause-button"
+            className="stop-watch-button-button"
+            onClick={() => {
+              StopWatchContextInButton.setCommand("resume");
+            }}
+          >
+            Resume
+          </button>
+        </div>
+
+        <button
+          id="stop-button"
+          className="stop-watch-button-button"
+          onClick={() => {
+            StopWatchContextInButton.setCommand("stop");
+          }}
+        >
+          Stop
+        </button>
+        <button
+          id="reset-button"
+          className="stop-watch-button-button"
+          onClick={() => {
+            StopWatchContextInButton.setCommand("reset");
+          }}
+        >
+          Reset
+        </button>
+        <button
+          id="lap-button"
+          data-testid="lap-button"
+          className="stop-watch-button-button"
+          onClick={() => {
+            StopWatchContextInButton.setCommand("lap");
+          }}
+          style={{
+            visibility: `${StopWatchContextStopCase()}`,
+          }}
+        >
+          Lap
+        </button>
+      </div>
+    </div>
+  );
+}
