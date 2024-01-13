d0b6723bf317698dc6a4c633d5882d02b4c3374a
Will Zhang <wz282@cornell.edu>
2024-01-13 18:46:06 -0500

    add polaris app icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,28 @@ import React from "react";
 
 import StopWatch from "./StopWatch";
 import {Page, Text, BlockStack, Link} from "@shopify/polaris";
+import {ChatMajor} from "@shopify/polaris-icons";
 
 export default function App() {
   return (
-    <Page>
+    <Page
+      narrowWidth
+      title="Stopwatch"
+      subtitle="For Shopify's 2024 Summer Frontend Internship"
+      secondaryActions={[
+        {
+          content: "Connect",
+          external: true,
+          icon: ChatMajor,
+          url: "https://www.linkedin.com/in/notwz/",
+        },
+      ]}
+    >
       <BlockStack gap="1000">
-        <Text variant="heading3xl" as="h1">
-          Stopwatch
-        </Text>
         <StopWatch />
         <Text variant="bodyLg" as="p">
           Made by{" "}
-          <Link monochrome url="https://wizhaa.com/">
+          <Link monochrome url="https://www.linkedin.com/in/notwz/">
             Will Zhang
           </Link>{" "}
           using Shopify's Polaris UI kit.
