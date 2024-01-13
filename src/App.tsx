bde1a1c3592df0bde329dce017bdced4c7b08405
HudsonPryde <hudson.stuart@ryerson.ca>
2024-01-12 22:27:58 -0500

    add page formatting and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import StopWatch from "./StopWatch";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "90vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <StopWatch />
+    </div>
+  );
 }
