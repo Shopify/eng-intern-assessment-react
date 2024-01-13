636a5000bcb296f4c35261084f606c9b791ea536
Lucas Prins <lucas2002prins@gmail.com>
2024-01-13 13:25:31 +0100

    feat: implement basic useStopwatch hook and time formatting

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import useStopWatch from "../hooks/useStopwatch";
+import { formatTime } from "../lib/formatting/time";
 
 export default function StopWatch() {
-    return(
-        <div>
-            Stopwatch
-        </div>
-    )
+  const {
+    state: stopwatchState,
+    elapsedTime: stopwatchElapsedTime,
+    handleStart: handleStartStopwatch,
+    handleStop: handleStopStopwatch,
+    handleReset: handleResetStopwatch,
+  } = useStopWatch({ debug: true });
+
+  const isIdle = React.useMemo(() => stopwatchState === "idle", [stopwatchState]);
+
+  const formattedTime = React.useMemo(() => {
+    return formatTime(stopwatchElapsedTime);
+  }, [stopwatchElapsedTime]);
+
+  return (
+    <div>
+      <span>{`${formattedTime.hours}:${formattedTime.minutes}:${formattedTime.seconds}:${formattedTime.milliseconds}`}</span>
+      <button onClick={isIdle ? handleStartStopwatch : handleStopStopwatch}>{isIdle ? "start" : "pause"}</button>
+      <button onClick={handleResetStopwatch}>Reset</button>
+    </div>
+  );
 }
--- src/hooks/useStopwatch.ts
+++ src/hooks/useStopwatch.ts
@@ -4,54 +4,86 @@ type StopwatchState = "idle" | "running";
 
 type UseStopwatch = {
   state: StopwatchState;
-  elapsedTime: number; 
+  elapsedTime: number;
   handleStart: () => void;
-  handleStop : () => void;
-}
+  handleStop: () => void;
+  handleReset: () => void;
+};
 
 type UseStopwatchParams = {
   initialTime?: number;
-}
+  updateInterval?: number;
+  debug?: boolean;
+};
+
+const log = (message: string) => console.log(`[useStopwatch] ${message}`);
 
 /**
  * A hook that encapsulates stopwatch behaviour.
- * 
+ *
  * @example
- * 
+ *
  * ```tsx
- * 
+ *
  * ```
  */
-export default function useStopWatch({
-  initialTime,
-}: UseStopwatchParams): UseStopwatch {
+export default function useStopWatch({ initialTime = 0, updateInterval = 10, debug = false }: UseStopwatchParams): UseStopwatch {
   const [state, setState] = React.useState<StopwatchState>("idle");
-  const [elapsedTime, setElapsedTime] = React.useState<number>(initialTime ?? 0);
+  const [elapsedTime, setElapsedTime] = React.useState<number>(initialTime);
 
-  const intervalRef = useRef<NodeJS.Timer | null>(null)
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+
+  const clearIntervalInternal = React.useCallback(() => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+  }, [intervalRef]);
 
   const handleStart = React.useCallback(() => {
-    
-  }, [])
+    debug && log("Starting timer");
+
+    if (state === "idle") {
+      // Using date is a more reliable approach than relying just on
+      // setInterval because it is not guaranteed to run at precise intervals.
+      const startTime = Date.now() - elapsedTime;
+
+      intervalRef.current = setInterval(() => {
+        const currentTime = Date.now();
+        const newElapsedTime = currentTime - startTime;
+
+        setElapsedTime(newElapsedTime);
+      }, updateInterval);
+
+      setState("running");
+    }
+  }, [state, intervalRef, elapsedTime, updateInterval]);
 
   const handleStop = React.useCallback(() => {
+    debug && log("Stopping timer");
 
-  }, [])
+    if (state === "running") {
+      clearIntervalInternal();
+      setState("idle");
+    }
+  }, [state]);
 
   const handleReset = React.useCallback(() => {
+    debug && log("Resetting timer");
 
-  }, [])
+    clearIntervalInternal();
+    setState("idle");
+    setElapsedTime(initialTime);
+  }, []);
+
+  React.useEffect(() => {
+    return () => clearIntervalInternal();
+  }, []);
 
   return {
     state,
     elapsedTime,
     handleStart,
-    handleStop
-  }
+    handleStop,
+    handleReset,
+  };
 }
 
-export type { 
-  StopwatchState,
-  UseStopwatch,
-  UseStopwatchParams
-}
+export type { StopwatchState, UseStopwatch, UseStopwatchParams };
