ef3c4ea91141c61d099023ce1a1a05cdcdcd3c24
Will Zhang <wz282@cornell.edu>
2024-01-13 02:12:14 -0500

    Create a simple table to display laps

---
+++ src/Laps.tsx
@@ -0,0 +1,24 @@
+import React, {useCallback, useState} from "react";
+
+import {Card, Text, BlockStack, DataTable} from "@shopify/polaris";
+
+type LapsProps = {
+  laps: Array<Array<any>>;
+};
+export default function Laps({laps}: LapsProps) {
+  return (
+    <Card>
+      <Text variant="heading2xl" as="h2">
+        Laps
+      </Text>
+      <BlockStack gap="1000">
+        <DataTable
+          columnContentTypes={["numeric", "text", "text"]}
+          headings={["Lap Number", "Lap Time", "Total Time Elapsed"]}
+          rows={laps}
+          hasZebraStripingOnData
+        />
+      </BlockStack>
+    </Card>
+  );
+}
