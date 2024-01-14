b8955b3674f41d5307de731f9c7c7bf413fb2fff
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-13 19:08:32 -0500

    implement start, stop, and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { AppProvider } from '@shopify/polaris';
+import StopWatch from './StopWatch'
 
-export default function App() {
-    return(
-        <div></div>
-    )
+export default function App () {
+  return (
+    <AppProvider i18n={ {
+      Polaris: {
+        ResourceList: {
+          sortingLabel: 'Sort by',
+          defaultItemSingular: 'item',
+          defaultItemPlural: 'items',
+          showing: 'Showing {itemsCount} {resource}',
+          Item: {
+            viewItem: 'View details for {itemName}',
+          },
+        },
+        Common: {
+          checkbox: 'checkbox',
+        },
+      },
+    } }>
+      <StopWatch />
+    </AppProvider>
+  )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button } from '@shopify/polaris';
+import moment from 'moment';
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+
+
+export default function StopWatch () {
+
+  // Timer function
+
+  const [ time, setTime ] = useState(0)
+  const [ isTimerActive, setIsTimerActive ] = useState(false)
+
+
+  // const hours = time + Math.floor(((1000 * 60 * 60 * 24)) );
+  // const minutes = time + Math.floor(((1000 * 60 * 60)) );
+  // const seconds = time + Math.floor(((1000 * 60)));
+
+  useEffect(() => {
+    console.log("TIME: ", time)
+    console.log("ISTIMERACTIVE: ", isTimerActive)
+
+    if (isTimerActive) {
+      setTimeout(() => {
+        setTime(time + 1)
+        // console.log(time)
+      }, 0);
+    }
+  }, [ time, isTimerActive ])
+
+  // Start button click(starts timer -> turns into pause/resume once started)
+  const handleStartClick = () => {
+    setIsTimerActive(true)
+  }
+
+
+  // Stop button click(stop timer -> when stopped, lap can't be pressed but restart can be pressed)
+  const handleStopClick = () => {
+    setIsTimerActive(false)
+    console.log("Stop btn: ", time)
+
+  }
+
+  // Lap button click(records time but timer keeps going)
+  const handleLapClick = () => {
+
+  }
+
+
+  // Restart button click (restarts timer -> shows up when timer is stopped)
+  const handleRestartClick = () => {
+    setIsTimerActive(false)
+    setTime(0)
+  }
+
+
+
+  return (
+    <div>
+      <h1>Countdown Timer</h1>
+      <p>{ moment(time).format("mm:ss:SS") }</p>
+      <div className='stopwatchBtns' >
+        <Button onClick={ handleStartClick }>Start</Button>
+        <Button onClick={ handleStopClick }>Stop</Button>
+        <Button onClick={ handleLapClick }>Lap</Button>
+        <Button onClick={ handleRestartClick }>Restart</Button>
+      </div>
+    </div>
+  )
 }
