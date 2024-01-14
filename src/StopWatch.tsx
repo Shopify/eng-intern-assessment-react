6e765e14e0c3158854ce78c3c0a57078f32c71ad
Alborz Khakbazan <alvisk1313@gmail.com>
2024-01-14 12:04:49 -0600

    Basic Stopwatch Functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [isRunning, setIsRunning] = useState(false); // stopwatch is running
+  const [elapsedTime, setElapsedTime] = useState(0); // in ms
+
+  // If the stopwatch is running, update the elapsed time every 10ms
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  // Handlers for the stop, start, and reset
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+  const handleStop = () => setIsRunning(false);
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>{elapsedTime}</h1>
+      </div>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
 }
