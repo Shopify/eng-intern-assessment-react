5a1d7749e7e7c78002326b3f118a190a3c652630
Juan Ramirez <juandiegoramirezarnal@gmail.com>
2024-01-14 17:47:05 -0500

    add minutes, seconds, and milliseconds display

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function StopWatch({ milliSeconds }:{ milliSeconds: number }) {
+    function getMilliseconds() : string {
+        return (milliSeconds%1000).toLocaleString('en-US', {minimumIntegerDigits: 2}).slice(0,2)
+    }
+    function getSeconds() : string {
+        var seconds = Math.floor(milliSeconds/1000)%60
+        return seconds.toLocaleString('en-US', {minimumIntegerDigits: 2})
+    }
+    function getMinutes() : string {
+        var minutes = Math.floor(milliSeconds/1000)/60
+        return minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, maximumFractionDigits: 0})
+    }
+
     return(
         <div>
-            <h1> 00:00:{ milliSeconds }</h1>
+            <h1> {getMinutes()}:{getSeconds()}:{getMilliseconds()}</h1>
         </div>
     )
 }
