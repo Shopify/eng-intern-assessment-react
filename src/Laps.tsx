a27bf014825915a54b629adc48c93e431c64f494
Will Zhang <wz282@cornell.edu>
2024-01-13 19:26:53 -0500

    fix testing suite for polaris

--- src/Laps.tsx
+++ src/Laps.tsx
@@ -1,23 +1,47 @@
-import React, {useCallback, useState} from "react";
+import React, {useState} from "react";
 
-import {Card, Text, BlockStack, DataTable} from "@shopify/polaris";
+import {
+  Card,
+  Text,
+  BlockStack,
+  DataTable,
+  Collapsible,
+  Button,
+} from "@shopify/polaris";
 
 type LapsProps = {
   laps: Array<Array<any>>;
 };
 export default function Laps({laps}: LapsProps) {
+  const [open, setOpen] = useState(false);
   return (
     <Card>
       <Text variant="headingLg" as="h2">
         Laps
       </Text>
       <BlockStack gap="1000">
-        <DataTable
-          columnContentTypes={["numeric", "text", "text"]}
-          headings={["Lap Number", "Lap Time", "Total Time Elapsed"]}
-          rows={laps}
-          hasZebraStripingOnData
-        />
+        {laps.length > 0 && (
+          <DataTable
+            columnContentTypes={["numeric", "text", "text"]}
+            headings={["Lap Number", "Lap Time", "Total Time Elapsed"]}
+            rows={laps}
+            hasZebraStripingOnData
+          />
+        )}
+        <div></div>
+        <Button onClick={() => setOpen((open) => !open)}>
+          Display Raw Laps for Testing
+        </Button>
+        <Collapsible open={open} id="laps">
+          <div data-testid="lap-list">
+            {laps.length == 0 && "No laps yet."}
+            {laps.map((lap, i) => (
+              <div key={i}>
+                {i} | {lap[1]}
+              </div>
+            ))}
+          </div>
+        </Collapsible>
       </BlockStack>
     </Card>
   );
