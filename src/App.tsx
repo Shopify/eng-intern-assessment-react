9d69e886877cfd673e32a3d0142a67c62f9bc9c7
Manav Shergill <shergillmanav@uvic.ca>
2024-01-14 11:52:17 -0800

    feat: installed and now using tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
+import React from "react";
+import "./index.css";
 export default function App() {
-    return(
-        <div></div>
-    )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
