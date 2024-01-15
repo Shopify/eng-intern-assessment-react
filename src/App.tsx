01c032d34dc8d06dd92f69721d1f8ebef79e7be0
Jared Drueco <drueco03@gmail.com>
2024-01-15 10:46:49 -0700

    feat: initialize page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Text } from '@shopify/polaris';
 import React from 'react';
+import { Page } from '@shopify/polaris';
+import { ExternalIcon } from '@shopify/polaris-icons';
+import StopWatch from './components/StopWatch';
 
 export default function App() {
   return (
-    <Text variant="heading3xl" as="h2">
-      Stop Watch App
-    </Text>
+    <Page
+      title="Stop Watch App"
+      subtitle="Shopify Frontend Engineering Challenge Summer 2024"
+      secondaryActions={[
+        {
+          content: 'By Jared Drueco',
+          external: true,
+          icon: ExternalIcon,
+          url: 'https://www.jareddrueco.com/',
+        },
+      ]}
+    >
+      <StopWatch />
+    </Page>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@shopify/polaris/build/esm/styles.css';
+import { AppProvider } from '@shopify/polaris';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <AppProvider i18n={{}}>
+    <App />
+  </AppProvider>
+);
