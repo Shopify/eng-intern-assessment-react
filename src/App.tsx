0c75e48532f21e9433d6d9c1572e00e627c6960c
Terence Liu <teren@Midnight.lan>
2024-01-15 15:50:57 -0500

    Moved logic code into StopWatch.tsx instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,10 @@
 import React from 'react'
 import StopWatch from './StopWatch'
-import StopWatchButton from './StopWatchButton'
-import { useState, useEffect } from 'react'
 
 export default function App() {
-  const [isRunning, setIsRunning] = useState(false)
-  const [time, setTime] = useState(0)
-  const [laps, setLaps] = useState<String[]>([])
-
-  let interval: NodeJS.Timer | undefined;
-  let minutes = ('0' + Math.floor((time / 60000) % 60)).slice(-2)
-  let seconds = ('0' + Math.floor((time / 1000) % 60)).slice(-2)
-  let milliseconds = ('0' + ((time / 10) % 100)).slice(-2)
-
-  const start = () => {
-    if (isRunning) {
-      setIsRunning(false)
-    } else if (!isRunning) {
-      setIsRunning(true)
-    }
-  }
-
-  const reset = () => {
-    if (!isRunning) {
-      setTime(0)
-      setLaps([])
-    }
-  }
-
-  const lap = () => {
-    setLaps([...laps, String(minutes + ':' + seconds + ':' + milliseconds)])
-  }
-
-  useEffect(() => {
-    if (isRunning) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10)
-      }, 10);
-    } else {
-      clearInterval(interval)
-    }
-
-    return () => {
-      clearInterval(interval)
-    };
-  }, [isRunning])
-
   return(
     <div>
-      <StopWatch minutes={minutes} seconds={seconds} milliseconds={milliseconds}/>
-      <StopWatchButton start={start} reset={reset} lap={lap} isRunning={isRunning}/>
-      {laps != null && laps.length > 0 && (
-        <div className="laps-grid">
-          {laps.map((lap, i) => <div>{lap}</div>)}
-        </div>
-      )}
+      <StopWatch />
     </div>
   )
 }
