3ac58ac4b8258d2dbd4716efe8367ebff4ac75b1
Manav Shergill <shergillmanav@uvic.ca>
2024-01-14 20:18:06 -0800

    fix: correcting the seconds calculation

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,6 +4,7 @@ export const getCurrentTime = (seconds: number) => {
   }
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds - hours * 3600) / 60);
+  seconds = seconds - hours * 3600 - minutes * 60;
 
   return {
     hours,
