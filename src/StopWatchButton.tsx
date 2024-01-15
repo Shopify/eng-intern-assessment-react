0ca237fced6c725db61bc5ce177bdc93871105b5
Mitchell Wagner <pim128@usask.ca>
2024-01-15 16:19:48 -0600

    Finalized Lap timer,
    function now needs to be fully tested to verify accuracy

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -23,17 +23,35 @@ interface stopWatchProps {
 *       setTime - useState Function - set's current time
 *       setIsRunning = useState Function - set's boolean if running or not
 */
-export default function StopWatchButton({setTime, setIsRunning, time, addLap, laps, lapTime, setLapTime}: stopWatchProps) {
+export default function StopWatchButton({setTime, setIsRunning, addLap, laps, lapTime, setLapTime}: stopWatchProps) {
+    /**
+     *  Purpose:
+     *      Handles the click of the lap button to display accurate lap times
+     * 
+     */
     const handleLap = (currentTime: moment.Duration) => {
         addLap([...laps, currentTime]);
         setLapTime(moment.duration(0));
+    }
+
+    /**
+     * Purpose:
+     *      Handles the reset of the timer
+     * 
+     *      Resets the list of laps and timers. Stops the time.
+     */
+    const handleReset = () => {
+        setIsRunning(false);
+        setLapTime(moment.duration(0));
+        setTime(moment.duration(0));
+        addLap([]);
     }
 
     return(
         <div>
             <button onClick={() => setIsRunning(true)}>Start</button>
             <button onClick={() => setIsRunning(false)}>Stop</button>
-            <button onClick={() => setTime(moment.duration(0))}>Reset</button>
+            <button onClick={() => handleReset()}>Reset</button>
             <button onClick={() => handleLap(lapTime)}>Lap</button>
         </div>
     )
