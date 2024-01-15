513058d595f914fe69598a491fc7ea1cef0f75f0
luevrata <firsovalizzz@gmail.com>
2024-01-15 15:21:17 -0800

    add milliseconds

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
   const [time, setTime] = useState<number>(0);
-  const [isPaused, setIsPaused] = useState<boolean>(false);
+  const [isPaused, setIsPaused] = useState<boolean>(true);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
     if (!isPaused) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
-      }, 1000);
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
     }
     return () => clearInterval(interval);
   }, [isPaused, time]);
 
-  const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
+  const formatTime = (totalMilSeconds: number) => {
+    const milliseconds = Math.floor((totalMilSeconds / 10) % 1000);
+    const seconds = Math.floor((totalMilSeconds / 1000) % 60);
+    const minutes = Math.floor((totalMilSeconds / 60000) % 60);
 
     const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
+    const formattedMilliseconds = String(milliseconds)
+      .padStart(2, "0")
+      .slice(-2);
 
-    return `${formattedMinutes}:${formattedSeconds}`;
+    return `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
   };
 
   return (
