aac18332400f05f616182c5ad0823ce419beb6db
Mitchell Wagner <pim128@usask.ca>
2024-01-15 14:25:58 -0600

    Added required buttons to app element
    add start, stop, reset buttons to the StopWatchButton and then called their components in the app.tsx
    
    might need to change types from number to time element.

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
-export default function StopWatchButton() {
+/**
+* Interface for stopwatch buttons.
+*/
+interface stopWatchProps {
+    setTime: React.Dispatch<React.SetStateAction<number>>
+    setIsRunning: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+/**
+* Purpose:
+*      Interface that has 3 buttons to add functionality
+*      to the stopwatch itself.
+* 
+* Params:
+*      setTime - useState Function - current time
+*/
+export default function StopWatchButton({setTime, setIsRunning}: stopWatchProps) {
     return(
-        <div></div>
+        <div>
+            <button onClick={() => setIsRunning(true)}>Start</button>
+            <button onClick={() => setIsRunning(false)}>Stop</button>
+            <button onClick={() => setTime(0.00)}>Reset</button>
+        </div>
     )
 }
