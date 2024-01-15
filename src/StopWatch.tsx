6083581ba84acc1b8195489e6ad4502cd9fc1d44
Chen Jun Chi <93779005+MosinLover@users.noreply.github.com>
2024-01-14 20:10:23 -0500

    initial stop watch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import StopWatchButton from './StopWatchButton';
+import './styles.css';
 
 export default function StopWatch() {
+    const [isRunning, setRunning] = useState(false)
+    const [isUsed, setUsed] = useState(false)
+    const [time, setTime] = useState(0)
+    const [lapTime, setLapTime] = useState(0)
+    const [lapStore, setLapStore] = useState([])
+
+    const handleStart = () => {
+        setRunning(true)
+        setUsed(true)
+    }
+    const handleStop = () => {
+        setRunning(false)
+    }
+    const handleReset = () => {
+        setTime(0)
+        setLapTime(0)
+        setLapStore([])
+        setUsed(false)
+    }
+    const handleLap = () => {
+        setLapTime(time)
+        setLapStore([...lapStore, lapTime])
+    }
+    useEffect(() => {
+        let interval: any = null
+        if (isRunning) {
+            interval = setInterval(() => {
+                setTime((time) => time + 10)
+            }, 1);
+        } else {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval)
+    }, [isRunning])
+
+    const processTime = (time: any) => {
+        const miliseconds = (time % 1000) / 10;
+        const seconds = Math.floor(time / 1000) % 60;
+        const minutes = Math.floor(time / 60000) % 60;
+        return `${minutes.toString().padStart(2, '0')} : ` + `${seconds.toString().padStart(2, '0')} : ` + `${miliseconds.toString().padStart(2, '0')}`
+    }
     return(
-        <div></div>
+        <div className='timer-container'>
+            <div>{processTime(time)}</div>
+            <StopWatchButton isUsed={isUsed} isRunning={isRunning} onStart={handleStart} onStop={handleStop} onReset={handleReset} onLap={handleLap}/>
+            <div>{lapStore.map(lapTime => processTime(lapTime))}</div>
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
+export interface StopWatchButtonProps {
+    isUsed: boolean;
+    isRunning: boolean;
+    onStart: () => void
+    onStop: () => void
+    onReset: () => void
+    onLap: () => void
+}
+
+export default function StopWatchButton({isUsed, isRunning, onStart, onStop, onReset, onLap}: StopWatchButtonProps) {
+    // a state controlling start and stop button since they are using the same button
+    // a state controlling reset and lap button since they are using the same button
+    // lap disabled at first
+    return (
+        <div>
+            <button className='button reset' disabled={!isUsed || isRunning} onClick={onReset}>Reset</button>
+            <button className='button' disabled={!isUsed && isRunning} onClick={onLap}>Lap</button>
+            <button className={(!isRunning || !isUsed)? 'button start': 'button stop'} onClick={isRunning ? onStop : onStart}>{isRunning ? "Stop": "Start"}</button>
+        </div>
     )
 }
