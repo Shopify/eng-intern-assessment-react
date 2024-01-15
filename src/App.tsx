771c38886d47a3597f01facd0c1286de5faf432b
sanakhd <sanakhademi13@gmail.com>
2024-01-15 00:55:42 -0500

    Added comments to App.tsx file as well

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import StopWatch from './StopWatch'
 import "./App.css"
 
+/**
+ * 
+ * @description The main application component.
+ * @author Sana Khademi
+ * @version 1.0.0 
+ */
 export default function App() {
     return (
         <div className='landing'>
