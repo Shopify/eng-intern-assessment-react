8c9a254740364fd03c42c67ca8d345176a48ae60
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-01-15 18:19:58 -0500

    Set up the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { THEME } from "./constants";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (
+    <div
+      style={{
+        backgroundColor: THEME[0],
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></div>
+  );
 }
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+const THEME: string[] = ["#4C3A51", "#774360", "#B25068", "#E7AB79"];
+
+export { THEME };
