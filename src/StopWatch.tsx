ccaea653b6ae7a3643560b0aff3ec0623d7a2428
Manav Shergill <shergillmanav@uvic.ca>
2024-01-14 17:42:59 -0800

    feat: added buttons to stopwatchbutton component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStopwatch from "./hooks/useStopwatch";
 import DisplayTime from "./components/DisplayTime";
 import DisplayLaps from "./components/DisplayLaps";
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
-  const { timer, laps, lap, start, stop, reset, isActive } =
-    useStopwatch();
+  const { timer, laps, lap, start, stop, reset, isActive } = useStopwatch();
 
   return (
     <div>
       <DisplayTime timer={timer} />
       <DisplayLaps laps={laps} />
-      <button onClick={start}>Start</button>
-      <button onClick={stop} disabled={!isActive}>
-        Stop
-      </button>
-      <button onClick={reset} disabled={!isActive && !timer}>
-        Reset
-      </button>
-      <button onClick={lap} disabled={!isActive && !timer}>
-        Lap
-      </button>
+      <StopWatchButton
+        isActive={isActive}
+        timer={timer}
+        start={start}
+        stop={stop}
+        reset={reset}
+        lap={lap}
+      />
     </div>
   );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import useStopwatch from "./hooks/useStopwatch";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+export default function StopWatchButton({
+  start,
+  stop,
+  reset,
+  lap,
+  isActive,
+  timer,
+}: {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+  lap: () => void;
+  isActive: boolean;
+  timer: number;
+}) {
+  return (
+    <div>
+      <button onClick={start} disabled={isActive}>
+        Start
+      </button>
+      <button onClick={stop} disabled={!isActive}>
+        Stop
+      </button>
+      <button onClick={reset} disabled={!isActive && !timer}>
+        Reset
+      </button>
+      <button onClick={lap} disabled={!isActive && !timer}>
+        Lap
+      </button>
+    </div>
+  );
 }
