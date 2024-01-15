491324080be59d68dc75c353e4fa579483074e93
luevrata <firsovalizzz@gmail.com>
2024-01-15 15:12:19 -0800

    set up counter

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [time, setTime] = useState<number>(0);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (!isPaused) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isPaused, time]);
+
+  return (
+    <div>
+      <h2>Stopwatch Display</h2>
+      <h1>{time}</h1>
+    </div>
+  );
 }
