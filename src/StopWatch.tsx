4c9b753fc0425b5e81b09204b3f4f54c9d679a3e
Jonathan Huo <jonhuo11@gmail.com>
2024-01-16 18:11:36 -0500

    basic stopwatch functionality with barebones ui

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import StopWatchButton from './StopWatchButton'
 
 export default function StopWatch() {
+
+    const timerIncrementMs = 10
+    const pause = useRef(true)
+    const intervalId = useRef(0)
+    const [exists, setExists] = useState<boolean>(false)
+    const [time, setTime] = useState<number>(0)
+    const [laps, setLaps] = useState<number[]>([])
+
+    const startTime = () => {
+        stopTime()
+        setTime(0)
+        setLaps([])
+        const id = window.setInterval(() => {
+            if (!pause.current) {
+                setTime(prevTime => (prevTime + timerIncrementMs))
+            }
+        }, timerIncrementMs)
+        intervalId.current = id
+        setExists(true)
+        pause.current = false
+    }
+
+    const togglePause = () => {
+        pause.current = !pause.current
+    }
+
+    const stopTime = () => {
+        window.clearInterval(intervalId.current)
+        setExists(false)
+        pause.current = true
+    }
+
     return(
-        <div></div>
+        <div>
+            <p style={{
+                fontSize: 24
+            }}>{time} ms</p>
+            <StopWatchButton
+                timerState={{
+                    exists: exists,
+                    time: time,
+                    isPaused: pause.current
+                }}
+                onStartClicked={startTime}
+                onLapClicked={() => { setLaps([...laps, time]) }}
+                onStopClicked={stopTime}
+                onPauseClicked={togglePause}
+            />
+            {laps.map((v:number, i:number) => (
+                <p key={i}>{`Lap ${i}: ${v} ms`}</p>
+            ))}
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export default function StopWatchButton() {
+interface TimerState {
+    exists: boolean
+    time: number
+    isPaused: boolean
+}
+
+interface StopWatchButtonProps {
+    timerState: TimerState
+    onStartClicked: () => void
+    onStopClicked: () => void
+    onLapClicked: () => void
+    onPauseClicked: () => void
+}
+
+export default function StopWatchButton({
+    timerState,
+    onStartClicked,
+    onStopClicked,
+    onLapClicked,
+    onPauseClicked
+}: StopWatchButtonProps) {
     return(
-        <div></div>
+        <div>
+            {!timerState.exists && <button onClick={onStartClicked}>Start</button>}
+            {timerState.exists && <button onClick={onPauseClicked}>{timerState.isPaused ? "Pause" : "Unpause"}</button>}
+            {timerState.exists && <button onClick={onStopClicked}>Stop</button>}
+            {timerState.exists && <button onClick={onLapClicked}>Lap</button>}
+        </div>
     )
 }
