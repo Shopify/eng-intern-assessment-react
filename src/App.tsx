4f58b119f06f3d1b831b2ddc5316c26315f1b588
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2024-01-15 20:01:51 -0500

    style: Add styles to app and stopwatch components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import StopWatch from './StopWatch'
+import './App.css';
 
 export default function App() {
     return(
-        <div></div>
+        <section className='stopwatch-section'>
+            <div className='stopwatch-div'>
+                <h1 className='stopwatch-title'>Your StopWatch</h1>
+                <StopWatch />
+            </div>
+        </section>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './StopWatch.css';
 
 export default function StopWatch() {
+    const [time, setTime] = useState(0);
+    const [start, setStart] = useState(false);
+
     return(
         <div></div>
     )
