6257eeaab1493fa833e20e4c3e61afe8e3e8592d
Juan Ramirez <juandiegoramirezarnal@gmail.com>
2024-01-16 07:02:27 -0500

    Make start and stop test check if the stopwatch properly stopped

--- __tests__/Stopwatch.test.js
+++ __tests__/Stopwatch.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import Stopwatch from '../src/StopWatch';
+import { randomInt } from 'crypto';
 
 describe('Stopwatch', () => {
   test('renders initial state correctly', () => {
@@ -11,14 +12,25 @@ describe('Stopwatch', () => {
     expect(screen.queryByTestId('lap-list')).toBeEmptyDOMElement();
   });
 
-  test('starts and stops the stopwatch', () => {
+  test('starts and stops the stopwatch', async () => {
     render(<Stopwatch />);
     
     fireEvent.click(screen.getByText('Start'));
     expect(screen.getByText(/(\d{2}:){2}\d{2}/)).toBeInTheDocument();
 
+    //Get the initial state
+    const intialTime = screen.getByText(/(\d{2}:){2}\d{2}/).textContent;
+
+    //Let the stopwatch run for a random amount of time and check if the value being displayed changed
+    await new Promise((r) => setTimeout(r, randomInt(1, 100)));
+    const randomTime = screen.getByText(/(\d{2}:){2}\d{2}/).textContent;
+    expect(randomTime).not.toEqual(intialTime);
+
+    //Stop the stopwatch wait for a random amount of time and check that the stopwatch display did not change
     fireEvent.click(screen.getByText('Stop'));
-    expect(screen.queryByText(/(\d{2}:){2}\d{2}/)).not.toBeInTheDocument();
+    const stopTime = screen.getByText(/(\d{2}:){2}\d{2}/).textContent;
+    await new Promise((r) => setTimeout(r, randomInt(1, 100)));
+    expect(screen.queryByText(/(\d{2}:){2}\d{2}/).textContent).toBe(stopTime);
   });
 
   test('pauses and resumes the stopwatch', async () => {
