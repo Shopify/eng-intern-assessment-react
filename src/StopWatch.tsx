7e6b627d381d2277b095ac63e454b7d371586dab
Taha Zaryab <tahazaryab10@gmail.com>
2024-01-16 14:03:40 -0500

    Initialize stopwatch component and render on screen

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StopWatchButton from './StopWatchButton'
 
 export default function StopWatch() {
+    const [running, setRunning] = useState(false);
+    const [reset, setReset] = useState(true);
+
     return(
-        <div></div>
+        <div>
+            <div>
+                <h1>Shopify StopWatch</h1>
+                <div>
+                    <p>00:00:00</p>
+                </div>
+            </div>
+            <div>
+                <p>{reset ? "Lap" : "Reset"}</p>
+                <p>{running ? "Stop" : "Start"}</p>
+            </div>
+        </div>
     )
 }
