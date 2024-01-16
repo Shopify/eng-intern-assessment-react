78706eefc0c11b1639e46b2c3c379f16523052e1
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-01-16 02:59:03 -0500

    📦 NEW: added chakra-ui and initial markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+import StopWatch from "./StopWatch";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='flex-start'
+      minHeight='100vh'
+      p={4}
+    >
+      <Heading as='h1' size='2xl' mb='8'>
+        React Stopwatch
+      </Heading>
+    </Box>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ChakraProvider theme={theme}>
+    <App />
+  </ChakraProvider>
+);
