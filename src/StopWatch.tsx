d502e9abb06a03eff0ede07148a66b38bbbc1d88
Daniel Tan <danieltan380@gmail.com>
2024-01-16 23:05:03 -0800

    Replace placeholder values with time values from calculateTime.

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import calculateTime from './helpers/calculateTime'
 
 export default function StopWatch() {
+  const [timeInSeconds, setTimeInSeconds] = useState<number>(0);
+  const [timerArray, setTimerArray] = useState<Array<number>>([]);
+
+  useEffect(() => {
+    let timeArray: Array<number> = calculateTime(timeInSeconds);
+    setTimerArray(timeArray);
+  }, [timeInSeconds])
+
     return(
         <div className = "time-container">
-          <p className='timer-text'>0</p>
+          <p className='timer-text'>{timerArray[0]}</p>
           <span>:</span>
-          <p className='timer-text'>0</p>
+          <p className='timer-text'>{timerArray[1]}</p>
           <span>:</span>
-          <p className='timer-text'>0</p>
+          <p className='timer-text'>{timerArray[2]}</p>
         </div>
     )
 }
