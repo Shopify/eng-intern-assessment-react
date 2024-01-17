7515ba90d5a87457fa2a7a2087d2fe1dcd897ad2
James Shi <jamesshi117@gmail.com>
2024-01-17 11:59:55 -0800

    implement basic stopwatch logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  const [isLive, setIsLive] = useState(false);
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState<number[]>([]);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
+
+  const startStopwatch = () => {
+    setIsLive(true);
+    const startTime = Date.now() - time;
+    const interval = setInterval(() => {
+      setTime(Date.now() - startTime);
+    }, 10);
+
+    setIntervalId(interval);
+    return () => {
+      clearInterval(interval);
+    };
+  };
+
+  const stopStopwatch = () => {
+    setIsLive(false);
+    clearInterval(intervalId);
+  };
+
+  const resetStopwatch = () => {
+    setIsLive(false);
+    clearInterval(intervalId);
+    setTime(0);
+    setLaps([]);
+  };
+
+  const addStopwatchLap = () => {
+    setLaps((prevLaps) => [...prevLaps, time]);
+  };
+
+  return <div></div>;
 }
