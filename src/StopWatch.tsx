ea944848fcebb3bd699c750b65e1556a00731ba3
Joseph Lee <josephlee3004@gmail.com>
2024-01-17 12:15:11 -0500

    Start stopwatch as soon as window opened

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function StopWatch() {
+    const [counter, setCounter] = useState(0);
+
+    // Empty dependency array because we don't want useEffect to
+    // run anytime other than at initial component render
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setCounter(counter => counter + 1);
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, [])
+
     return(
-        <div></div>
+        <>
+            <p>{counter}s</p>
+        </>
     )
 }
