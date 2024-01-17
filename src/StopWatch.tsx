aa0280318db96a6c5b510ed183069011dcadddc3
Daniel Tan <danieltan380@gmail.com>
2024-01-16 21:37:31 -0800

    Add placeholder numbers to build basic display.

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function StopWatch() {
     return(
-        <div></div>
+        <div className = "time-container">
+          <p className='timer-text'>0</p>
+          <span>:</span>
+          <p className='timer-text'>0</p>
+          <span>:</span>
+          <p className='timer-text'>0</p>
+        </div>
     )
 }
