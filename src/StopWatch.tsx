4ff076681cc717395001d147239cac70f2fd715f
Aditya Patel <adityapatel@Adityas-MacBook-Air.local>
2024-01-16 20:26:13 -0500

    [testing] added StopWatch component tests

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './styles/StopWatch.css'
 
 interface StopWatchProps{
-    timeString: string
+    timeString?: string
 }
 
 // Component to render the main stopwatch timer clock 
-export default function StopWatch({timeString}:StopWatchProps) {
+export default function StopWatch({timeString="00:00:00:00"}:StopWatchProps) {
     return(
-        <div className="stopWatchContainer">
+        <div className="stopWatchContainer" data-testid="stopWatchTest">
             {timeString}
         </div>
     )
