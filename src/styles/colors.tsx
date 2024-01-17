209306986c321fc7794b91387adf120f31734c88
Andrei Tekhtelev <a.tekhtelev@gmail.com>
2024-01-17 14:45:12 -0800

    Added style folder with colors and media-queries

---
+++ src/styles/colors.tsx
@@ -0,0 +1,7 @@
+// Food categories colors
+export const orange = '#D3752B';
+export const brown = '#E4DDCB';
+export const yellow = '#F3BC48';
+export const blue = '#2C5666';
+export const white = '#FFFFFF';
+export const green = 'green';
---
+++ src/styles/media-queries.tsx
@@ -0,0 +1,3 @@
+export const large = '@media (min-width: 1250px)';
+export const medium = '@media (min-width: 750px) and (max-width: 1249px)';
+export const small = '@media (max-width: 749px)';
