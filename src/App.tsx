d0f6c7cb90319356a64fd7cffe258edd2b709bc6
amg455 <amg455@cornell.edu>
2024-01-17 19:42:53 +0800

    got timer logic working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import StopWatchButton from './StopWatchButton'
-import StopWatch from './StopWatch'
+import React, { useState, useEffect } from 'react';
+import StopWatchButton from './StopWatchButton';
+import StopWatch from './StopWatch';
+
 export default function App() {
+    const [counting, setCounting] = useState(false);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        const updateTime = () => {
+            setTime((prevTime) => prevTime + 0.1);
+        };
+
+        if (counting) {
+            setTimeout(updateTime, 100)
+        }
+
+    }, [counting, time]);
+
+    const start = () => {
+        setCounting(true);
+    };
+
+    const stop = () => {
+        setCounting(false);
+    };
+
+
     return (
-        <div> 
-            <StopWatch time={0} /> 
-            <StopWatchButton title="Start" func={() => console.log('Start')} />
-            <StopWatchButton title="Stop" func={() => console.log('Stop')} />
-            <StopWatchButton title="Reset" func={() => console.log('Reset')} />
-            <StopWatchButton title="Lap" func={() => console.log('Lap')} />
+        <div>
+            <StopWatch time={time} />
+            <StopWatchButton title="Start" func={start} />
+            <StopWatchButton title="Stop" func={stop} />
+            {/* Assuming you'll add functionalities for these buttons later */}
+            <StopWatchButton title="Reset" func={() => { }} />
+            <StopWatchButton title="Lap" func={() => { }} />
         </div>
-    )
+    );
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 interface StopWatchProps {
-    time: number
+    time: number;
 }
 
 const StopWatch: React.FC<StopWatchProps> = ({ time }) => {
-    return (
-        <h1>{time}</h1>
-    );
+    return <h1>{time}</h1>;
 };
 
 export default StopWatch;
