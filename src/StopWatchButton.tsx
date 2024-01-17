ad3d23ffda366cff74945075a0ccc11dcd6d7ac1
Daniel Tan <danieltan380@gmail.com>
2024-01-16 23:05:39 -0800

    Add stopwatch buttons.

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function StopWatchButton() {
     return(
-        <div></div>
+        <div>
+          <button>Start</button>
+          <button>Stop</button>
+          <button>Resume</button>
+          <button>Lap</button>
+        </div>
     )
 }
