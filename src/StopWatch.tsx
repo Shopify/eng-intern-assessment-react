c7bafcbf3672f845319360adf292ca4df2848bae
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2024-01-16 23:49:19 -0500

    StopWatch Component. Displays the current running time

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
-}
+interface StopWatchProps {
+    minutes: string;
+    seconds: string;
+    milliseconds: string;
+  }
+  
+  /**
+     * This component takes in the appropriate stopwatch timer values and displays them
+     */
+  const StopWatch: React.FC<StopWatchProps> = ({ minutes, seconds, milliseconds}) => {
+    return (
+        <div className="timer-container">
+            <div className="timer-box">
+            <h1>{minutes}</h1>
+            </div>
+            <span className="colon">:</span>
+            <div className="timer-box">
+            <h1>{seconds}</h1>
+            </div>
+            <span className="colon">:</span>
+            <div className="timer-box">
+            <h1>{milliseconds}</h1>
+            </div>
+        </div>
+    );
+  };
+  
+  export default StopWatch;
