737fc2e530fc827531ae034d8ad0d76d6e2921fc
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2024-01-16 20:37:47 -0500

    Implemented logic for stopwatch timer/laps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,104 @@
-import React from 'react'
+/**
+ * @file App.tsx
+ * @desc React component to display the Stopwatch app
+ * @author Hadi Naqvi
+ */
 
+import React, { useState, useEffect, useRef } from 'react';
+import StopWatch from './StopWatch';
+import StopWatchButton from './StopWatchButton';
+import { formatTime } from './StopWatchUtils';
+import { containerStyle, buttonsContainerStyle, titleStyle } from './styles';
+
+
+/**
+ * App component to be rendered onto the index page
+ * @returns {JSX.Element} - React component that displays a stopwatch with various buttons to control it
+ */
 export default function App() {
+    const [isRunning, setRunning] = useState<boolean>(false);
+    const [startTime, setStartTime] = useState<number | null>(null);
+    const [elapsedTime, setElapsedTime] = useState<number>(0);
+    const [laps, setLaps] = useState<number>(0);
+    const animationFrameRef = useRef<number>();
+
+    // Stopwatch timer
+    useEffect(() => {
+        const animate = (timestamp: number) => {
+            if (isRunning && startTime) {
+                const newElapsedTime = elapsedTime + (timestamp - startTime);
+                // If the stopwatch time gets too high, it resets back to 0ms by default
+                elapsedTime === Number.MAX_SAFE_INTEGER ? setElapsedTime(0) : setElapsedTime(newElapsedTime);
+                setStartTime(timestamp);
+                animationFrameRef.current = requestAnimationFrame(animate);
+            }
+        };
+    
+        const startAnimation = () => {
+            if (!startTime) {
+                setStartTime(performance.now());
+            }
+            animationFrameRef.current = requestAnimationFrame(animate);
+        };
+    
+        if (isRunning) {
+            startAnimation();
+        } 
+        else {
+            cancelAnimationFrame(animationFrameRef.current as number);
+        }
+    
+        return () => {
+            cancelAnimationFrame(animationFrameRef.current as number);
+        };
+    }, [isRunning, startTime, elapsedTime]);
+
+    /**
+     * Function which starts and stops the stopwatch
+     */
+    const startStopTime = () => {
+        setRunning(!isRunning);
+    }
+
+    /**
+     * Function which resets the stopwatch time and lap counter
+     */
+    const resetStopWatch = () => {
+        setRunning(false);
+        setStartTime(null);
+        setElapsedTime(0);
+        setLaps(0);
+    }
+
+    /**
+     * Function which increments the lap counter
+     */
+    const countLap = () => {
+        // By default the lap count wraps back around to 0 if it gets too high
+        if (laps === Number.MAX_SAFE_INTEGER) {
+            setLaps(0);
+        }
+        else {
+            setLaps(laps + 1);
+        }
+    }
+
     return(
-        <div></div>
+        <>
+        {/* Removes default margin/padding of HTML pages */}
+        <style>{"body, html {margin: 0; padding: 0;"}</style>
+        
+        <div style={containerStyle}>
+            <h1 style={titleStyle}>Stopwatch</h1>
+            <StopWatch time={formatTime(elapsedTime)} laps={laps}/>
+
+            <div style={buttonsContainerStyle}>
+                <StopWatchButton colour={isRunning ? "red" : "green"} name={isRunning ? "Stop" : "Start"} onClick={startStopTime}/>
+                <StopWatchButton colour={"blue"} name={"Reset"} onClick={resetStopWatch}/>
+                <StopWatchButton colour={"blue"} name={"Lap"} onClick={countLap}/>
+            </div>
+
+        </div>
+        </>
     )
 }
---
+++ src/StopWatchUtils.ts
@@ -0,0 +1,32 @@
+/**
+ * @file StopWatchUtils.ts
+ * @desc Utility functions such as time formatters for a stopwatch
+ * @author Hadi Naqvi
+ */
+
+/**
+ * Formats the elapsed time of a stopwatch into a string format of HH:MM:ss.mm
+ * @param {number} elapsedTime - The elapsed time in milliseconds of a stopwatch timer
+ * @returns {string} - A formatted string representing the elapsed time in the format HH:MM:SS.mm
+ * 
+ * @example
+ * // Usage example:
+ * const elapsedTime = 3665500; // 1 hour, 1 minute, 5.5 seconds
+ * const formattedTime = formatTime(elapsedTime);
+ * console.log(formattedTime); // Output: "01:01:05.55"
+ */
+export const formatTime = (elapsedTime: number): string => {
+    // Calculates the number of hours, minutes, seconds, and milliseconds (First 2 digits) passed
+    const hours = Math.floor(elapsedTime / 3600000);
+    const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+    const seconds = Math.floor((elapsedTime % 60000) / 1000);
+    const truncatedMilliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+    // Formats and returns the time in HH:MM:SS.mm
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    const formattedMilliseconds = String(truncatedMilliseconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
+}
