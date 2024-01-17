49ba6bf97531b4c82732e95d4a4b6b08e8523183
Jason Huang <jasonhuang20035@gmail.com>
2024-01-17 23:00:28 +0000

    feat: add tailwind for styling

--- webpack.common.js
+++ webpack.common.js
@@ -30,7 +30,16 @@ module.exports = {
         }
     },{
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          'postcss-loader'
+        ]
     },{
       test: /\.(png|jpe?g|gif|jp2|webp)$/,
       loader: 'file-loader',
