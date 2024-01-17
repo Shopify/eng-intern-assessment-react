1af86ac417096b31d27c1661c48be8d262a5e3d0
Daniel Tan <danieltan380@gmail.com>
2024-01-16 23:00:12 -0800

    Import StopWatchButton to App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../styles/App.css'
 import StopWatch from './StopWatch'
+import StopWatchButton from './StopWatchButton'
 import '../styles/StopWatch.css'
 
 export default function App() {
     return(
         <div>
           <StopWatch/>
+          <StopWatchButton/>
         </div>
     )
 }
