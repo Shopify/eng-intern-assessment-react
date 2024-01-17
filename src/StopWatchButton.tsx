80075124047f2774c409caea4ec7e080f3185493
hadi-naqvi <92334527+hadi-naqvi@users.noreply.github.com>
2024-01-16 20:37:20 -0500

    Created custom component for stopwatch buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+/**
+ * @file StopWatchButton.tsx
+ * @desc Custom React component for a stopwatch button
+ * @author Hadi Naqvi
+ */
 
-export default function StopWatchButton() {
+import React, { useState } from 'react'
+import { buttonStyle } from './styles'
+
+/**
+ * StopWatchButtonProps interface that defines expected props for the StopWatchButton component
+ * @interface StopWatchButtonProps
+ * @property {string} time - The name of the button
+ * @property {function} onclick - The callback function executed upon clicking the button
+ * @property {colour} colour - The colour of the button
+ */
+interface StopWatchButtonProps {
+    name: string;
+    onClick: () => void;
+    colour: string;
+}
+
+/**
+ * StopWatchButton component, a control button for a stopwatch
+ * @param {string} name - The name of the button
+ * @param {function} laps - The callback function executed upon clicking the button
+ * @param {colour} - The colour of the button
+ * @returns {JSX.Element} - React component representing a stopwatch button
+ */
+export default function StopWatchButton({name, onClick, colour}: StopWatchButtonProps) {
     return(
-        <div></div>
+        <div>
+            <button onClick={onClick} style={buttonStyle(colour)}>
+                {name}
+            </button>
+        </div>
     )
 }
