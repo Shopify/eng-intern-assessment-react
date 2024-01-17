ff68867063336604c3d7caf97b4f077a81feb369
Shawn Thomas <sljt@proton.me>
2024-01-17 18:45:02 -0500

    update test to capture StopWatch on initial render

--- __tests__/StopWatch.test.js
+++ __tests__/StopWatch.test.js
@@ -7,16 +7,30 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { toBeInTheDocument } from '@testing-library/jest-dom';
 import StopWatch from '../src/StopWatch.tsx';
 
-// Create snapshot to compare new output against previously saved snap
-describe("snapshot test for stability", () => {
-  it("should render the initial StopWatch component correctly", () => {
 
-    const { container } = render(<StopWatch />);
-    expect(container).toMatchSnapshot();
+describe("StopWatch functionality", () => {
+
+  it("should render the initial StopWatch component correctly", () => {
+    render(<StopWatch />)
 
     // Check if initial time is "00:00:00".
     expect(screen.getByText("00:00:00")).toBeInTheDocument();
+
+    // Check if Start and Reset buttons are present on initial render
+    expect(screen.getByText("Start")).toBeInTheDocument();
+    expect(screen.getByText("Reset")).toBeInTheDocument();
+
+    // Check if Lap and Pause buttons are not present on initial render when stopwatch is inactive
+    expect(screen.queryByTestId("stopwatch-lap")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("stopwatch-pause")).not.toBeInTheDocument();
+
+    // Check if lap list is not present on initial render when stopwatch is inactive
+    expect(screen.queryByTestId("stopwatch-lap-list")).not.toBeInTheDocument();
   });
+  //end
+
+
 });
-//end
+
+
 
