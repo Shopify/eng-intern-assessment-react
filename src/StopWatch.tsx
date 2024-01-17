df1a10e5e1dbace891ce0e8a551195eaf9fec200
Sagar Saha <sagarsaha2004@gmail.com>
2024-01-16 21:13:45 -0500

    implemented basic clock functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
+    const [timeInSeconds, setTimeInSeconds] = useState(0);
+    const [timeArray, setTimeArray] = useState<Array<number|string>>([]);
+    const [timeLaps, setTimeLaps] = useState<Array<string>>([])
+    const [laps, setLaps] = useState<Array<number|string>>([]);
+
+    function calculateTimeInSeconds(timeInSeconds: number): (number | string)[] {
+        let hours:number = Math.floor(timeInSeconds / 3600);
+        let minutes:number = Math.floor((timeInSeconds - (hours * 3600)) / 60);
+        let seconds:number = timeInSeconds - (hours * 3600) - (minutes * 60);
+        
+        return [
+            hours < 10 ? `0${hours}` : hours,
+            minutes < 10 ? `0${minutes}` : minutes,
+            seconds < 10 ? `0${seconds}` : seconds
+        ];
+    }
+
+    function here() {
+        if (timeArray[0] !=  "00"|| timeArray[1] != "00" || timeArray[2] != "00") {
+            console.log(timeLaps);
+            let x = timeArray[0] + ":" + timeArray[1] + ":" +  timeArray[2]
+            setTimeLaps([...timeLaps, x])
+        }
+    }
+
+    function clear() {
+        setTimeLaps([])
+    }
+
+    useEffect(() => {
+        setTimeArray(calculateTimeInSeconds(timeInSeconds));
+    }, [timeInSeconds]);
     return(
-        <div></div>
+        <div>
+            <main className="stopwatch-container">
+            <section className="timer-display">
+                <p id="hour">{timeArray[0]}</p>
+                <span>:</span>
+                <p id="minute">{timeArray[1]}</p>
+                <span>:</span>
+                <p id="second">{timeArray[2]}</p>
+            </section>
+            <StopWatchButton setTimeInSeconds={setTimeInSeconds}  setLap={here} clearLap={clear}/>
+            <p>{timeLaps}</p>
+        </main>
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function StopWatchButton() {
+type Props = {
+    setTimeInSeconds: Function,
+    setLap: Function,
+    clearLap: Function
+}
+
+
+export default function StopWatchButton(props:Props) {
+    const { setTimeInSeconds } = props;
+    const { setLap } = props;
+    const [intervalId, setIntervalId] = useState<number>(0);
+    const [currentState, setCurrentState] = useState("Start");
+
+    const handleStartPauseButton = () => {
+        if (currentState == "Start") {
+            setCurrentState("Pause")
+            handlePlayButton(Object);
+        }
+        else if (currentState === "Pause"){
+            setCurrentState("Start")
+            handleStopButton();
+        }
+
+    }
+
+    const handleLap=(e:object) => {
+        props.setLap()
+    }
+
+    const handlePlayButton = (e: object) => {
+        const interval:any = setInterval(() => {
+            setTimeInSeconds((previousState:number) => previousState + 1);
+        }, 1000);
+
+        setIntervalId(interval);
+    }
+
+    const handleStopButton = () => {
+        clearInterval(intervalId);
+    }
+
+    const handleReset = () => {
+        clearInterval(intervalId);
+        setTimeInSeconds(0);
+        setCurrentState("Start");
+        props.clearLap();
+    }
+
     return(
-        <div></div>
-    )
+        <div className="stopwatch-controls-container">
+            <button onClick={handleStartPauseButton} type="button">{currentState}</button>
+            <button onClick={handleReset} type="button">Reset</button>
+            <button onClick={handleLap} type="button">Laps</button>
+        </div>
+    );
 }
