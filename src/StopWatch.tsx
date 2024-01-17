b71464ffc6fe1614df195660a5745490ae982e5b
Tudor Barsan <tudor.barsan46@gmail.com>
2024-01-16 19:37:25 -0500

    Timer functionality + change font

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import StopWatchButton from './StopWatchButton'
 
 
 const StopWatch: React.FC = () => {
+    const [time, setTime] = useState(0);  // State for time
+    const [isOn, setIsOn] = useState(false);  // State for on/off
+
+    // Function to toggle the timer
+    const toggle = () => {
+        setIsOn(!isOn);
+    }
+
+    // Function to reset the timer
+    const reset = () => {
+        setTime(0);
+        setIsOn(false);
+    }
+
+    // Increments the timer every 10ms
+    useEffect(() => {
+        let interval: any = null;
+        if (isOn) {  // Check if the timer is on
+            interval = setInterval(() => {  // Interval that runs every 10ms
+                setTime((time) => time + 10);  // Incrementing the time by 10ms
+            }, 10);
+        } else if (!isOn && time !== 0) { // Check if the timer is off and the time is not 0 
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);  // Clear the interval
+    });
+
+    // Converting the time into hours, minutes, seconds, milliseconds
+    const hours = Math.floor((time / 3600000) % 24);
+    const minutes = Math.floor((time / 60000) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    const milliseconds = Math.floor((time / 10) % 100);
+
+
     return(
         <StopWatchContainerParent>
             <StopWatchContainer>
                 <StopWatchTimerContainer>
-                    00:00:00
+                    {hours.toString().padStart(2, "0")}
+                    :{minutes.toString().padStart(2, "0")}
+                    :{seconds.toString().padStart(2, "0")}
+                    :{milliseconds.toString().padStart(2, "0")}
                 </StopWatchTimerContainer>
                 <StopWatchButtonContainer>
-                    <StopWatchButton label="Start" color="#0EC761" onClick={() => console.log("Start")} />
-                    <StopWatchButton label="Stop" color="#E30039" onClick={() => console.log("Stop")} />
-                    <StopWatchButton label="Reset" color="#E7A92E" onClick={() => console.log("Reset")} />
+                    <StopWatchButton label="Start" color="#0EC761" onClick={toggle} />
+                    <StopWatchButton label="Stop" color="#E30039" onClick={toggle} />
+                    <StopWatchButton label="Reset" color="#E7A92E" onClick={reset} />
                     <StopWatchButton label="Lap" color="#A43AE5" onClick={() => console.log("Lap")} />
                 </StopWatchButtonContainer>
             </StopWatchContainer>  
@@ -61,9 +98,9 @@ const StopWatchTimerContainer = styled.div`
     justify-content: center;
     align-items: center;
     
-    font-size: 180px;
+    font-size: 200px;
     font-style: normal;
-    font-family: 'Kadwa', serif;
+    font-family: 'Courier New', Courier, monospace;
     font-weight: 400;
     line-height: normal;
 `;
