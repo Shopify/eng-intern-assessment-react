e99fb8d81bd268c06eba007fab7d875ab226af51
Joseph Lee <josephlee3004@gmail.com>
2024-01-17 14:31:41 -0500

    Start stopwatch when start button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import StopWatch from './StopWatch'
+import StopWatchButton from './StopWatchButton'
 
 export default function App() {
+    const [counter, setCounter] = useState(0);
+
+    function startStopWatch() {
+        setInterval(() => {
+            setCounter(counter => counter + 1);
+        }, 1000)
+    }
+
+    const handleButtonClick = () => {
+        console.log("clicked");
+        startStopWatch();
+    }
+
     return(
         <div>
-            <StopWatch/>
+            <StopWatch seconds={counter}/>
+            <StopWatchButton text="Start" handleClick={handleButtonClick}/>
         </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,23 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
-export default function StopWatch() {
-    const [counter, setCounter] = useState(0);
-
-    // Empty dependency array because we don't want useEffect to
-    // run anytime other than at initial component render
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setCounter(counter => counter + 1);
-        }, 1000)
-
-        return () => {
-            clearInterval(intervalID);
-        }
-    }, [])
-
+export default function StopWatch({ seconds }: {seconds: number}) {
     return(
         <>
-            <p>{counter}s</p>
+            <p>{seconds}s</p>
         </>
     )
 }
