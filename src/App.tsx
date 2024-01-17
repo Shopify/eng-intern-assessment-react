d3c66af0e66059bfec5415b31d964a1ee904503b
Joseph Lee <josephlee3004@gmail.com>
2024-01-17 15:19:29 -0500

    Implement stop button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import StopWatch from './StopWatch'
 import StopWatchButton from './StopWatchButton'
 
 export default function App() {
+    let intervalID: ReturnType<typeof setInterval>;
+
     const [counter, setCounter] = useState(0);
+    const [counting, setCounting] = useState(false);
 
-    function startStopWatch() {
-        setInterval(() => {
-            setCounter(counter => counter + 1);
-        }, 1000)
-    }
+    useEffect(() => {
+        if (counting) {
+            intervalID = setInterval(() => {
+                setCounter(counter => counter + 1);
+            }, 1000)
+        } else {
+            clearInterval(intervalID);
+        }
 
-    const handleButtonClick = () => {
-        console.log("clicked");
-        startStopWatch();
-    }
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, [counting])
 
     return(
         <div>
             <StopWatch seconds={counter}/>
-            <StopWatchButton text="Start" handleClick={handleButtonClick}/>
+            <StopWatchButton text="Start" handleClick={() => {
+                setCounting(true);
+            }}/>
+            <StopWatchButton text="Stop" handleClick={() => {
+                setCounting(false);
+            }}/>
         </div>
     )
 }
