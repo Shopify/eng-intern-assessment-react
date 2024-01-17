67bfff9df8412e50ddbad5ba28b281c60f5d28f5
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2024-01-17 04:06:52 -0500

    moved test file to __tests__

--- __tests__/Stopwatch.test.js
+++ __tests__/Stopwatch.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
 import '@testing-library/jest-dom'
 
 jest.useFakeTimers();
