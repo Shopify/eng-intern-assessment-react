eb31910319b78e2749cbeb1160e5326c460c7aec
Shawn Thomas <sljt@proton.me>
2024-01-16 20:35:34 -0500

    update StopWatch and StopWatchButton component with functionality and Typescript types

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,143 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import StopWatchButton from "./StopWatchButton";
+import formatWithLeadingZero from "./helpers/formatWithLeadingZero";
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+interface TimeState {
+  milliseconds: number;
+  seconds: number;
+  minutes: number;
+}
+
+/**
+ * StopWatch component that allows users to measure time and record laps.
+ *
+ * App -> StopWatch -> StopWatchButton
+ */
+
+export default function StopWatch(): JSX.Element {
+  const [time, setTime] = useState<TimeState>({
+    milliseconds: 0,
+    seconds: 0,
+    minutes: 0,
+  });
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [laps, setLaps] = useState<string[]>([]);
+
+
+  /**
+   * This effect is responsible for updating the timer values (minutes, seconds,
+   * and milliseconds) when the stopwatch is active.
+   *
+   * @dependency
+   * - isActive: determines whether the stopwatch time is incrementing.
+   */
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((prevTime) => {
+          let newMilliseconds = prevTime.milliseconds + 1;
+          let newSeconds = prevTime.seconds;
+          let newMinutes = prevTime.minutes;
+
+          if (newMilliseconds >= 99) {
+            newSeconds += 1;
+            newMilliseconds = 0;
+
+            if (newSeconds >= 60) {
+              newMinutes += 1;
+              newSeconds = 0;
+            }
+          }
+
+          return {
+            minutes: newMinutes,
+            seconds: newSeconds,
+            milliseconds: newMilliseconds,
+          };
+        });
+      }, 10);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+
+  /**
+   * Handles the start or stop control of the stopwatch.
+   */
+
+  const handleStartStop = (): void => {
+    setIsActive(!isActive);
+    console.log("Timer is active: ", !isActive);
+  };
+
+
+  /**
+   * Handles the reset control of the stopwatch.
+   * Reset the time state, deactivate the stopwatch and clear all recorded laps.
+   */
+
+  const handleReset = (): void => {
+    setTime({ minutes: 0, seconds: 0, milliseconds: 0 });
+    setIsActive(false);
+    setLaps([]);
+  };
+
+  /**
+   * Handles recording a lap in the stopwatch.
+   */
+
+  const handleLap = (): void => {
+    // format the current minute, second, ms into a string with leading zeros.
+    // ex. { minute: 0, second: 7, millisecond: 11 } => "00: 07: 11"
+    const lapTime: string = `
+            ${formatWithLeadingZero(time.minutes)}:
+            ${formatWithLeadingZero(time.seconds)}:
+            ${formatWithLeadingZero(time.milliseconds)}
+        `;
+
+    // update the laps state by pushing the formatted lapTime to the array
+    setLaps((prevLaps) => [...prevLaps, lapTime]);
+  };
+
+  return (
+    <div className="stopwatch-container">
+      <div className="stopwatch-timer">
+        {formatWithLeadingZero(time.minutes)}:
+        {formatWithLeadingZero(time.seconds)}:
+        {formatWithLeadingZero(time.milliseconds)}
+      </div>
+
+      <div className="stopwatch-buttons">
+        <StopWatchButton
+          onClick={handleStartStop}
+          id={isActive ? "btn-pause" : "btn-start"}
+          label={isActive ? "Pause" : "Start"}
+        />
+        <StopWatchButton onClick={handleLap} id={"btn-lap"} label="Lap" />
+        <StopWatchButton onClick={handleReset} id={"btn-reset"} label="Reset" />
+      </div>
+
+      <div
+        className="stopwatch-laps"
+        style={{ display: laps.length ? "block" : "none" }}
+      >
+        <h3>
+          <span>Lap</span>
+          <span>Time</span>
+        </h3>
+        <ul>
+          {laps.map((lap, index) => (
+            <li key={index}>
+              <span>{`Lap ${index + 1}`}</span>
+              <span>{lap}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { MouseEvent } from "react";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+/**
+ * Props for the StopWatchButton component.
+ */
+
+interface StopWatchButtonProps {
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  label: string;
+  id: string;
+}
+
+/**
+ * A button component for controlling a stopwatch.
+ *
+ * @param {Function} props.onClick - the function to be called when the button is clicked.
+ * @param {string} props.label - the label/text displayed on the button.
+ * @param {string} props.id - the ID attribute for the button.
+ *
+ * StopWatch -> StopWatchButton
+ */
+
+export default function StopWatchButton({
+  onClick,
+  label,
+  id,
+}: StopWatchButtonProps): JSX.Element {
+  return (
+    <button className="stowatch-button" id={id} onClick={onClick}>
+      {label}
+    </button>
+  );
 }
