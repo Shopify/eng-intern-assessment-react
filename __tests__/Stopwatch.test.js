eaab8bea51f1077d607347388e724bd09baf3573
James Shi <jamesshi117@gmail.com>
2024-01-17 13:08:54 -0800

    add jest pkgs and setup test file

---
+++ __tests__/Stopwatch.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+
+describe('Stopwatch Application', () => {
+  test('renders initial state correctly', () => {});
+
+  test('starts and stops the stopwatch', async () => {});
+
+  test('pauses and resumes the stopwatch', async () => {});
+});
