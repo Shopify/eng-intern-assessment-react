e6ae85731ed66984c78ffbcd73b7480f452ec15e
Mitchell Wagner <pim128@usask.ca>
2024-01-16 20:30:25 -0600

    Removed un needed test scenario

--- src/StopWatch.test.tsx
+++ src/StopWatch.test.tsx
@@ -9,7 +9,6 @@ import StopWatch from './StopWatch';
  * stop watch gets displayed, and also the time is displayed.
  * 
  * 1. Verify that there is the text on the display
- * 2. Verify that the time is on the display
  * 
  * For greater test coverage I would need to figure out how to
  * use this test suite more. I would want to verify that the
@@ -25,12 +24,4 @@ test('verify that the text is on the display', () => {
 
     const timerText = getByText("Current Time(Hours, Minutes, Seconds):");
     expect(timerText).toBeDefined();
-});
-
-test('verify that the time is on the display', () => {
-
-    const { getByText } = render(<StopWatch laps={[]} time={moment.duration(0)} />);
-
-    const timeText = getByText("0:0:0");
-    expect(timeText).toBeDefined();
 });
