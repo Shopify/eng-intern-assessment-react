e532bc1c4bd242313b83f2c600b37c5ab1411acf
Tudor Barsan <tudor.barsan46@gmail.com>
2024-01-16 20:21:18 -0500

    Lap functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -6,16 +6,28 @@ import StopWatchButton from './StopWatchButton'
 const StopWatch: React.FC = () => {
     const [time, setTime] = useState(0);  // State for time
     const [isOn, setIsOn] = useState(false);  // State for on/off
+    const [lapTime, setLapTime] = useState<number[]>([]) // State for laps
 
-    // Function to toggle the timer
-    const toggle = () => {
-        setIsOn(!isOn);
+    // Function to start the timer
+    const setOn = () => {
+        setIsOn(true);
+    }
+
+    // Function to stop the timer
+    const setOff = () => {
+        setIsOn(false);
     }
 
     // Function to reset the timer
     const reset = () => {
         setTime(0);
         setIsOn(false);
+        setLapTime([]);
+    }
+
+    // Function to add lap time
+    const addLap = () => {
+        setLapTime([...lapTime, time]);
     }
 
     // Increments the timer every 10ms
@@ -48,11 +60,18 @@ const StopWatch: React.FC = () => {
                     :{milliseconds.toString().padStart(2, "0")}
                 </StopWatchTimerContainer>
                 <StopWatchButtonContainer>
-                    <StopWatchButton label="Start" color="#0EC761" onClick={toggle} />
-                    <StopWatchButton label="Stop" color="#E30039" onClick={toggle} />
+                    <StopWatchButton label="Start" color="#0EC761" onClick={setOn} />
+                    <StopWatchButton label="Stop" color="#E30039" onClick={setOff} />
                     <StopWatchButton label="Reset" color="#E7A92E" onClick={reset} />
-                    <StopWatchButton label="Lap" color="#A43AE5" onClick={() => console.log("Lap")} />
+                    <StopWatchButton label="Lap" color="#A43AE5" onClick={addLap} />
                 </StopWatchButtonContainer>
+                <StopWatchLapContainer>
+                    {lapTime.map((lap, index) => (  // Map over the laps and display them 
+                        <StopWatchLapLabel key={index}>
+                            {lap}
+                        </StopWatchLapLabel>
+                    ))}
+                </StopWatchLapContainer>
             </StopWatchContainer>  
         </StopWatchContainerParent>
     )
@@ -63,13 +82,14 @@ const StopWatch: React.FC = () => {
 const StopWatchContainerParent = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
     height: 100vh;
 `;
 
 // Styled container to hold the StopWatch
 const StopWatchContainer = styled.div`
     width: 80vw;
-    height: 55vh;
+    height: 96vh;
     background: #b8b7b7;
     border-radius: 30px;
     box-shadow: 0px 6px 6px 0px rgba(0, 0, 0, 0.30) inset;
@@ -113,6 +133,27 @@ const StopWatchButtonContainer = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
+`;
+
+const StopWatchLapContainer = styled.div`
+    width: 90%;
+    height: 90%;
+    margin: 0 0 3vh 0;
+    background: #b8b7b7;
+    border-radius: 20px;
+    box-shadow: 8px 8px 8px 8px rgba(0, 0, 0, 0.30) inset;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const StopWatchLapLabel = styled.div`
+    width: 25%;
+    height: 10%;
+    margin: 2vh 2wh;
+    background: linear-gradient(0deg, rgba(200,200,200,1) 0%, rgba(250,250,250,1) 50%, rgba(200,200,200,1) 100%);
+    border-radius: 20px;
 `;
 
 
