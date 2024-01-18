b85231521bae44c98dc39b8d6cc409c8a982b910
Deep <deep52p@gmail.com>
2024-01-18 10:28:53 -0500

    add barebone timer ui

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import './StopWatch.css'
+
 export default function StopWatch() {
     return(
-        <div></div>
+        <div className='timer'>
+            <p className='timer-text'>1</p>
+            <p className='timer-colon'>:</p>
+            <p className='timer-text'>2</p>
+            <p className='timer-colon'>:</p>
+            <p className='timer-text'>3</p>
+        </div>
     )
 }
