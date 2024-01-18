28846158ae6637635c77a39e64d34b5fecbce94f
davidkochanski <kochanskidavid9@gmail.com>
2024-01-17 20:25:16 -0500

    Finishing touches, restyle laps UI, make responsive, optimize rendering && clock interval

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -7,7 +7,27 @@ interface StopWatchButtonProps {
 }
 
 export default function StopWatchButton({ onPress, label, isPressed }: StopWatchButtonProps) {
+
+    /**
+     * Attach an appropriate className to this button based on params.
+     */
+    const putClassName = () => {
+        // I originally did this as a long terinary opperator
+        // but it was a bit messy so I extracted it into its own function.
+        let classes = ["stopwatch-button"];
+
+        classes.push(`stopwatch-${label.toLowerCase()}`);  // Each button's ID for styling
+
+        if(isPressed) classes.push("stopwatch-button-pressed");
+
+        return classes.join(" ");
+    }
+
     return(
-        <button type="button" className={`stopwatch-button stopwatch-${label.toLowerCase()} ${isPressed ? "stopwatch-button-pressed" : ""}`} onClick={onPress}>{label}</button>
+        <button 
+            type="button" 
+            className={putClassName()} 
+            onClick={onPress}>{label}
+        </button>
     );
 }
