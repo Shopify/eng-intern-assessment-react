31244758c1608955d1fe1a60c01348407a1eb9e8
Nate Card <ncard24@gmail.com>
2024-01-18 11:25:55 -0500

    Moved stopwatch logic to App per instructions, kept formatting and rendering logic in StopWatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import StopWatch from "./StopWatch";
 import "../src/styles/styles.css";
+import StopWatchButton from "./StopWatchButton";
 
 export default function App() {
+  const [timerRunning, setTimerRunning] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+  const [laps, setLaps] = useState<number>(0);
+  const [lapTimeArray, setLapTimeArray] = useState<number[]>([]);
+
+  useEffect(() => {
+    let interval: number | NodeJS.Timeout = null;
+    // Check if timer is running
+    if (timerRunning) {
+      // Interval updates every 10ms
+      interval = setInterval(() => {
+        // Increment the time state every 10ms
+        setTime((time) => time + 10);
+      }, 10);
+    }
+    // If timer is not running and time is not at 0
+    else if (!timerRunning && time !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+    // this useEffect will run when the timerRunning state changes
+  }, [timerRunning]);
+
+  function handleStartOrPause() {
+    if (!timerRunning) {
+      setTimerRunning(true);
+    } else if (timerRunning) setTimerRunning(false);
+  }
+  function handleLap() {}
+
   return (
-    <div>
-      <StopWatch />
+    <div id="stopwatch">
+      <StopWatch time={time} />
+      <div id="button-group">
+        <StopWatchButton
+          onClick={() => handleStartOrPause()}
+          title={timerRunning ? "Pause" : "Start"}
+        />
+        <StopWatchButton onClick={() => handleLap()} title={"Lap"} />
+      </div>
     </div>
   );
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,45 +1,24 @@
 import React, { useEffect, useState } from "react";
-import StopWatchButton from "./StopWatchButton";
 
-export default function StopWatch() {
-  const [timerRunning, setTimerRunning] = useState<boolean>(false);
-  const [time, setTime] = useState<number>(0);
-  const [laps, setLaps] = useState<number>(0);
-  const [lapTimeArray, setLapTimeArray] = useState<number[]>([]);
+interface stopWatchProps {
+  time: number;
+}
 
-  useEffect(() => {
-    let interval = null;
-    // Check if timer is running
-    if (timerRunning) {
-      // Interval updates every 10ms
-      interval = setInterval(() => {
-        // Increment the time state every 10ms
-        setTime((prev) => prev + 10);
-      }, 10);
-    }
-    // If timer is not running and time is not at 0
-    else if (!timerRunning && time !== 0) {
-      clearInterval(interval);
-    }
-    return clearInterval(interval);
-    // this useEffect will run when the timerRunning state changes
-  }, [timerRunning]);
+export default function StopWatch(props: stopWatchProps) {
   // converts number to a string and pads with '0' if single digit number
   function padNumbers(num: number) {
     return num.toString().padStart(2, "0");
   }
   // convert each value to correct value, format and pads number
   function formatNumbers(time: number) {
-    const unFormattedHours = Math.floor((time / 3600000) % 24);
     const unFormattedMinutes = Math.floor((time / 60000) % 60);
     const unFormattedSeconds = Math.floor((time / 1000) % 60);
     const unFormattedMilliseconds = Math.floor((time / 10) % 100);
-    const hours = padNumbers(unFormattedHours);
+
     const minutes = padNumbers(unFormattedMinutes);
     const seconds = padNumbers(unFormattedSeconds);
     const milliseconds = padNumbers(unFormattedMilliseconds);
-    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    return `${minutes}:${seconds}:${milliseconds}`;
   }
-
-  return <div id="stopwatch">{formatNumbers(time)}</div>;
+  return <div className="stopwatch">{formatNumbers(props.time)}</div>;
 }
