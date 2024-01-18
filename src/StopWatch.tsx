159bc6ef398e313aa40002a4abc2428dfba541dc
Nate Card <ncard24@gmail.com>
2024-01-17 21:33:27 -0500

    Add timer functionality to StopWatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [timerRunning, setTimerRunning] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+
+  useEffect(() => {
+    let interval = null;
+    // Check if timer is running
+    if (timerRunning) {
+      // Interval updates every 10ms
+      interval = setInterval(() => {
+        // Increment the time state every 10ms
+        setTime((prev) => prev + 10);
+      }, 10);
+    }
+    // If timer is not running and time is not at 0
+    else if (!timerRunning && time !== 0) {
+      clearInterval(interval);
+    }
+    return clearInterval(interval);
+    // useEffect will run when the timerRunning state changes
+  }, [timerRunning]);
+
+  return <div id="stopwatch"></div>;
 }
