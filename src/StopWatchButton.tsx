399589bce11ffc1562a5c525d6af1d0657694fc8
Daniel Tan <danieltan380@gmail.com>
2024-01-17 23:16:06 -0800

    Implement logic for play, stop, and resume buttons. Import useState and useEffect.

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/StopWatchButton.css'
 
 type Props = {
@@ -6,12 +6,46 @@ type Props = {
 };
 
 export default function StopWatchButton(props:Props) {
+  const { setTimeInSeconds } = props;
+  const [intervalId, setIntervalId] = useState<number>(0);
+  const [timerOn, setTimerOn] = useState<boolean>(false);
+
+  useEffect(() => {
+    let interval:any = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTimeInSeconds((prevState:number) => prevState + 10)
+      }, 10)
+    } else {
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+
+  }, [timerOn])
+
+  const handlePlayButton = () => {
+    setTimerOn(true);
+  }
+
+  const handleStopButton = () => {
+    setTimerOn(false);
+  }
+
+  const handleResumeButton = () => {
+    setTimerOn(true);
+  }
+
+  const handleLapButton = () => {
+
+  }
     return(
         <div className="controls">
-          <button>Start</button>
-          <button>Stop</button>
-          <button>Resume</button>
-          <button>Lap</button>
+          <button onClick={handlePlayButton}>Start</button>
+          <button onClick={handleStopButton}>Stop</button>
+          <button onClick={handleResumeButton}>Resume</button>
+          <button onClick={handleLapButton}>Lap</button>
         </div>
     )
 }
