ae85ef29557324eac679676e6a85579abc9e9bc1
Deep <deep52p@gmail.com>
2024-01-18 11:37:13 -0500

    logic for timer and start/stop/reset btn functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,23 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './StopWatch.css'
 import StopWatchButton from './StopWatchButton'
 
+const calculateTime = (time: number) => {
+    // Minutes calculation
+    const minutes = Math.floor((time % 360000) / 6000);
+
+    // Seconds calculation
+    const seconds = Math.floor((time % 6000) / 100);
+
+    // Milliseconds calculation
+    const milliseconds = time % 100;
+
+    return [minutes.toString().padStart(2, "0"), seconds.toString().padStart(2, "0"), milliseconds.toString().padStart(2, "0")]
+}
+
 export default function StopWatch() {
+    const [time, setTime] = useState<number>(0);
+    const [timerData, setTimerData] = useState<string[]>([]);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+
+    useEffect(() => {
+        const timerInfo = calculateTime(time)
+        setTimerData(timerInfo)
+
+        let timeInterval: NodeJS.Timeout;
+        if (isRunning) {
+            timeInterval = setInterval(() => setTime(time + 1), 10)
+        }
+
+        return () => clearInterval(timeInterval)
+    }, [time, isRunning])
+
+    const startStopTime = () => {
+        setIsRunning(!isRunning)
+    }
+
+    const resetTime = () => {
+        setTime(0)
+    }
+
     return (
         <div>
             <div className='timer'>
-                <p className='timer-text'>00</p>
+                <p className='timer-text'>{timerData[0]}</p>
                 <p className='timer-colon'>:</p>
-                <p className='timer-text'>00</p>
+                <p className='timer-text'>{timerData[1]}</p>
                 <p className='timer-colon'>:</p>
-                <p className='timer-text'>00</p>
+                <p className='timer-text'>{timerData[2]}</p>
             </div>
             <div className='buttons-container'>
-                <StopWatchButton name='Start' />
-                <StopWatchButton name='Stop' />
-                <StopWatchButton name='Reset' />
-                <StopWatchButton name='Lap' />
+                <StopWatchButton name={!isRunning ? 'Start' : 'Stop'} handleClick={startStopTime}/>
+                <StopWatchButton name='Reset' handleClick={resetTime}/>
+                <StopWatchButton name='Lap' handleClick={startStopTime}/>
             </div>
         </div>
     )
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 
 import './StopWatchButton.css'
 
 
 type Props = {
     name: string
+    handleClick: MouseEventHandler<HTMLButtonElement>
 }
 
 export default function StopWatchButton(props: Props) {
     return (
-        <button className='stopwatch-button'>{props.name}</button>
+        <button className='stopwatch-button' onClick={props.handleClick}>{props.name}</button>
     )
 }
