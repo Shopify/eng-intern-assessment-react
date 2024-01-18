57ffd9faff8d43c60b5db2ff5b5f5a51a0ba4845
jessicaxu0605 <jessicaxu0605@gmail.com>
2024-01-17 22:29:51 -0500

    add timing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import StopWatch from './StopWatch';
+
+type Time = {
+    hundreths: number;
+    seconds: number;
+    minutes: number;
+}
 
 export default function App() {
+    const [time, setTime] = React.useState<number>(0);
+
+    React.useEffect(()=>{
+        let interval: NodeJS.Timeout;
+
+        interval = setInterval(()=>{
+            setTime((time)=> time + 10);
+        }, 10);
+        console.log(time);
+
+    })
+
     return(
-        <div></div>
+        //give Stopwatch props: hundreths seconds minutes
+        <div>
+            <StopWatch 
+            hundreths={Math.floor((time/10)%100)} 
+            seconds={Math.floor(((time/1000)%60))} 
+            minutes={Math.floor(time/60000)}/>
+        </div>
     )
 }
