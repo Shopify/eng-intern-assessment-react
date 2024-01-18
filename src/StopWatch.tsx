4b8abfb23c31883d6d087064b432f62642d6c73d
Sowad <sowadkhan912@gmail.com>
2024-01-18 14:12:22 -0500

    Added laps

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -10,11 +10,15 @@ export default function StopWatch() {
 
     useEffect(() => {
         let interval: ReturnType<typeof setInterval>
+
+        // Update time every 10 miliseconds
         if (isRunning) {
             interval = setInterval(() => {
                 setElapsedTime(Date.now() - startTime.current)
             }, 10)
-        } 
+        }
+        
+        // Clear interval when stopwatch is stopped
         return () => {
             clearInterval(interval)
         }
@@ -35,28 +39,37 @@ export default function StopWatch() {
         
     }
 
-    const handleLap = () => {
-        
+    const handleLaps = () => {
+        setLaps(prevLaps => [...prevLaps, elapsedTime])
     }
 
-    const displayTime = () => {
-        let hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 24)
-        let mins = Math.floor((elapsedTime / (1000 * 60)) % 60)
-        let secs = Math.floor((elapsedTime / 1000) % 60)
-        let milisecs = Math.floor((elapsedTime % 1000) / 10)
+    // Function to separate time in miliseconds to hours, minutes, and seconds
+    const displayTime = (time: number) => {
+        let hours = Math.floor((time / (1000 * 60 * 60)) % 24)
+        let mins = Math.floor((time / (1000 * 60)) % 60)
+        let secs = Math.floor((time / 1000) % 60)
+        let milisecs = Math.floor((time % 1000) / 10)
         return `${hours < 10 ? "0" + hours: hours}:${mins < 10 ? "0" + mins: mins}:${secs < 10 ? "0" + secs: secs}:${milisecs < 10 ? "0" + milisecs: milisecs}`
     }
 
     return(
         <div className="StopWatch" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
             <h1>StopWatch</h1>
-            <h2>{displayTime()}</h2>
+            <h2>{displayTime(elapsedTime)}</h2>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <StopWatchButton text={"Start"} handleClick={handleStart}/>
                 <StopWatchButton text={"Stop"} handleClick={handleStop}/>
                 <StopWatchButton text={"Reset"} handleClick={handleReset}/>
-                <StopWatchButton text={"New Lap"} handleClick={handleLap}/>
+                <StopWatchButton text={"New Lap"} handleClick={handleLaps}/>
             </div>
+
+            {!laps.length ? null : laps.map((lap, idx) => {
+                if (idx == 0) {
+                    return <p>Lap {idx+1}: {displayTime(lap)}</p>
+                } else {
+                    return <p>Lap {idx+1}: {displayTime(lap - laps[idx-1])}</p>
+                }
+            }) }
         </div>
     )
 }
