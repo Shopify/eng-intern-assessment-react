868aad4ac1a863b633686f0f1056babed6f163f1
Jugal Lad <jug091000@gmail.com>
2024-01-18 16:47:17 -0800

    set up StopWatch.tsx

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+interface Lap {
+  id: number;
+  time: number;
 }
+
+interface stopWatchProps {
+  isRunning: boolean;
+  time: number;
+  laps: Lap[];
+}
+
+const StopWatch: React.FC<stopWatchProps> = ({ isRunning, time, laps }) => {
+  return (
+    <div>
+      <h1>StopWatch!!</h1>
+      <p>Time: {time}s</p>
+      {laps.length > 0 && (
+        <div>
+          <h2>Laps</h2>
+          <ul>
+            {laps.map((lap) => (
+              <li key={lap.id}>
+                Lap {lap.id}: {lap.time}s
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StopWatch;
