04a9c1331c8111685050ed946dbe50a26fb37c54
Victoria Gao <victoria@victoriagao.com>
2024-01-19 15:06:22 -0500

    Added some webpage formatting, and began implementing the stopwatch counter

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -15,15 +15,66 @@ const [time, setTime] = React.useState<Time>({
     milliseconds: 0
 })
 const [lapNum, setLapNum] = React.useState(0)
-const [state, setState] = React.useState("INITIAL")
+const state = 0
 
 export default function StopWatch() {
 
+    React.useEffect(() => {
+        setTimeout(() => {
+            const newTime = time
+            if (newTime.seconds == 59){
+                newTime.seconds = 0;
+                newTime.minutes++;
+            }
+            if (newTime.minutes == 60){
+                newTime.minutes = 0;
+                newTime.hours++;
+            }
+        }, state);
+    })
 
+    document.getElementById('lap')?.addEventListener('click', function(){
+        const newLap = document.createElement("div")
+        const lapText = document.createTextNode("Lap " + lapNum + ": " + time.hours + ":" + time.minutes + ":" + time.seconds + ":" + time.milliseconds)
+        newLap.appendChild(lapText);
+        const lapList = document.getElementById('lapList')
+        lapList?.appendChild(newLap)
+    })  
 
     return(
-        <div>
-            <StopWatchButton state={state} setState={setState}/>
+        <div style={{
+            display: 'grid', //2 columns, 3 rows
+            gridTemplateColumns: 'minmax(0, 1fr) minmax(0, 1fr)',
+            gridTemplateRows: 'minmax(0, 1fr) minmax(0, 3fr) minmax(0, 1fr)'
+        }}>
+            <div style={{ //Title
+                gridColumn: '1 / span 2',
+                gridRow:'1'
+            }}>
+                Stopwatch
+            </div>
+
+            <div style={{ //Timer Element
+                gridColumn: '1',
+                gridRow: '2'
+            }}>
+                {time.hours + ":" + time.minutes + ":" + time.seconds + ":" + time.milliseconds}
+            </div>
+
+            <div style={{ //Lap Display
+                gridColumn: '2',
+                gridRow: '2'
+            }} id="lapList">
+                Laps
+            </div>
+
+            <div style={{ //Buttons
+                gridColumn: '1',
+                gridRow: '3'
+            }}>
+            <StopWatch/>
+            <input type="button" value="Lap" id="lap"/>
+            </div>
         </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -3,16 +3,9 @@ import React, { Dispatch, SetStateAction } from 'react'
 type state = string;
 type setState = string;
 
-export default function StopWatchButton({state}: {state: string}, {setState}: {setState: Dispatch<SetStateAction<string>>}) {
+export default function StopWatchButton() {
     return(
         <div>
-            {/* <button onClick={() => {
-                if (mode2){ //Stopwatch is currently running
-
-                }
-                }}> Stop! </button> */}
-            <button onClick={() => {setState("COUNTING")}}> Start! </button>
-            {/* <button onClick={() => mode1 = 2}> Lap </button> */}
         </div>
     )
 }
