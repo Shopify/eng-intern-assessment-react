7f30fa5d0d62045602737c007ac2fd7aa224120c
Bryan Zhang <maglev@tagbits.xyz>
2024-01-18 18:16:25 -0800

    added Start, Stop buttons and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,36 @@
 import React, { useState, useEffect } from "react";
 
 import StopWatch from "./StopWatch";
+import StopWatchButton from "./StopWatchButton";
 
 export default function App() {
   // states for timer
+  const [isStopped, setIsStopped] = useState(true);
   const [startTime, setStartTime] = useState(0);
   const [timeElapsed, setTimeElapsed] = useState(0);
 
   // use setInterval to update timer
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimeElapsed(timeElapsed + 10);
-    }, 10); // update every 100 milliseconds
+    if (!isStopped) {
+      const intervalId = setInterval(() => {
+        setTimeElapsed((prevTimeElapsed) => prevTimeElapsed + 10);
+      }, 10); // increment timeElapsed by 10 every 10 milliseconds
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  });
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [isStopped]);
+
+  // function to update isStopped state of timer
+  const updateStop = (stop: boolean): void => {
+    setIsStopped(stop);
+  };
 
   return (
     <div>
-      <h1>{startTime}</h1>
       <StopWatch timeElapsed={timeElapsed} />
+      <StopWatchButton updateStop={updateStop} />
     </div>
   );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+// component that represents the start, stop, and reset buttons
+import React from "react";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+interface Props {
+  updateStop: (stop: boolean) => void;
+}
+
+export default function StopWatchButton({ updateStop }: Props) {
+  return (
+    <div>
+      <button onClick={() => updateStop(false)}>Start</button>
+      <button onClick={() => updateStop(true)}>Stop</button>
+    </div>
+  );
 }
