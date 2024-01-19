7eb993bbc8a85196628ca8fb0d84e1ba11fd4ff0
Joseph Lee <josephlee3004@gmail.com>
2024-01-19 18:23:12 -0500

    Create list of todos for tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -23,4 +23,13 @@ describe('Stopwatch', () => {
         expect(screen.getByText('Lap')).toBeInTheDocument();
         expect(screen.queryByTestId('lap-list')).toBeEmptyDOMElement();
     });
+
+    test.todo('starts the stopwatch to begin counting time');
+    test.todo('stops the stopwatch to pause the timer');
+    test.todo('resets the stopwatch to zero');
+    test.todo('displays correct lap times');
+    test.todo('keeps time correctly');
+    test.todo('displays time with proper formatting');
+    test.todo('does not allow for seconds to overflow');
+    test.todo('does not allow for minutes to overflow');
 });
