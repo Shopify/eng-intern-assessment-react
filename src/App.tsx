8b98e5d9e8153abcc13c4b47fcc4312b5fea599d
Joseph Lee <josephlee3004@gmail.com>
2024-01-19 17:47:28 -0500

    Add test id to lap list

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ export default function App() {
     const [hours, setHours] = useState(0);
     const [counting, setCounting] = useState(false);
     const [laps, setLaps] = useState([]);
-    
+
     useEffect(() => {
         if (counting) {
             intervalID = setInterval(() => {
 
                 // Handle seconds overflow
-                if (seconds == 59 ) {
+                if (seconds == 59) {
                     setMinutes(minutes => minutes + 1);
                     setSeconds(0);
                     return;
@@ -48,30 +48,30 @@ export default function App() {
         }
     }, [counting, seconds, minutes, hours])
 
-    return(
+    return (
         <div>
-            <StopWatch hours={hours} minutes={minutes} seconds={seconds}/>
+            <StopWatch hours={hours} minutes={minutes} seconds={seconds} />
             <StopWatchButton text='Start' handleClick={() => {
                 setCounting(true);
-            }}/>
+            }} />
             <StopWatchButton text='Stop' handleClick={() => {
                 setCounting(false);
-            }}/>
+            }} />
             <StopWatchButton text='Reset' handleClick={() => {
                 setCounting(false);
                 setSeconds(0);
                 setMinutes(0);
                 setHours(0);
                 setLaps([]);
-            }}/>
+            }} />
             <StopWatchButton text='Lap' handleClick={() => {
                 // Add current time to lap list
                 setLaps([...laps, { id: uuidv4(), hours: hours, minutes: minutes, seconds: seconds }])
-            }}/>
+            }} />
 
-            <ol>
+            <ol data-testid='lap-list'>
                 {laps.map(lap => (
-                    <li  key={lap.id}>{String(lap.hours).padStart(2, '0')}:{String(lap.minutes).padStart(2, '0')}:{String(lap.seconds).padStart(2, '0')}</li>
+                    <li key={lap.id}>{String(lap.hours).padStart(2, '0')}:{String(lap.minutes).padStart(2, '0')}:{String(lap.seconds).padStart(2, '0')}</li>
                 ))}
             </ol>
         </div>
