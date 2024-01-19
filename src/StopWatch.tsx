828d5379bc30a6adfe6daf6e93b67ecce136147c
ethansjc <ethansjcheong@gmail.com>
2024-01-19 16:08:45 -0500

    created watch ui

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import StopWatchButton from './StopWatchButton'
 export default function StopWatch() {
-    return(
-        <div></div>
+    return (
+        <div className="container">
+            <div className="watch">
+                <div className="outer-circle">
+                    <div className="inner-circle">
+                        <span className="text minute">0 :</span>
+                        <span className="text sec">&nbsp; 0 :</span>
+                        <span className="text msec">&nbsp; 0</span>
+                    </div>
+                </div>
+                <StopWatchButton/>
+            </div>
+           
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import './styles.css'
 
 export default function StopWatchButton() {
-    return(
-        <div></div>
+    
+    return (
+        <div className="container">
+            <div className="watch">
+                <div className="button-wrapper">
+                    <button className="button reset">Reset</button>
+                    <button className="button play">Play</button>
+                    <button className="button lap">Lap</button>
+                </div>
+            </div>
+            <ul className="laps">
+                <button className="lap-clear-button">Clear All</button>
+            </ul>
+        </div>
     )
 }
