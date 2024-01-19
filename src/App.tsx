af158cb18736c95f05f2c4fabfe312426d5468d3
Jugal Lad <jug091000@gmail.com>
2024-01-18 16:49:03 -0800

    set up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import StopWatch from './StopWatch'
 
-export default function App() {
-    return(
-        <div></div>
+interface Lap {
+    id:number,
+    time: number
+}
+
+const App: React.FC = () => {
+    const [isRunning, setIsRunning] = useState(false)
+    const [time,setTime] = useState(0);
+    const [laps,setLaps] = useState<Lap[]>([]);
+
+    return (
+        <div>
+            <StopWatch isRunning={isRunning} time={time} laps={laps} />
+        </div>
     )
 }
+
+export default App;
