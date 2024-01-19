166b4dcb0b12a1216b9dbf6cb8c77c7b1fd8a593
Bryan Zhang <maglev@tagbits.xyz>
2024-01-18 16:47:42 -0800

    added states and useEffect to get time increment working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+// The main component that renders the stopwatch and handles its functionality.
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  // states for timer
+  const [startTime, setStartTime] = useState(0);
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  
+  // use setInterval to update timer 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimeElapsed(timeElapsed + 10);
+    }, 10); // update every 100 milliseconds
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
+
+  return (
+    <div>
+      <h1>{startTime}</h1>
+      <h1>{timeElapsed}</h1>
+    </div>
+  );
 }
