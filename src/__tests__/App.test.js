f3ea35e237246155479e2f46b24cce8e0573728c
Joseph Lee <josephlee3004@gmail.com>
2024-01-20 12:45:27 -0500

    Create tests for counting seconds, minutes and hours correctly

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
 
 import App from '../App';
 
@@ -11,26 +12,56 @@ describe('Stopwatch', () => {
     });
 
     afterEach(() => {
-        jest.runOnlyPendingTimers();
         jest.useRealTimers();
     });
 
     test('renders initial state correctly', () => {
         expect(screen.getByText('00:00:00')).toBeInTheDocument();
 
-        expect(screen.getByRole('button', { name: /Start/i})).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /Stop/i})).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /Reset/i})).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /Lap/i})).toBeInTheDocument();
-        
+        expect(screen.getByRole('button', { name: /Start/i })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /Stop/i })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /Reset/i })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /Lap/i })).toBeInTheDocument();
+
         expect(screen.queryByTestId('lap-list')).toBeEmptyDOMElement();
     });
 
-    test.todo('starts the stopwatch to begin counting time');
+    test('counts seconds correctly', () => {
+        const startBtn = screen.getByRole('button', { name: /Start/i });
+        fireEvent.click(startBtn);
+
+        act(() => {
+            // Move stopwatch forward by 5 seconds
+            jest.advanceTimersByTime(5 * 1000);
+        })
+        expect(screen.getByText('00:00:05')).toBeInTheDocument();
+    });
+
+    test('counts minutes correctly', () => {
+        const startBtn = screen.getByRole('button', { name: /Start/i });
+        fireEvent.click(startBtn);
+
+        act(() => {
+            // Move stopwatch forward by 2 minutes
+            jest.advanceTimersByTime(2 * 60 * 1000);
+        })
+        expect(screen.getByText('00:02:00')).toBeInTheDocument();
+    });
+    
+    test('counts hours correctly', () => {
+        const startBtn = screen.getByRole('button', { name: /Start/i });
+        fireEvent.click(startBtn);
+
+        act(() => {
+            // Move stopwatch forward by 3 hours
+            jest.advanceTimersByTime(3 * 60 * 60 * 1000);
+        })
+        expect(screen.getByText('03:00:00')).toBeInTheDocument();
+    });
+
     test.todo('stops the stopwatch to pause the timer');
     test.todo('resets the stopwatch to zero');
     test.todo('displays correct lap times');
-    test.todo('keeps time correctly');
     test.todo('displays time with proper formatting');
     test.todo('does not allow for seconds to overflow');
     test.todo('does not allow for minutes to overflow');
