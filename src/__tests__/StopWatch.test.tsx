245e6aef17886fd246b4af7fdfb228007845f772
Daniel Tan <danieltan380@gmail.com>
2024-01-19 22:59:22 -0800

    Add tests for StopWatch.

---
+++ src/__tests__/StopWatch.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, } from '@testing-library/react';
+import StopWatch from '../StopWatch';
+import React from 'react';
+
+describe('StopWatch', () => {
+  it('should render StopWatch component, with the display showing "00:00:00"', () => {
+    render(<StopWatch />);
+  
+    const display = screen.getByTitle('display');
+    
+    expect(display).toBeDefined();
+  
+    expect(display.textContent).toEqual("00:00:00");
+  });
+  
+  it('should render StopWatch component and all buttons', () => {
+    render(<StopWatch />);
+    
+    const start = screen.getByTitle('start')
+    const stop = screen.getByTitle('stop')
+    const reset = screen.getByTitle('reset')
+    const lap = screen.getByTitle('lap')
+  
+    expect(start).toBeDefined();
+    expect(stop).toBeDefined();
+    expect(reset).toBeDefined();
+    expect(lap).toBeDefined();
+  });
+})
