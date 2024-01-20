65005f17feae1c8835e6b20ea153394611f40651
victoriamazilu <victoria.mazilu@icloud.com>
2024-01-20 18:45:46 -0500

    Update StopWatchButton.tsx
    Fixed bug with props

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-type Props = {
+interface Props {
     label: string;
     onClick: () => void; // Function to call when the button is clicked
     isRunning: boolean;
     className?: string;
-  };
+};
 
 export default function StopWatchButton({ label, onClick, isRunning, className }: Props) {
     return(
         <button className={`button-outline ${className} ${isRunning ? 'running' : ''}`} onClick={onClick}>
-      {label}
-    </button>
+            {label}
+        </button>
     )
 }
