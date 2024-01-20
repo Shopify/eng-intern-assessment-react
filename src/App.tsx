c6c1b7ce54c242293602871d602ebc22c7ca3573
Sulilium <lilyjennyw@gmail.com>
2024-01-20 00:37:31 -0500

    Implemented milliseconds and minutes
    Added more precision to the time which only previously displayed seconds.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { time } from 'console';
 import React, { useEffect, useState } from 'react'
 
 export default function App() {
@@ -6,13 +7,26 @@ export default function App() {
     const [laps, setLaps] = useState([]);
     const [prevLap, setPrevLap] = useState(0);
 
+    let milliseconds, seconds, minutes;
+    let timeUnits = [];
+    let lapTimeUnits = [];
+
     useEffect(() => {
         let timeInterval: any;
         if(status){
-            timeInterval = setInterval(() => setTimeElapsed(timeElapsed+1) , 1000);
+            timeInterval = setInterval(() => setTimeElapsed(timeElapsed+1) , 10);
         } 
         return () => clearInterval(timeInterval);
     }, [status, timeElapsed]);
+
+    function convertToTimeUnits(time:number){
+        let milliseconds, seconds, minutes;
+
+        milliseconds = (time%100).toString().padStart(2, "0");
+        seconds = Math.floor((time/100)%60).toString().padStart(2, "0");
+        minutes = Math.floor(time/6000).toString().padStart(2, "0");
+        return[minutes, seconds, milliseconds];
+    }
     
     function toggleStopwatch(){
         if(!status){
@@ -36,16 +50,19 @@ export default function App() {
             {return [...curLaps, time-prevLap]})
         setPrevLap(time)
     }
+    
+    timeUnits = convertToTimeUnits(timeElapsed);
 
     return(
         <div>
             <button id="toggle-stopwatch" onClick={toggleStopwatch}>Start</button>
             <button id="reset-stopwatch" onClick={resetStopwatch}>Reset</button>
             <button id="lap-stopwatch" onClick={() => addLap(timeElapsed)}>Lap</button>
-            {timeElapsed}
+            {timeUnits[0]}:{timeUnits[1]}.{timeUnits[2]}
             {laps.map((lap, index) => {
+                lapTimeUnits = convertToTimeUnits(lap)
                 return(
-                    <p>Lap {index+1} | {lap}</p>
+                    <p>Lap {index+1} | &emsp; {lapTimeUnits[0]}:{lapTimeUnits[1]}.{lapTimeUnits[2]}</p>
                 )
             })}
         </div>
