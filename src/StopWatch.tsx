2dc1e4a313a9a23e482a4f09615b36c5b0cd7785
Ali Amin <aliamin745@gmail.com>
2024-01-19 19:22:21 -0500

    Added motion animation to Stopwatch and Showcase

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import StopWatchButton from "./StopWatchButton";
+import { motion } from "framer-motion";
 
 interface StopwatchProps {
 	updateTimeshowcase: (lapData: string[]) => void;
@@ -74,7 +75,10 @@ export default function StopWatch({ updateTimeshowcase }: StopwatchProps) {
 	};
 
 	return (
-		<div className="text-white m-auto flex flex-col justify-center gap-5 items-center mb-20 bg-[#2a2c3c] rounded-full h-80 w-80 neumorphic-shadow ">
+		<motion.div
+			layout
+			className="text-white m-auto flex flex-col justify-center gap-5 items-center mb-20 bg-[#2a2c3c] rounded-full h-80 w-80 neumorphic-shadow "
+		>
 			<div className="text-center text-4xl rounded-full bg-[rgb(92,110,245)] neumorphic-invert-shadow p-4">
 				{hr.toString().padStart(2, "0")}:
 				{min.toString().padStart(2, "0")}:
@@ -89,6 +93,6 @@ export default function StopWatch({ updateTimeshowcase }: StopwatchProps) {
 				<StopWatchButton label={"Lap"} methodCall={lapTiming} />
 				<StopWatchButton label={"Reset"} methodCall={resetTiming} />
 			</div>
-		</div>
+		</motion.div>
 	);
 }
--- src/Timeshowcase.tsx
+++ src/Timeshowcase.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface TimeshowcaseProps {
 	lapData: string[];
@@ -6,21 +7,33 @@ interface TimeshowcaseProps {
 
 const Timeshowcase = ({ lapData }: TimeshowcaseProps) => {
 	return (
-		<div className="relative m-auto rounded-lg h-72 w-80 p-2 bg-white border-b-4 border-r-4 border-[rgb(92,110,245)]">
+		<motion.div
+			initial={{ y: 100, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			exit={{ y: 100, opacity: 0 }}
+			transition={{ duration: 0.5, delay: 0.2 }}
+			className="relative m-auto rounded-lg h-72 w-80 p-2 bg-white border-b-4 border-r-4 border-[rgb(92,110,245)]"
+		>
 			<h2 className="absolute -top-5 left-1/2 -translate-x-1/2 w-40 bg-[rgb(92,110,245)] border-b-4 border-r-4 border-[#2a2c3c] pl-4 pr-4 pt-1 pb-1 text-white text-center text-xl rounded-lg">
 				Lap Table
 			</h2>
 			<div className="mt-5 h-[240px] overflow-auto">
 				{lapData.map((lap, index) => {
 					return (
-						<div className="text-center text-xl" key={index}>
+						<motion.div
+							initial={{ y: 10, opacity: 0 }}
+							animate={{ y: 0, opacity: 1 }}
+							transition={{ duration: 0.2 }}
+							className="text-center text-xl"
+							key={index}
+						>
 							{index !== 0 && <hr className="m-1" />}
 							{lap}
-						</div>
+						</motion.div>
 					);
 				})}
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
