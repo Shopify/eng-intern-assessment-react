e11a0e073ca912ba185fa3034b53ceb5b9aa584f
Edward He <ehe058@uottawa.ca>
2024-01-20 15:45:07 -0500

    completed stopwatch interface and buttons, and also the functions

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './styles/StopWatchButtonStyles.css';
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+/**
+ * Props for StopWatchButton component.
+ */
+interface StopWatchButtonProps {
+    onStartStopClick: () => void; // Handler for start/stop button click
+    onLapClick: () => void;       // Handler for lap button click
+    onResetClick: () => void;     // Handler for reset button click
+}
+
+/**
+ * A component for the stopwatch buttons: Start/Stop, Lap, and Reset.
+ * @param props The properties passed to the component.
+ */
+export default function StopWatchButton({ onStartStopClick, onLapClick, onResetClick }: StopWatchButtonProps) {
+    return (
+        <div className='container-button'>
+            {/* Start/Stop button */}
+            <button id='StartStop' onClick={onStartStopClick}>Start/Stop</button>
+
+            {/* Lap button */}
+            <button id='Laps' onClick={onLapClick}>Lap</button>
+
+            {/* Reset button */}
+            <button id='Reset' onClick={onResetClick}>Reset</button>
+        </div>
+    );
 }
