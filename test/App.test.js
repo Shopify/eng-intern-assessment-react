98317dad909d09f457edb6641d1434d136db4b33
xius666 <xiu@ualberta.ca>
2024-01-19 21:59:14 -0500

    add a few intitial tests along with new dependencies

--- test/App.test.js
+++ test/App.test.js
@@ -1,15 +1,20 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../src/App";
-describe("Scoped / Nested block", () => {
-  beforeAll(() =>
-    beforeEach(() => {
-      console.log("render the app");
-      render(<App />);
-    })
-  );
+import React from "react";
+import "@testing-library/jest-dom";
+describe("test", () => {
+  beforeEach(() => {
+    console.log("render the app");
+    render(<App />);
+  });
 
-  test("render the StopWatch view", () => {});
-  test("render the  StopWatchButton componenent", () => {});
+  test("render the StopWatch view", () => {
+    expect(screen.getByText("00:00:00:00"));
+  });
+  test("render the StopWatchButton componenent", () => {
+    expect(screen.getByRole("button", { name: "Start" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Reset" })).toBeInTheDocument();
+  });
   test("click the stop Button after start the timer", () => {});
   test("click the lap Button after start the timer", () => {});
   test("click the start Button after stopping the timer", () => {});
