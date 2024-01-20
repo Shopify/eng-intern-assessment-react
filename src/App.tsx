f2d171e4e0e262080633e4f1e34735cc9e34f1ed
Ali Amin <aliamin745@gmail.com>
2024-01-19 19:02:18 -0500

    App.tsx created, added app background and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import StopWatch from "./StopWatch";
+import Timeshowcase from "./Timeshowcase";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+	const [lapData, setLapData] = useState<string[]>([]);
+
+	const updateTimeshowcase = (lapData: string[]) => {
+		setLapData(lapData);
+	};
+
+	return (
+		<div className="w-full transition-all flex min-h-screen bg-[#2a2c3c]">
+			<div className="m-auto p-10 w-2/4">
+				<StopWatch updateTimeshowcase={updateTimeshowcase} />
+				{lapData.length !== 0 && <Timeshowcase lapData={lapData} />}
+			</div>
+		</div>
+	);
 }
