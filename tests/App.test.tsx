9f6798b4552d307c08f6fc5fac54ab58a801bcac
Peter Tran <peterkhoatran@gmail.com>
2024-01-20 00:05:39 -0700

    Setup jest test runner, added unit tests for App.tsx, changed setInterval in App.tsx to call setTime every 1ms.

---
+++ tests/App.test.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import App from '../src/App'
+import { render, fireEvent, screen, act, waitFor } from '@testing-library/react';
+
+describe('Stopwatch App Component', () => {
+    beforeAll(() => {
+        jest.useFakeTimers()
+    })
+
+    afterAll(() => {
+        jest.useRealTimers()
+    })
+
+    test('Render 00:00:00.000 starting state', () => {
+        render(<App />);
+        screen.getByText('Stopwatch');
+        // Check each part of the starting time separately, starting with hours
+        expect(screen.getByTestId('hours-1').textContent).toBe('0');
+        expect(screen.getByTestId('hours-2').textContent).toBe('0');
+        // Check minutes
+        expect(screen.getByTestId('min-1').textContent).toBe('0');
+        expect(screen.getByTestId('min-2').textContent).toBe('0');
+        // Check seconds
+        expect(screen.getByTestId('seconds-1').textContent).toBe('0');
+        expect(screen.getByTestId('seconds-2').textContent).toBe('0');
+        // Check Milliseconds
+        expect(screen.getByTestId('ms-1').textContent).toBe('0');
+        expect(screen.getByTestId('ms-2').textContent).toBe('0');
+        expect(screen.getByTestId('ms-3').textContent).toBe('0');
+    })
+
+    test('Test reset button resets the timer', async () => {
+        render(<App/>);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(1000)
+          });
+        fireEvent.click(screen.getByText('Reset'));
+        expect(screen.getByTestId('hours-1').textContent).toBe('0');
+        expect(screen.getByTestId('hours-2').textContent).toBe('0');
+        expect(screen.getByTestId('min-1').textContent).toBe('0');
+        expect(screen.getByTestId('min-2').textContent).toBe('0');
+        expect(screen.getByTestId('seconds-1').textContent).toBe('0');
+        expect(screen.getByTestId('seconds-2').textContent).toBe('0');
+        expect(screen.getByTestId('ms-1').textContent).toBe('0');
+        expect(screen.getByTestId('ms-2').textContent).toBe('0');
+        expect(screen.getByTestId('ms-3').textContent).toBe('0');
+    });
+
+    test('Test 1ms has passed', async () => {
+        render(<App/>);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(1)
+          });
+        fireEvent.click(screen.getByText('Stop'));
+        await waitFor(() => {
+            expect(screen.getByTestId('ms-1').textContent).toBe('0');
+            expect(screen.getByTestId('ms-2').textContent).toBe('0');
+            expect(screen.getByTestId('ms-3').textContent).toBe('1');
+        });
+    });
+
+    test('Test 1 second has passed', () => {
+        render(<App/>);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(1000)
+          });
+        fireEvent.click(screen.getByText('Stop'));
+        expect(screen.getByTestId('seconds-1').textContent).toBe('0');
+        expect(screen.getByTestId('seconds-2').textContent).toBe('1');
+    });
+
+    test('Test 1 minute has passed', () => {
+        render(<App/>);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(60000)
+          });
+        fireEvent.click(screen.getByText('Stop'));
+        expect(screen.getByTestId('min-1').textContent).toBe('0');
+        expect(screen.getByTestId('min-2').textContent).toBe('1');
+    });
+
+    test('Test 1 hour has passed', () => {
+        render(<App/>);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(3600000)
+          });
+        fireEvent.click(screen.getByText('Stop'));
+        expect(screen.getByTestId('hours-1').textContent).toBe('0');
+        expect(screen.getByTestId('hours-2').textContent).toBe('1');
+    });
+
+    test('lap button records lap times', () => {
+        render(<App />);
+        fireEvent.click(screen.getByText('Start'));
+        act(() => {
+            jest.advanceTimersByTime(1000)
+          });
+        fireEvent.click(screen.getByText('Lap'));
+        // Check if the lap time is displayed
+        const lapTime = screen.getByText('00:00:01.000');
+        expect(lapTime).toBeTruthy();
+    });
+});
