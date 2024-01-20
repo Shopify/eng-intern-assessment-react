c0d8e0af96fe0239eeffc8eda8797ad8e364fc4a
Sulilium <lilyjennyw@gmail.com>
2024-01-19 22:24:56 -0500

    Basic Stopwatch Functionality (No Laps)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+    const [timeElapsed, setTimeElapsed] = useState(0);
+    const [status, setStatus] = useState(false);
+    
+    useEffect(() => {
+        let timeInterval: any;
+        if(status){
+            timeInterval = setInterval(() => setTimeElapsed(timeElapsed+1) , 1000);
+        } 
+        return () => clearInterval(timeInterval);
+    }, [status, timeElapsed]);
+    
+    function toggleStopwatch(){
+        if(!status){
+            setStatus(true);
+            document.getElementById("toggle-stopwatch").innerHTML = "Stop";
+        }
+        else{
+            setStatus(false);
+            document.getElementById("toggle-stopwatch").innerHTML = "Start";
+        }
+    }
+    
     return(
-        <div></div>
+        <div>
+            <button id="toggle-stopwatch" onClick={toggleStopwatch}>Start</button>
+            <button id="reset-stopwatch" onClick={() => setTimeElapsed(0)}>Reset</button>
+            <button id="lap-stopwatch" >Lap</button>
+            {timeElapsed}
+        </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function StopWatch() {
+
     return(
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
