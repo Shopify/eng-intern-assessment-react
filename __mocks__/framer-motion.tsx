445b94453bb35372b85919cfa381851260a1c536
Ali Amin <aliamin745@gmail.com>
2024-01-19 23:01:03 -0500

    Added framer motion mock

--- __mocks__/framer-motion.tsx
+++ __mocks__/framer-motion.tsx
@@ -1,14 +1,13 @@
-// __mocks__/framer-motion.ts
 import * as React from "react";
 const actual = jest.requireActual("framer-motion");
 
+// This file is used to mock the framer-motion library for testing purposes
 module.exports = {
 	...actual,
 	motion: {
 		div: ({ children }: { children: React.ReactNode }) => (
 			<div>{children}</div>
 		),
-		// Add other HTML tags or custom components as needed
 	},
 	AnimatePresence: ({ children }: { children: React.ReactNode }) => (
 		<>{children}</>
