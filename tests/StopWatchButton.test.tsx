80c8c3250514fa438d84edfb4fcc1429a0ed7b60
Ali Amin <aliamin745@gmail.com>
2024-01-19 21:01:50 -0500

    Created stopwatch button test

---
+++ tests/StopWatchButton.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import StopWatchButton from "../src/StopWatchButton";
+
+describe("StopWatchButton Component", () => {
+	it("renders without crashing", () => {
+		render(<StopWatchButton label="Test" methodCall={() => {}} />);
+	});
+
+	it("calls methodCall when button is clicked", () => {
+		const mockMethodCall = jest.fn();
+		const { getByText } = render(
+			<StopWatchButton label="Test" methodCall={mockMethodCall} />
+		);
+
+		fireEvent.click(getByText("Test"));
+
+		expect(mockMethodCall).toHaveBeenCalled();
+	});
+});
