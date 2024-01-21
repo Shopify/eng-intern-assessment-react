8909009f256a5d7b7ad349d82b9809ce817d35f0
Nathan Cai <nathancai1031@gmail.com>
2024-01-20 22:39:20 -0500

    implemented basic features

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import StopWatchButton from "./StopWatchButton";
+import { formatTime, secsToTime } from "./utils";
+
+export type Lap = {
+  hours: number;
+  min: number;
+  sec: number;
+};
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState<Lap[]>([]);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const [hours, setHours] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+
+  // Using useRef to persist the interval ID across renders
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+
+  function onClick() {
+    if (isCounting) {
+      // Clearing the interval using the current value of the ref
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+      setIsCounting(false);
+      setLaps([...laps, secsToTime(time)]);
+    } else {
+      intervalRef.current = setInterval(() => setTime((prev) => prev + 1), 1000);
+      setIsCounting(true);
+    }
+  }
+
+  useEffect(() => {
+    let { hours, min, sec } = secsToTime(time);
+    setHours(hours);
+    setMin(min);
+    setSec(sec);
+  }, [time]);
+
+  return (
+    <div>
+      <div>{formatTime({ hours, min, sec })}</div>
+      <div>
+        <StopWatchButton onClick={onClick} isCounting={isCounting}/>
+      </div>
+    </div>
+  );
 }
