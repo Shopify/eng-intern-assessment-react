f1150fff70c0aeec4094c6e802a44c1367f91852
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-01-21 16:23:50 -0500

    ADDED: useStopWatch Hook
    useStopWatch encapsulates the logic and state for the stop watch, optimizes with useCallback hooks.

---
+++ src/components/StopWatch/hooks/useStopWatch.test.ts
@@ -0,0 +1,272 @@
+
+import {renderHook, act} from '@testing-library/react';
+
+import useStopwatch from './useStopWatch';
+
+
+describe('useStopwatch hook - start functionality', () => {
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+    const startTime = 0;
+    jest.spyOn(global, 'requestAnimationFrame').mockImplementation((callback) => {
+      return setTimeout(() => callback(startTime + Date.now()), 0) as unknown as number;
+    });
+    jest.spyOn(performance, 'now').mockImplementation(() => Date.now() - startTime);
+
+    jest.spyOn(global, 'cancelAnimationFrame').mockImplementation((id) => {
+      clearTimeout(id) as unknown as number;
+    })
+  });
+
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+
+  });
+
+  it('should start the stopwatch', () => {
+
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+    });
+
+    expect(result.current.running).toBe(true);
+
+
+  });
+
+
+  it('should start counting time', () => {
+
+    const { result } = renderHook(() => useStopwatch({}));
+    expect(result.current.milliseconds).toBe(0);
+    act(()=>{
+
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000); 
+    })
+    expect(result.current.milliseconds).toBeGreaterThan(0);
+
+
+
+
+  });
+
+  it("should count time accurately", () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000); 
+    });
+
+    expect(result.current.milliseconds).toBe(1000);
+  })
+});
+
+describe('useStopwatch hook - stop functionality', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+    const startTime = 0;
+    jest.spyOn(global, 'requestAnimationFrame').mockImplementation((callback) => {
+      return setTimeout(() => callback(startTime + Date.now()), 0) as unknown as number;
+    });
+    jest.spyOn(performance, 'now').mockImplementation(() => Date.now() - startTime);
+
+    jest.spyOn(global, 'cancelAnimationFrame').mockImplementation((id) => {
+      clearTimeout(id) as unknown as number;
+    })
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+
+  });
+
+
+
+  it('should stop the stopwatch', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1000);
+      result.current.actions.stop();
+    })
+    
+    expect(result.current.running).toBe(false);
+  });
+
+  it('should stop counting time', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      result.current.actions.stop();
+      jest.advanceTimersByTime(1000); 
+    });
+
+    expect(result.current.milliseconds).toBe(0);
+  });
+
+  it('should stop counting time accurately', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000); 
+      result.current.actions.stop();
+      jest.advanceTimersByTime(1000); 
+    });
+
+    expect(result.current.milliseconds).toBe(1000);
+  });
+})
+
+describe('useStopwatch hook - reset functionality', () => {
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+    const startTime = 0;
+    jest.spyOn(global, 'requestAnimationFrame').mockImplementation((callback) => {
+      return setTimeout(() => callback(startTime + Date.now()), 0) as unknown as number;
+    });
+    jest.spyOn(performance, 'now').mockImplementation(() => Date.now() - startTime);
+
+    jest.spyOn(global, 'cancelAnimationFrame').mockImplementation((id) => {
+      clearTimeout(id) as unknown as number;
+    })
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+
+  });
+
+
+  it('should reset the stopwatch', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      result.current.actions.reset();
+    });
+
+    expect(result.current.running).toBe(true);
+    expect(result.current.milliseconds).toBe(0);
+  });
+
+  it('should reset the stopwatch accurately', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.reset();
+
+    });
+    expect(result.current.running).toBe(true);
+    expect(result.current.milliseconds).toBe(0);
+
+  });
+  it('should reset the stopwatch accurately after stop', () => {
+    const { result } = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.stop();
+      result.current.actions.reset();
+
+    });
+    expect(result.current.running).toBe(false);
+    expect(result.current.milliseconds).toBe(0);
+  });
+
+})
+
+describe("useStopwatch hook - lap functionality", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+    const startTime = 0;
+    jest.spyOn(global, 'requestAnimationFrame').mockImplementation((callback) => {
+      return setTimeout(() => callback(startTime + Date.now()), 0) as unknown as number;
+    });
+    jest.spyOn(performance, 'now').mockImplementation(() => Date.now() - startTime);
+
+    jest.spyOn(global, 'cancelAnimationFrame').mockImplementation((id) => {
+      clearTimeout(id) as unknown as number;
+    })
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+
+  });
+
+  it("should add a lap", () => {
+    const {result} = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.lap();
+    });
+
+    expect(result.current.laps.length).toBe(1);
+  });
+
+  it("should add a lap accurately", () => {
+    const {result} = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.milliseconds).toBe(1000);
+
+    act(() => {
+        result.current.actions.lap();
+    })
+
+    expect(result.current.laps.length).toBe(1);
+    expect(result.current.laps[0]).toBe(1000);
+
+  });
+
+  it("should add a lap accurately after stop", () => {
+    const {result} = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.stop();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1000);
+      result.current.actions.lap();
+    })
+
+    expect(result.current.laps.length).toBe(1);
+    expect(result.current.laps[0]).toBe(1000);
+  });
+
+  it("should add a lap accurately after reset", () => {
+    const {result} = renderHook(() => useStopwatch({}));
+
+    act(() => {
+      result.current.actions.start();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.reset();
+      jest.advanceTimersByTime(1000);
+      result.current.actions.lap();
+    });
+
+    expect(result.current.laps.length).toBe(1);
+
+  })
+});
--- src/components/StopWatch/hooks/useStopWatch.ts
+++ src/components/StopWatch/hooks/useStopWatch.ts
@@ -1,5 +1,89 @@
-import React from "react"
+import React, {useRef,useCallback} from 'react'
+import {Resolution,SwActions} from "@types";
 
-export const useStopWatch = () => {
+
+export interface StopWatchController {
+    milliseconds: number;
+    resolutions: Resolution[];
+    laps: number[];
+    running: boolean;
+    actions: SwActions;
+}
+
+export interface UseStopWatchProps{
+    resolutions?: Resolution[]
+}
+
+
+const defaultResolution: Resolution[] = [
+    {divisor: 3600000, modulus: 24},
+    {divisor: 60000, modulus: 60},
+    {divisor: 1000, modulus: 60},
+    {divisor: 10, modulus: 100}
+]
+export const useStopWatch = (
+    {
+        resolutions
+    }: UseStopWatchProps) : StopWatchController  =>  {
+
+    if(!resolutions) resolutions = defaultResolution;
+
+    const [milliseconds, setMilliseconds] = React.useState(0)
+    const [laps, setLaps] = React.useState<number[]>([])
+    const [running, setRunning] = React.useState<boolean>()
+
+    const pref = useRef(null);
+    let curTime = 0;
+    const calculateTime = useCallback((time) => {
+
+        const delta = time - curTime;
+        if(delta > 0) {
+            console.log("DELTA: ", delta)
+            curTime = time;
+            setMilliseconds(prev => prev + delta);
+        }
+        pref.current = requestAnimationFrame(calculateTime);
+
+    }, [curTime, setMilliseconds]);
+
+    const start = useCallback(() => {
+
+        setRunning(true)
+        curTime = performance.now();
+        pref.current = requestAnimationFrame(calculateTime);
+    },[calculateTime, setRunning, curTime]);
+
+    const stop = useCallback (() => {
+        setRunning(false)
+        cancelAnimationFrame(pref.current);
+        //reset pref maybe?
+        pref.current = null
+    },[setRunning])
+
+    const reset = useCallback(() => {
+        setMilliseconds(0);
+        setLaps([]);
+    },[setMilliseconds, setLaps])
+
+
+    const lap = useCallback(() => {
+        setLaps(prev => [...prev, milliseconds]);
+
+    }, [setLaps, milliseconds]);
+
+    return {
+        milliseconds,
+        resolutions,
+        laps,
+        running,
+        actions: {
+            start,
+            stop,
+            reset,
+            lap
+        }
+    }
 
 }
+
+export default useStopWatch;
