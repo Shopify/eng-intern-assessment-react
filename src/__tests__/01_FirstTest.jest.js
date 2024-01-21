f8c90cc3821e6c398e8ed9cb550b40bd104ae845
ADedicatedCoder <mahin.kukreja@gmail.com>
2024-01-20 20:15:34 -0500

    Fixes #12

--- src/__tests__/01_FirstTest.jest.js
+++ src/__tests__/01_FirstTest.jest.js
@@ -7,8 +7,11 @@ import StopWatch from "../StopWatch";
  */
 
 test("Title is rendered", () => {
+	// Render the component being tested.
 	render(<StopWatch />);
 
+	// Get the relevant items by their test IDs.
 	const title = screen.getByTestId("title");
+	// Check if the component renders correctly by seeing if the title renders correctly.
 	expect(title.textContent).toBe("Frontend Engineering Technical Challenge");
 });
--- src/__tests__/03_ThirdTest.jest.js
+++ src/__tests__/03_ThirdTest.jest.js
@@ -8,16 +8,25 @@ import { act } from "react-dom/test-utils";
  */
 
 test("Stop button is functional", () => {
+	// Use fake timers to jump time instantly.
 	jest.useFakeTimers();
+
+	// Render the component being tested.
 	render(<StopWatch />);
 
+	// Get the relevant items by their test IDs.
 	const timer = screen.getByTestId("timer");
 	const startButton = screen.getByTestId("start");
 
+	// Wrap events that change state in the act() function
 	act(() => {
+		// Press the start button
 		fireEvent.click(startButton);
+
+		// Run the fake timer to skip the below 15 second wait.
 		jest.runAllTimers();
 
+		// Wait for 6 seconds, press the stop button and check that the time elapsed is 6 seconds.
 		setTimeout(() => {
 			const stopButton = screen.getByTestId("stop");
 			fireEvent.click(stopButton);
