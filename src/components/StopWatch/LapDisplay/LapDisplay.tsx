42145e39c4c5e624a207fb6530258ecd7a58ce1b
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-01-21 17:36:50 -0500

    ADDED: Lap Display + Unit tests

--- src/components/StopWatch/LapDisplay/LapDisplay.tsx
+++ src/components/StopWatch/LapDisplay/LapDisplay.tsx
@@ -1,17 +1,71 @@
-import React from "react"
+import React from 'react'
+import {StopWatchUIElement, Resolution, stopWatch} from "@types";
 
 
-interface compProps {
-    
+import { defaultListItemStyle,defaultLapListStyle } from './utils';
+import {DigitalDisplay, DigitalDisplayProps} from '../DigitalDisplay/';
+import { StopWatchContext } from '../'
+export interface LapDisplayProps extends StopWatchUIElement{
+    digitStyles?: React.CSSProperties;
+    orderedListStyles?: React.CSSProperties;
+    orderedListItemStyles?: React.CSSProperties;
+    DigitalDisplayProps?: Partial<DigitalDisplayProps>;
+    laps?: number[];
+    resolutions?: Resolution[];
+    darkTheme?: boolean;
+
 }
 
-export default function LapDisplay({
+export function LapDisplay({
+    containerStyles,
+    digitStyles,
+    orderedListStyles,
+    orderedListItemStyles,
+    DigitalDisplayProps,
+    laps,
+    resolutions,
+    darkTheme,
+}:LapDisplayProps) {
+
+   
+    if(laps === undefined){ laps = React.useContext(StopWatchContext).sw.laps}
+    if(resolutions === undefined){ resolutions = React.useContext(StopWatchContext).sw.resolutions}
+    if(darkTheme === undefined){ darkTheme = React.useContext(StopWatchContext).darkTheme}
     
-}: compProps) {
+    
+    const labelStyles: React.CSSProperties = {
+		fontSize:"1rem",
+		fontWeight:"bold",
+		margin:"0",
+        marginTop:"0px",
+        marginBottom:"0px",
+        color:darkTheme ? "#c0c0c0" : "#333",
+	}
+
 
     return (
-        <section className={""}>
+        <div data-testid="lap-display-wrapper" style={
+            {
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"apart",
+                width:"100%",
+                ...containerStyles
+            }
+        }>
 
-        </section>
+        <ol style={{...defaultLapListStyle,...orderedListStyles}} className={'lapDisplay'}>
+        {
+            laps.map((lap, index) => {
+                return (
+                    <li style={{...defaultListItemStyle}} key={index}>
+                        <h4 style={{...labelStyles ,...digitStyles}}>lap {index+1}{ '\u00A0'}</h4>
+                        <DigitalDisplay {...DigitalDisplayProps} milliseconds={lap} isLap darkTheme={darkTheme} />
+                    </li>
+                )
+            })
+        }
+    </ol>
+        </div>
     )
 }
