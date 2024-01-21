3e67354c0cefee90f5976232b3f4ec781ec59ff6
Magnum Dong <ebrahimhagh2004@gmail.com>
2024-01-21 16:57:39 -0500

    add tailwind and setup structure

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function StopWatch() {
-    return(
-        <div></div>
+    return (
+        <div className='h-full flex flex-col'>
+            <div className='h-1/3 border-b-2 flex justify-center items-center border-neutral-200 dark:border-neutral-800'>
+                <div className='text-3xl'>
+                    02:43:23.221
+                </div>
+            </div>
+            <div className='h-32 border-b-2 border-neutral-200 dark:border-neutral-800'>
+
+            </div>
+            <div className='flex-grow'>
+
+            </div>
+        </div>
     )
 }
