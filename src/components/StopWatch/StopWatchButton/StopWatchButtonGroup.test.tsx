050ae31a9a16cdbb85472757b440786eac324647
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-01-21 17:00:57 -0500

    StopWatchButtonGroup Unit tests
    All Pass

---
+++ src/components/StopWatch/StopWatchButton/StopWatchButtonGroup.test.tsx
@@ -0,0 +1,56 @@
+import '@testing-library/jest-dom/'
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { useStopWatch } from '../hooks/useStopWatch';
+import {StopWatchButtonGroupProps}  from './StopWatchButtonGroup'; // Adjust the import path as needed.
+
+describe('StopWatchButtonGroupProps Component', () => {
+    const mockActions = {
+        start: jest.fn(),
+        stop: jest.fn(),
+        reset: jest.fn(),
+        lap: jest.fn(),
+    };
+
+    it('renders correctly with required props', () => {
+        render(<StopWatchButtonGroupProps darkTheme actions={mockActions} running={false} />);
+        // Assertions to check if the component renders correctly
+        expect(screen.getByText('Start')).toBeInTheDocument();
+        expect(screen.getByText('Reset')).toBeInTheDocument();
+        expect(screen.getByText('Lap')).toBeInTheDocument();
+    });
+
+    it('Start button changes to stop when running', () => {
+        render(<StopWatchButtonGroupProps darkTheme actions={mockActions} running={true} />);
+
+        // Assertions to check if the component renders correctly
+        expect(screen.getByText('Stop')).toBeInTheDocument();
+    });
+    it('Stop button changes to start button when not running', () => {
+        render(<StopWatchButtonGroupProps darkTheme actions={mockActions} running={false} />);
+
+        // Assertions to check if the component renders correctly
+        expect(screen.getByText('Start')).toBeInTheDocument();
+    });
+
+    it('calls the correct action handlers', () => {
+        render(<StopWatchButtonGroupProps darkTheme actions={mockActions} running={false} />);
+
+        fireEvent.click(screen.getByText('Start'));
+        expect(mockActions.start).toHaveBeenCalled();
+
+        fireEvent.click(screen.getByText('Reset'));
+        expect(mockActions.reset).toHaveBeenCalled();
+
+        fireEvent.click(screen.getByText('Lap'));
+        expect(mockActions.lap).toHaveBeenCalled();
+    });
+
+    it('applies custom styles', () => {
+        const customStyles = { backgroundColor: 'blue' };
+        render(<StopWatchButtonGroupProps darkTheme actions={mockActions} running={false} startButtonStyles={customStyles} />);
+
+        const startButton = screen.getByText('Start');
+        expect(startButton).toHaveStyle('backgroundColor: blue');
+    });
+});
