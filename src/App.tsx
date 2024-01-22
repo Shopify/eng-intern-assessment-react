6ed6a44b5990f993bab7e90b2571668f588843d9
Ianniu123 <985683179yzy@gmail.com>
2024-01-21 22:05:21 -0500

    setting up the basic layout for the stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import StopWatch from './StopWatch'
 
 export default function App() {
     return(
-        <div></div>
+        <div>
+            <h1>My Stopwatch</h1>
+            <StopWatch />
+        </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function StopWatch() {
+	//stores time in milliseconds
+	const [time, setTime] = useState(0)
+	const [start, setStart] = useState(false)
+
+
     return(
-        <div></div>
+        <div>
+			<div>
+
+			</div>
+			<div>
+
+			</div>
+			<div>
+			
+			</div>			
+        </div>
     )
 }
