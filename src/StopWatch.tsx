dd9fab192d7d1414b59341a8fbda9b443884b4af
Imran Yafith <imranyafith@gmail.com>
2024-01-22 15:03:46 -0500

    implemented all the basic functionalities of stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  // state to keep track of the time
+  const [time, setTime] = useState(0);
+
+  // state to keep track of whether the stopwatch is running or not
+  const [isRunning, setIsRunning] = useState(false);
+
+  // state to keep track of the laps
+  const [laps, setLaps] = useState<number[]>([]);
+
+  // useEffect runs every time isRunning or time changes
+  useEffect(() => {
+    if (isRunning) {
+      // increments time by 1 every 10 milliseconds
+      const interval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 10);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning, time]);
+
+  // converts time to hours, minutes, seconds, and milliseconds
+  const hours = Math.floor(time / 360000);
+  const minutes = Math.floor((time % 360000) / 6000);
+  const seconds = Math.floor((time % 6000) / 100);
+  const milliseconds = time % 100;
+
+  return (
+    <div>
+      <button onClick={() => setIsRunning(!isRunning)}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button
+        onClick={() => {
+          setTime(0);
+          setIsRunning(false);
+          setLaps([]);
+        }}
+      >
+        Reset
+      </button>
+      {`${hours}:${minutes}:${seconds}:${milliseconds}`}
+      {isRunning && (
+        <button
+          onClick={() => {
+            setLaps((laps) => [...laps, time]);
+          }}
+        >
+          Lap
+        </button>
+      )}
+      <ul>
+        {laps.map((lap) => (
+          <li>{lap}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
