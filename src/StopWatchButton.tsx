5ad3fb2a1765a87b8973fcb8bbe6fd20d7edacf5
Julian Paglia <julianpaglia00@gmail.com>
2024-01-22 23:22:30 -0500

    Added functionality to stopwatch buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/StopWatchButtons.css";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+interface StopWatchButtonProps {
+  onStart: () => void;
+  onStop: () => void;
+  onReset: () => void;
+  onLap: () => void;
+  isRunning: () => boolean;
 }
+
+const StopWatchButton: React.FC<StopWatchButtonProps> = ({
+  onStart,
+  onStop,
+  onReset,
+  onLap,
+  isRunning,
+}) => {
+  let [runningState, setRunningState] = useState(true);
+
+  const buttonStyleStartStop = {
+    background: runningState ? "green" : "red",
+  };
+
+  const buttonStyleResetLap = {
+    background: runningState ? "red" : "green",
+  };
+
+  return (
+    <div>
+      {runningState && (
+        <button
+          style={buttonStyleStartStop}
+          onClick={() => {
+            setRunningState(false);
+            onStart();
+          }}
+        >
+          Start
+        </button>
+      )}
+      {!runningState && (
+        <button
+          style={buttonStyleStartStop}
+          onClick={() => {
+            setRunningState(true);
+            onStop();
+          }}
+        >
+          Stop
+        </button>
+      )}
+      {runningState && (
+        <button
+          style={buttonStyleResetLap}
+          onClick={() => {
+            onReset();
+          }}
+        >
+          Reset
+        </button>
+      )}
+      {!runningState && (
+        <button
+          style={buttonStyleResetLap}
+          onClick={() => {
+            onLap();
+          }}
+        >
+          Lap
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default StopWatchButton;
