b890286aec4c38a09f8570bd81a02cf5fd66dd7e
zhwang2001 <zhwang2001@gmail.com>
2024-01-23 18:39:59 -0500

    added the stop/start, reset and lap buttons

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch(): React.JSX.Element {
     //state management for updating the unconverted time;
-    const [time, setTime] = useState(0);
+    const [time, setTime] = useState<number>(0);
+    //state management for updating start/stop state
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    //state management for storing laps
+    const [laps, setLaps] = useState<string[]>([]);
+
 
     // Hours calculation
     const hours = Math.floor(time / 360000).toString().padStart(2, "0");
@@ -16,6 +22,15 @@ export default function StopWatch(): React.JSX.Element {
     // Timer to be displayed
     const formattedTime = `${hours}:${minutes}:${seconds}:${milliseconds}`
 
+    useEffect(() => {
+        let interval: any
+        if (isRunning) {
+            interval = setInterval(() => {
+                setTime(time + 1);
+            }, 10);
+        }
+        return () => clearInterval(interval)
+    }, [time, isRunning])
 
     return (
         <div style={{
@@ -30,6 +45,18 @@ export default function StopWatch(): React.JSX.Element {
                 width: '500px',
                 margin: '30px',
             }}>{formattedTime}</div>
+            <StopWatchButton
+                formattedTime={formattedTime}
+                laps={laps}
+                setLaps={setLaps}
+                time={time}
+                setTime={setTime}
+                isRunning={isRunning}
+                setIsRunning={setIsRunning}
+
+            />
+            <div>
+            </div>
         </div>
     )
 }
--- src/components/StopWatchButton.tsx
+++ src/components/StopWatchButton.tsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import {Dispatch, SetStateAction} from 'react'
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
+interface StopWatchState {
+    formattedTime: string
+    laps: string[]
+    setLaps: Dispatch<SetStateAction<string[]>>
+    time: number
+    setTime: Dispatch<SetStateAction<number>>
+    isRunning: boolean
+    setIsRunning: (value: boolean) => void;
+}
+
+export default function StopWatchButton({
+                                            formattedTime,
+                                            laps, setLaps,
+                                            time, setTime,
+                                            isRunning, setIsRunning
+                                        }: StopWatchState) {
+
+    const handleStartStop = () => {
+        /*Function for starting and stopping the stopwatch*/
+        setIsRunning(!isRunning)
+    }
+    const handleReset = () => {
+        /*Function for resetting the stopwatch*/
+        setIsRunning(false)
+        setTime(0)
+        setLaps([])
+    }
+    const handleLaps = () => {
+        /*Function for recording laps*/
+        setLaps([...laps, formattedTime])
+    }
+
+    return (
+        <>
+            <div style={{width: '480px', display: 'flex', justifyContent: 'space-between'}}>
+                <button onClick={handleStartStop}>{isRunning ? "Stop" : 'Start'}</button>
+                <button onClick={handleReset}>Reset</button>
+                <button onClick={handleLaps}>Lap</button>
+            </div>
+            <div style={{marginTop: '50px', overflow: 'scroll', height: '500px', width: '480px'}}>
+                {laps.map((lap: string, index: number) => {
+                    return (
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '7px',
+                        }}>
+                            <div key={index}>Lap {index + 1}:</div>
+                            <div>{lap}</div>
+                        </div>)
+                })}
+            </div>
+        </>
     )
 }
