259a786c1e931877ce16ecf9eca236ccb518f9ef
zhwang2001 <zhwang2001@gmail.com>
2024-01-23 17:56:59 -0500

    feature: created and formatted stopwatch display

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
 
-export default function StopWatch() {
-    return(
-        <div></div>
+export default function StopWatch(): React.JSX.Element {
+    //state management for updating the unconverted time;
+    const [time, setTime] = useState(0);
+
+    // Hours calculation
+    const hours = Math.floor(time / 360000).toString().padStart(2, "0");
+    // Minutes calculation
+    const minutes = Math.floor((time % 360000) / 6000).toString().padStart(2, "0");
+    // Seconds calculation
+    const seconds = Math.floor((time % 6000) / 100).toString().padStart(2, "0");
+    // Milliseconds calculation
+    const milliseconds = (time % 100).toString().padStart(2, "0");
+    // Timer to be displayed
+    const formattedTime = `${hours}:${minutes}:${seconds}:${milliseconds}`
+
+
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%'
+        }}>
+            <div style={{
+                fontSize: '90px',
+                width: '500px',
+                margin: '30px',
+            }}>{formattedTime}</div>
+        </div>
     )
 }
