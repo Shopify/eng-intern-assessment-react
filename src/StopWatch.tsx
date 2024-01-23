ba9aa6ff7a1c7e0692d182caa4839fbbb052b4fa
Meredith Jonatan <mjonatan@me.com>
2024-01-23 13:15:12 -0500

    add numbers logic

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function StopWatch() {
+
+    const [timeInSeconds, setTimeInSeconds] = useState<number>(0)
+    const [stopWatchTime, setStopWatchTime] = useState<Array<number | string>>([])
+
+    function calculateTime(timeInSeconds: number): Array<number | string> {
+
+        let hours: number= Math.floor(timeInSeconds/ 3600);
+        let minutes: number= Math.floor((timeInSeconds - (hours * 3600)) / 60);
+        let seconds: number= timeInSeconds - (hours * 3600) - (minutes * 60);
+
+        let hoursFormat = hours < 10 ? `0${hours}` : hours;
+        let minutesFormat = minutes < 10 ? `0${minutes}` : minutes;
+        let secondsFormat = seconds < 10 ? `0${seconds}` : seconds;
+
+        return [hoursFormat, minutesFormat, secondsFormat]
+    }
+
+    useEffect (() => {
+        let stopWatchArray: Array<number | string> = calculateTime(timeInSeconds);
+        setStopWatchTime(stopWatchArray)
+    }, [timeInSeconds])
+    
+
     return(
-        <div></div>
+        <section className='stopwatch_container'>
+            <span className='stopwatch_text'>{stopWatchTime[0]}</span>
+            <span>:</span>
+            <span className='stopwatch_text'>{stopWatchTime[1]}</span>
+            <span>:</span>
+            <span className='stopwatch_text'>{stopWatchTime[2]}</span>
+        </section>
     )
 }
