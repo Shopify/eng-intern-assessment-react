282a7a36c0fdf7203a49d16911237e4a27b5a069
Daniel Tan <danieltan380@gmail.com>
2024-01-22 21:59:54 -0800

    Refactor calculateTime to be more readable. Split formatting time into a separate function.

--- src/helpers/calculateTime.ts
+++ src/helpers/calculateTime.ts
@@ -1,16 +1,24 @@
-//Helper function for calculating and formatting time. Returns an array.
+export interface TimeFormat {
+  minutesFormatted: string;
+  secondsFormatted: string;
+  millisecondsFormatted: string;
+};
 
-const calculateTime = (timeInSeconds: number): Array<number|string> => {
+const formatTime = (value: number, length: number): string => {
+  return String(value).padStart(length, '0');
+}
+
+const calculateTime = (timeInSeconds: number): TimeFormat => {
   
-  const milliseconds = (timeInSeconds / 10) % 100;
-  const minutes = Math.floor((timeInSeconds / 60000) % 60)
+  const milliseconds = ((timeInSeconds / 10) % 100);
+  const minutes = Math.floor((timeInSeconds / 60000) % 60);
   const seconds = Math.floor((timeInSeconds / 1000) % 60);
 
-  let minutesFormat = `0${minutes}`.slice(-2);
-  let secondsFormat = `0${seconds}`.slice(-2);
-  let millisecondsFormat = `0${milliseconds}`.slice(-2);
+  const minutesFormatted = formatTime(minutes, 2);
+  const secondsFormatted = formatTime(seconds, 2);
+  const millisecondsFormatted = formatTime(milliseconds, 2)
 
-  return [minutesFormat, secondsFormat, millisecondsFormat];
+  return { minutesFormatted, secondsFormatted, millisecondsFormatted };
 }
 
 export default calculateTime;
