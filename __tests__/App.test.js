91fa0286c7728f4135769d3b83c7b4f897850ecb
Justin Charney <justin.charney@gmail.com>
2024-01-23 11:43:20 -0500

    added tests and refactored for reusability

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -16,11 +16,11 @@ afterEach(() => {
 });
 
 describe('StopWatch', () => {
-    test("initialization test", () => {
+    test("should initially display the timer at zero", () => {
         expect(screen.getByText("00:00:00:00"));
     });
 
-    test("check start functionality", () => {
+    test("should start the timer and display updated time when Start is clicked", () => {
         fireEvent.click(screen.getByText("Start"));
         expect(screen.getByText("Stop")).toBeTruthy();
         expect(screen.getByText("Lap")).toBeTruthy();
@@ -30,7 +30,7 @@ describe('StopWatch', () => {
         expect(screen.getByText("00:00:01:00")).toBeTruthy();
     });
 
-    test("check stop functionality", ()=> {
+    test("should stop the timer at the current time when Stop is clicked", ()=> {
         fireEvent.click(screen.getByText("Start"));
         act(() => {
             clock.tick(5000)
@@ -40,4 +40,61 @@ describe('StopWatch', () => {
         expect(screen.getByText("Reset")).toBeTruthy();
         expect(screen.getByText("00:00:05:00")).toBeTruthy();
     });
+
+    test("should resume the timer from the stopped time when Start is clicked after Stop", ()=> {
+        fireEvent.click(screen.getByText("Start"));
+        act(() => {
+            clock.tick(3000)
+        });
+        fireEvent.click(screen.getByText("Stop"));
+        fireEvent.click(screen.getByText("Start"));
+        act(() => {
+            clock.tick(3000)
+        });
+        fireEvent.click(screen.getByText("Stop"));
+        expect(screen.getByText("00:00:06:00")).toBeTruthy();
+    });
+
+    test("should record and display lap times correctly when Lap is clicked", ()=> {
+        fireEvent.click(screen.getByText("Start"));
+        act(() => {
+            clock.tick(3000)
+        });
+        fireEvent.click(screen.getByText("Lap"));
+        expect(screen.getByText("Lap 1: 00:00:03:00")).toBeTruthy();
+        act(() => {
+            clock.tick(1000)
+        });
+        fireEvent.click(screen.getByText("Lap"));
+        expect(screen.getByText("Lap 2: 00:00:01:00")).toBeTruthy();
+    });
+
+    test('should display the time in the correct format for hours, minutes, seconds, and millisecondst', () => {
+        fireEvent.click(screen.getByText("Start"));
+        act(() => {
+            clock.tick(3661000)
+        });
+        expect(screen.getByText("01:01:01:00")).toBeTruthy();
+    });
+
+    test("should reset the timer to zero and remove all laps when Reset is clicked", () => {
+        fireEvent.click(screen.getByText("Start"));
+        act(() => {
+            clock.tick(1000);
+        });
+        fireEvent.click(screen.getByText("Lap"));
+        act(() => {
+            clock.tick(2000);
+        });
+        fireEvent.click(screen.getByText("Lap"));
+        fireEvent.click(screen.getByText("Stop"));
+        fireEvent.click(screen.getByText("Reset"));
+        // Check if the timer is reset to zero
+        expect(screen.getByText("00:00:00:00")).toBeTruthy();
+        // Check if the laps are removed
+        expect(screen.queryByText("Lap 1: 00:00:01:00")).toBeNull();
+        expect(screen.queryByText("Lap 2: 00:00:02:00")).toBeNull();
+    });
+
+
 });
--- src/StopWatch/StopWatch.tsx
+++ src/StopWatch/StopWatch.tsx
@@ -1,55 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import styles from './StopWatch.module.css'
 import StopWatchButton from "../StopWatchButton/StopWatchButton";
+import {useStopWatch} from "../hooks/useStopWatch";
+import {formatTime} from "../utils/timeUtils";
 
 export default function StopWatch() {
-    const [time, setTime] = useState<number>(0);
-    const [running, setRunning] = useState<boolean>(false);
-    const [laps, setLaps] = useState<number[]>([]);
-    const [lastLapTime, setLastLapTime] = useState<number>(0);
-    const [referenceTime, setReferenceTime] = useState<number>(Date.now());
+    const { time, running, laps, toggleStartStop, handleLapReset} = useStopWatch();
 
     const shortestLapTime = laps.length > 0 ? Math.min(...laps) : 0;
     const longestLapTime = laps.length > 0 ? Math.max(...laps) : 0;
-
-    useEffect(() => {
-        let timeoutId: NodeJS.Timeout;
-        if(running){
-            const updateStopwatch = () => {
-                setTime(prevTime => {
-                    const now = Date.now();
-                    const interval = now - referenceTime; // This takes into account drift in callback execution
-                    setReferenceTime(now)
-                    return prevTime + interval;
-                })
-            }
-
-            timeoutId = setTimeout(updateStopwatch, 10)
-        }
-
-        return () => {
-            clearTimeout(timeoutId); // Clear the timeout when the stopwatch stops or the component unmounts
-        };
-    }, [running, time]);
-
-    const toggleStartStop = () => {
-        setRunning(!running)
-        if (!running) {
-            setReferenceTime(Date.now());
-        }
-    };
-    const handleLapReset = () => {
-        if (running) {
-            const lapTime = time - lastLapTime;
-            setLaps([...laps, lapTime]) //  Create a new array with existing lap times + new time. Update laps.
-            setLastLapTime(time);
-        } else {
-            setRunning(false);
-            setTime(0);
-            setLaps([]);
-            setLastLapTime(0);
-        }
-    }
 
     return (
         <div className={styles.stopwatch}>
@@ -73,8 +32,4 @@ export default function StopWatch() {
         </div>
 
     )
-}
-
-function formatTime(time: number) {
-    return `${("0" + Math.floor(time / 3600000)).slice(-2)}:${("0" + Math.floor((time % 3600000) / 60000)).slice(-2)}:${("0" + Math.floor((time % 60000) / 1000)).slice(-2)}:${("0" + Math.floor((time % 1000) / 10)).slice(-2)}`;
 }
