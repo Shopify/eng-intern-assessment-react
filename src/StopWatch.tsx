83f202435d993108bd1c45bb9a005456bc0cc016
shaded-shade <shahid.salha02@gmail.com>
2024-01-23 17:35:30 -0500

    Basic working stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import StopWatchButton from './StopWatchButton'
 
 export default function StopWatch() {
+
+    const [time, setTime] = React.useState(0)
+    const [timerOn, setTimerOn] = React.useState(false)
+    const [laps, setLaps] = useState<number[]>([])
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout | null = null
+
+        if(timerOn) {
+            interval = setInterval(() => {
+                setTime(prevTime => prevTime + 10)
+            }, 10)
+
+            console.log(laps)
+        }else if(!timerOn && interval) {
+            clearInterval(interval)
+        }
+
+        return () => clearInterval(interval!)
+    }, [timerOn]);
+
+
+    /*
+    This function handles the start/stop button
+        If the timerOn is true, the timerOn should be set to false
+        If the timerOn is false, the timerOn should be set to true
+    */
+    const handleStartStop = () => {
+        setTimerOn(!timerOn) 
+        console.log(timerOn)
+    }
+
+    /*
+    This function handles the lap button
+        it should add the current time to the laps array
+    */
+    const handleLap = () => {
+        setLaps([...laps, time])
+        console.log('Lap = ' + time/1000)
+        console.log(laps)
+        setTime(0)
+        
+    }
+
+    /*
+    This function handles the reset button
+        it resets all values to their initial values
+    */
+    const handleReset = () => {
+        setTimerOn(false)
+        setTime(0)
+        setLaps([])
+        console.log('Reset')
+    }
+
     return(
-        <div></div>
+        <div>
+            <h1>Stopwatch</h1>
+            {(time/1000).toFixed(2)}
+            <div id='buttons'>
+                <StopWatchButton onClick={handleStartStop} displayText={timerOn ? 'Stop' : 'Start'}/>
+                <StopWatchButton onClick={handleLap} displayText='Lap'/>
+                <StopWatchButton onClick={handleReset} displayText='Reset'/>
+            </div>
+
+            <div id='lapsList'>
+                {/*Display the laps here*/}
+                {laps.length > 0 && (
+                    <ul>
+                        {laps.map((lap, index) => {
+                            return <li key={index}>Lap {index + 1} - {(lap/1000).toFixed(2)}</li>
+                        })}
+                    </ul>
+                )}
+            </div>
+        </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default function StopWatchButton() {
+// Component that renders a button with the text passed in from the parent component, StopWatch, as a prop
+const StopWatchButton = ({displayText, onClick}: {displayText: string, onClick: () => void}) => {
     return(
-        <div></div>
+        <div>
+            <button onClick={onClick}>{displayText}</button>
+        </div>
     )
 }
+
+export default StopWatchButton;
