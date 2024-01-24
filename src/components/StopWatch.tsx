4199cb15260ce4e88f5f7d9debb9b3059b981dbb
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-01-24 02:51:37 -0500

    Restructure file

---
+++ src/components/StopWatch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/StopWatch.css';
+
+const StopWatch: React.FC = () => {
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default StopWatch;
---
+++ src/components/StopWatchButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/StopWatchButton.css';
+
+const StopWatchButton: React.FC = () => {
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default StopWatchButton;
