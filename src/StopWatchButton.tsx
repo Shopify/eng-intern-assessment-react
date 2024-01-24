3668d766913431a4cd4aa5641f52d2406a68b07f
Jason Gao <jasonzgao@gmail.com>
2024-01-23 20:35:54 -0500

    project setup

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 
+/* 
+Requirements:
+    - The stopwatch should start counting when the user clicks the start button.
+    - The stopwatch should stop counting when the user clicks the stop button.
+    - The stopwatch should reset to zero when the user clicks the reset button.
+    - The stopwatch should record and display laps when user clicks the lap button.
+
+We will model this stopwatch after the iOS version. This means:
+    - Stopwatch timer continuously shows the running time
+    - Laps are listed in descending order (latest lap first) and show the interval from last lap
+    - Stop button replaces start button and vice versa
+    - Reset button clears the laps and running timer
+*/
+
 export default function StopWatchButton() {
     return(
         <div></div>
