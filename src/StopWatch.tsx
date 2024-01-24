ccbbf21d2b7a59d64b5e28263c3e1bbe5cddeee1
diego <diego.gonzalezdelgado@mail.utoronto.ca>
2024-01-24 18:34:05 -0500

    improved functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -4,15 +4,19 @@ import StopWatchButton from './StopWatchButton';
 type firstDigits = 11 | 14; // have the first digits be hours or minutes
 type lastDigits = 19 | 21 | 22; // have the last digits be seconds, tenths of a second, or hundredths of a second
 
+const divStyling = {
+    fontFamily: "sans-serif",
+    alignContent: "middle"
+}
+
 export default function Stopwatch(){
   const [time, setTime] = useState<number>(0);
   const [running, setRunning] = useState<boolean>(false);
   const [laps, setLaps] = useState<number[]>([]);
-  const [stoppedLaps, setStoppedLaps] = useState<boolean>(false);
   const firstDigits: firstDigits = 14;
-  const lastDigits: lastDigits = 21;
+  const lastDigits: lastDigits = 22;
 
-
+  
   // use effect block will update the time every 10 milliseconds
   useEffect(() => {
     let interval: NodeJS.Timeout;
@@ -27,30 +31,35 @@ export default function Stopwatch(){
 
   const startStopwatch = (): void => {
     setRunning(true)
-    setStoppedLaps(false)
   };
   const stopStopwatch = (): void => setRunning(false);
   const lapStopwatch = (): void => {
     setLaps([...laps, time])
     if (!running){
-        setStoppedLaps(true)
     }
   };
   const resetStopwatch = (): void => {
     setTime(0);
     setLaps([]);
     setRunning(false);
-    setStoppedLaps(false)
   };
 
   return (
-    <div>
+    <div style={divStyling}>
       <h2>Stopwatch</h2>
       <div>Time: {new Date(time).toISOString().slice(firstDigits, lastDigits)}</div>
-      <StopWatchButton onClick={startStopwatch} text="Start" />
-      <StopWatchButton onClick={stopStopwatch} text="Stop" />
-      <StopWatchButton onClick={lapStopwatch} text="Lap" />
-      <StopWatchButton onClick={resetStopwatch} text="Reset" />
+      { running ? (
+      <>
+        <StopWatchButton onClick={stopStopwatch} text="Stop" />
+        <StopWatchButton onClick={lapStopwatch} text="Lap" />
+      </>):(
+      <>
+        <StopWatchButton onClick={startStopwatch} text="Start" />
+        <StopWatchButton onClick={resetStopwatch} text="Reset" />
+        
+      </>)
+      }
+      {/* Displays lap times */}
       {laps.length > 0 && (
         <ul>
           {laps.map((lap, index) => (
