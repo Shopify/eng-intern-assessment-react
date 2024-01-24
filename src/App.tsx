20f5d7e55fb28b353bce8eabd492bbaa391b45f1
Jason Huang <jasonhuang20035@gmail.com>
2024-01-24 22:01:59 +0000

    improve performance and accuracy of timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import StopWatch from "./StopWatch";
 import StopWatchButton from "./StopWatchButton";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-const formatMs = (ms: number) => {
+export const formatMs = (ms: number) => {
+  // round ms to integer
+  ms = Math.round(ms);
+
   const minutes = Math.floor(ms / 1000 / 60);
   ms = ms - minutes * 1000 * 60;
 
@@ -12,7 +15,7 @@ const formatMs = (ms: number) => {
 
   const minutesStr = minutes.toString().padStart(2, "0");
   const secondsStr = seconds.toString().padStart(2, "0");
-  const msStr = (ms / 10).toString().padStart(2, "0");
+  const msStr = ms.toString().padStart(3, "0");
   return `${minutesStr}:${secondsStr}:${msStr}`;
 };
 
@@ -22,73 +25,78 @@ const formatMs = (ms: number) => {
  * and holds the state for the timer itself.
  */
 export default function App() {
-  const [time, setTime] = useState(0);
+  // timer state
   const [running, setRunning] = useState(false);
-  const [lapTimes, setLapTimes] = useState<Array<number>>([]);
-  const [parent] = useAutoAnimate();
 
-  // hook to update the time when the timer is running
+  // timerMs at the time the pause button was clicked
+  const [pausedMs, setPausedMs] = useState(0);
+  // Date.now() from when timer start button was clicked
+  const [startMs, setStartMs] = useState<number>(undefined);
+
+  // ms elapsed, displayed on timer
+  const [timerMs, setTimerMs] = useState(0);
+  // memoize formatted timer string to reduce rerenders
+  const runningTimeString = useMemo(() => formatMs(timerMs), [timerMs]);
+
+  // list of lap times
+  const [lapTimes, setLapTimes] = useState<Array<string>>([]);
+
+  const [lapsParent] = useAutoAnimate();
+
+  // timer loop
   useEffect(() => {
-    let id: NodeJS.Timer;
+    let intervalId: NodeJS.Timer;
     if (running) {
-      // we set a delay when updating the time if the timer is running
-      id = setInterval(() => {
-        setTime((time) => time + 10);
+      intervalId = setInterval(() => {
+        // running ms = paused ms + diff since last paused
+        setTimerMs(pausedMs + performance.now() - startMs);
       }, 10);
-    } else if (!running) {
-      clearInterval(id);
     }
-
-    // and wait for the interval to elapse before returning in the callback
-    return () => clearInterval(id);
+    return () => clearInterval(intervalId);
   }, [running]);
+
+  const startTimer = () => {
+    console.log("Start");
+    setStartMs(performance.now());
+    setRunning(true);
+  };
+
+  const stopTimer = () => {
+    console.log("Stop");
+    setRunning(false);
+    // update pausedMs
+    setPausedMs(timerMs);
+  };
+
+  const resetTimer = () => {
+    console.log("Reset");
+    setRunning(false);
+    // reset runningMs and pausedMs
+    setTimerMs(0);
+    setPausedMs(0);
+  };
+
+  const addLap = () => {
+    console.log("Lap");
+    setLapTimes([runningTimeString, ...lapTimes]);
+  };
 
   return (
     <main className="mx-auto max-w-3xl p-5">
       <h1 className="text-4xl">Stopwatch</h1>
-      <StopWatch>{formatMs(time ?? 0)}</StopWatch>
+      <StopWatch>{runningTimeString}</StopWatch>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-        <StopWatchButton
-          onClick={() => {
-            console.log("Start");
-            setRunning(true);
-          }}
-        >
-          Start
-        </StopWatchButton>
-        <StopWatchButton
-          onClick={() => {
-            console.log("Stop");
-            setRunning(false);
-          }}
-        >
-          Stop
-        </StopWatchButton>
-        <StopWatchButton
-          onClick={() => {
-            console.log("Lap");
-            setLapTimes((lapTimes) => [time, ...lapTimes]);
-          }}
-        >
-          Lap
-        </StopWatchButton>
-        <StopWatchButton
-          onClick={() => {
-            console.log("Reset");
-            setRunning(false);
-            setTime(0);
-            setLapTimes([]);
-          }}
-        >
-          Reset
-        </StopWatchButton>
+        <StopWatchButton onClick={startTimer}>Start</StopWatchButton>
+        <StopWatchButton onClick={stopTimer}>Stop</StopWatchButton>
+        <StopWatchButton onClick={addLap}>Lap</StopWatchButton>
+        <StopWatchButton onClick={resetTimer}>Reset</StopWatchButton>
       </div>
-      <ul className="text-2xl" ref={parent}>
+      <ul className="text-2xl" ref={lapsParent}>
         {lapTimes.map((lapTime, idx) => {
           return (
             <li key={lapTimes.length - idx}>
               Lap {(lapTimes.length - idx).toString().padStart(2, "0")}:{" "}
-              {formatMs(lapTime)}
+              {lapTime}
             </li>
           );
         })}
