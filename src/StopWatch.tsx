d4a570cda52d5846e07e8d1ba17276a5fca295d2
sfbarts <sfbedu@gmail.com>
2024-01-25 15:10:27 -0500

    created stopTime function to pause duration update

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { start } from "repl";
 
 //define time proportions
 const msInSecond = 1000;
@@ -12,19 +13,33 @@ export default function StopWatch() {
   //lastTick is used to improve reliability in the actual calculation of duration in milliseconds
   const lastTick = useRef(null);
   const [duration, setDuration] = useState<number | null>(null);
+  //timer holds the setInterval so that it can be stopped.
+  const [timer, setTimer] = useState<number | null>(null);
 
   //startTime calculates and sets the total duration of the timer in milliseconds.
   const startTime = () => {
     lastTick.current = Date.now();
-    window.setInterval(() => {
-      const now = Date.now();
-      const deltaTime = now - lastTick.current;
-      setDuration((d) => d + deltaTime);
-      lastTick.current = now;
-    }, 1);
+    setTimer(
+      window.setInterval(() => {
+        const now = Date.now();
+        const deltaTime = now - lastTick.current;
+        setDuration((d) => d + deltaTime);
+        lastTick.current = now;
+      }, 1)
+    );
   };
 
-  startTime();
+  const stopTime = () => {
+    window.clearInterval(timer);
+    // setTimer(null);
+    console.log(timer);
+  };
 
-  return <p>{duration}</p>;
+  return (
+    <>
+      <p>{duration}</p>
+      <button onClick={startTime}>Start</button>
+      <button onClick={stopTime}>Stop</button>
+    </>
+  );
 }
