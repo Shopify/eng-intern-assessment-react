9d14851cbf1545f9e27efd61bffc551a7f00fc55
Francisco <fmartinezv2002@gmail.com>
2024-01-25 13:29:06 -0300

    Implemented the project :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import StopWatch from "./StopWatch";
+import StopWatchButton from "./StopWatchButton";
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  const [timePassed, setTimePassed] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimePassed((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (!isRunning && timePassed !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isRunning, timePassed]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTimePassed(0);
+    setIsRunning(false);
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <StopWatch timePassed={timePassed} />
+      <StopWatchButton action="Start" handleClick={handleStart} />
+      <StopWatchButton action="Stop" handleClick={handleStop} />
+      <StopWatchButton action="Reset" handleClick={handleReset} />
+    </div>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+// StopWatchButton interface to define the props that it is going to accept
+interface StopWatchButtonProps {
+  action: string;
+  handleClick: () => void;
 }
+
+// The StopWatchButton component
+const StopWatchButton: React.FC<StopWatchButtonProps> = ({
+  action,
+  handleClick,
+}) => {
+  return (
+    <button onClick={handleClick}>
+      {action.charAt(0).toUpperCase() + action.slice(1)} {}
+    </button>
+  );
+};
+
+export default StopWatchButton;
