fe6108b2d13e74d23f5f8eebd5443ccfb4a155cf
JeanJoblin <invisibleorbs@gmail.com>
2024-01-25 15:53:57 -0500

    Build Basic Stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import StopWatch from './StopWatch'
+import StopWatchButton from './StopWatchButton'
 
 export default function App() {
+    const [counter, setCounter] = useState(0);
+    const [secCounter, setSecCounter] = useState(0);
+    const [running, setRunning] = useState(false);
+    const [laps, setLaps] = useState([]);
+
+    useEffect(() => {
+        if(running) {
+            const timer = setInterval(() => {
+                setCounter(c => c + 4);
+            }, 4);
+            // 4 is chosen here because updating any faster results in incorrect timing
+            //BE SURE TO RECHECK BEFORE SUBMITTING
+        return () => clearInterval(timer);
+        }
+    }, [running]);
+
+    useEffect(() => {
+        if(running) {
+            const timer = setInterval(() => {
+                setSecCounter(c => c + 1);
+            }, 1000);
+        return () => clearInterval(timer);
+        }
+    }, [running]);
+
+    
+function toggleTimer() {
+    setRunning(r => !r);
+}
+
+function reset() {
+    setCounter(0);
+    setSecCounter(0);
+}
+
+function lap() {
+    setLaps(l => [...l, counter]);
+    reset();
+}
+
     return(
-        <div></div>
+        <div>
+            <StopWatch time={counter} />
+            <StopWatch time={secCounter} />
+            <StopWatchButton onclick={toggleTimer} name='Play/Pause' />
+            <StopWatchButton onclick={lap} name='Lap' />
+            <StopWatchButton onclick={reset} name='Reset' />
+            {laps.length > 0 && <div>
+                <h2>Laps:</h2>
+                <ul>
+                    {laps.map((lap: number, ind: number) => {
+                        const timeString: string = Math.floor(lap / 60000) + ":" + Math.floor(lap / 1000) + ":" + lap % 1000;
+                        return (
+                            <li>
+                                <p>Lap {ind+1}: {timeString}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>}
+        </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+export default function StopWatch(props: { time: number }) {
+  {
+    const min = Math.floor(props.time / 60000);
+    const sec = Math.floor(props.time / 1000) % 1000;
+    const msec = props.time % 1000;
+
+    return (
+      <div>
+        {min < 10 ? "0" + min : min}:{sec < 10 ? "0" + sec : sec}:
+        {msec > 99 ? msec : msec > 9 ? "0" + msec : "00" + msec}
+      </div>
+    );
+  }
 }
