502fffc003d5118e83d5f5850dcc06c542cb4d5d
Parin Parikh <parinparikh1@gmail.com>
2024-01-25 17:05:06 -0500

    add buttons and time span

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function App() {
     return(
-        <div></div>
+        <div>
+            <p><span id="seconds">00</span>:<span id="tens">00</span></p>
+            <button id="button-start">Start</button>
+            <button id="button-stop">Stop</button>
+            <button id="button-reset">Reset</button>
+            <button id="button-lap">Lap</button>
+        </div>
     )
 }
