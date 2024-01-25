e4e23d8d3ecc419f9ec3d873e738438f85213efc
beatuano <beatuano@gmail.com>
2024-01-24 21:22:38 -0500

    usestate, time format and buttons added

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
 
 export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+    const [timer, setTimer] = useState<number>(0)
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div>
+        <span>{"0" + Math.floor((timer / 60000) % 60)};</span>
+        <span>{"0" + Math.floor((timer / 1000) % 60)};</span>
+        <span>{"0" + Math.floor((timer / 10) % 100)};</span>
+
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+        <button>Lap</button>
+      </div>
+    </div>
+  );
 }
+
