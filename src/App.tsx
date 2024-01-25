50d1e8c852594a1b6d152b2e36e7b7a10aa0bb01
sfbarts <sfbedu@gmail.com>
2024-01-25 14:57:24 -0500

    created startTime function that updates duration in ms

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import StopWatch from "./StopWatch";
 
 export default function App() {
-  return <div></div>;
+  return <StopWatch />;
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
+//define time proportions
+const msInSecond = 1000;
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const msInMinute = secondsInMinute * msInSecond;
+const msInHour = minutesInHour * msInMinute;
+
+//StopWatch handles all timing functionality.
 export default function StopWatch() {
-  return <div></div>;
+  //lastTick is used to improve reliability in the actual calculation of duration in milliseconds
+  const lastTick = useRef(null);
+  const [duration, setDuration] = useState<number | null>(null);
+
+  //startTime calculates and sets the total duration of the timer in milliseconds.
+  const startTime = () => {
+    lastTick.current = Date.now();
+    window.setInterval(() => {
+      const now = Date.now();
+      const deltaTime = now - lastTick.current;
+      setDuration((d) => d + deltaTime);
+      lastTick.current = now;
+    }, 1);
+  };
+
+  startTime();
+
+  return <p>{duration}</p>;
 }
