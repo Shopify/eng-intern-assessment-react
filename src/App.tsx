ac8c9c4f4d32bc238b878f2211cea52d584f9696
prach19 <prachi19@my.yorku.ca>
2024-01-25 18:40:27 -0500

    resetting files, restarting with right structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 export default function App() {
-    const [timeSec, setTimeSec] = useState<number>(0);
-    const [watchArray, setWatchArray] = useState<Array<number>>([])
-
-    useEffect(() => {
-        let watchArray: Array<number> = calcTime(timeSec);
-        setWatchArray(watchArray);
-    }, [timeSec]);
-
     return(
-        <div>
-            <p className='watch-text'>{watchArray[0]}</p>
-            <span>:</span>
-            <p className='watch-text'>{watchArray[1]}</p>
-            <span>:</span>
-            <p className='watch-text'>{watchArray[2]}</p>
-            
-        </div>
-    );
-}
-
-function calcTime(timeSec: number): Array<number>{
-    const convert = 3600;
-    
-    let hrs: number = Math.floor(timeSec / convert);
-    let mins: number = Math.floor((timeSec - (hrs * convert))/60);
-    let secs: number = Math.floor(timeSec - (hrs * convert) - (mins*60));
-
-
-
-    return[
-        hrs,
-        mins,
-        secs
-    ];
+        <div></div>
+    )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function StopWatch() {
+    const [seconds, setSeconds] = useState<number>(0)
+    const [minutes, setMinutes] = useState<number>(0)
+    const [hours, setHours] = useState<number>(0)
+    const [laps, setLaps] = useState<Array<number>>([])
+    const [run, setRun] = useState<boolean>(false)
+
+    const formatZero = (num: number): string => {
+        if(num < 10){
+            return "0" + num
+        }
+
+        else return num.toString()
+    }
+
+    useEffect(() => {
+        let interval: number;
+
+        if(run){
+            interval = setInterval(() => {
+                setSeconds((seconds: number) => {
+                    if(seconds >= 59){
+                        setMinutes((minutes: number) =>{
+                            if(minutes >= 59){
+                                setHours((hours: number) => hours + 1);
+                                return 0;
+                            } else {
+                                return minutes + 1;
+                            }
+                        });
+
+                        return 0;
+                    } else {
+                        return seconds + 1;
+                    }
+
+                }
+                
+                
+                )
+            })
+        }
+    
+    
+}, )
+
+
+
     return(
         <div></div>
     )
