18c00cc986fdddb992a1baa5e2a343291d8b32a4
School <School@dhcp-81-44.cu-internet.carleton.ca>
2024-01-25 14:02:11 -0500

    Created button component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,20 @@
+//Import needed modules
 import React from 'react'
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+
+//Define Properties for the buttons (Taking in onClicks)
+interface ButtonProperties {
+    onClick:() => void;
+    buttonLabel: string;
 }
+
+//Create a button component and a render method that returns how the button is displayed
+const StopwatchButton: 
+React.FC<ButtonProperties> = ({ onClick, buttonLabel}) => {
+    return (
+        <button onClick = {onClick}>{buttonLabel}</button>
+    );
+}
+
+//Export the component for use
+export default StopwatchButton;
