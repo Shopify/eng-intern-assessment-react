5ca10ca658946151b836c0e825fca9ed93a139ae
Jason Huang <jasonhuang20035@gmail.com>
2024-01-25 21:30:11 +0000

    add reset test when stopped

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -93,3 +93,58 @@ it("resets time correctly when running", async () => {
   expect(queryByText("00:00:000")).toBeTruthy();
 });
 
+it("resets time correctly when stopped", async () => {
+  jest.useFakeTimers();
+
+  const { queryByText } = render(<App />);
+
+  // buttons exist
+  const [start, stop, reset] = ["Start", "Stop", "Reset"].map((t) =>
+    queryByText(t),
+  );
+  [start, stop, reset].forEach((e) => expect(e).toBeTruthy());
+
+  // timer starts at 0
+  expect(queryByText("00:00:000")).toBeTruthy();
+
+  act(() => {
+    fireEvent.click(start);
+  });
+
+  expect(queryByText("00:00:000")).toBeTruthy();
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
+  // after 1 second passes, the timer displays exactly 1 second
+  expect(queryByText("00:01:000")).toBeTruthy();
+
+  act(() => {
+    fireEvent.click(stop);
+  });
+
+  expect(queryByText("00:01:000")).toBeTruthy();
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
+  // the timer does not advance when it's stopped
+  expect(queryByText("00:01:000")).toBeTruthy();
+
+  act(() => {
+    fireEvent.click(reset);
+  });
+
+  // the reset button stops and resets displayed time
+  expect(queryByText("00:00:000")).toBeTruthy();
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
+  // the timer does not advance when it's stopped
+  expect(queryByText("00:00:000")).toBeTruthy();
+});
+
