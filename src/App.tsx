82540551182da2be89fa1ae74fe71b1556f3fc56
Jason Huang <jasonhuang20035@gmail.com>
2024-01-25 22:37:44 +0000

    improve documentation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import StopWatch from "./StopWatch";
 import StopWatchButton from "./StopWatchButton";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-export const formatMs = (ms: number) => {
+/**
+ * Format milliseconds elapsed into a human-readable string
+ * @param ms Milliseconds elapsed
+ * @returns Formatted timer string in HH:MM:SS.sss
+ */
+export const formatMs = (ms: number): string => {
   // round ms to integer
   ms = Math.round(ms);
 
+  // break ms into components and compute the value for each
   const hours = Math.floor(ms / 1000 / 60 / 60);
   ms = ms - hours * 1000 * 60 * 60;
 
@@ -16,15 +22,16 @@ export const formatMs = (ms: number) => {
   const seconds = Math.floor(ms / 1000);
   ms = ms - seconds * 1000;
 
+  // format components with 2 or 3 leading zeroes as needed
   const hoursStr = hours.toString().padStart(2, "0");
   const minutesStr = minutes.toString().padStart(2, "0");
   const secondsStr = seconds.toString().padStart(2, "0");
   const msStr = ms.toString().padStart(3, "0");
+
   return `${hoursStr}:${minutesStr}:${secondsStr}.${msStr}`;
 };
 
 /**
- * Stopwatch App
  * This component handles the coordination between the display and buttons,
  * and holds the state for the timer itself.
  */
@@ -53,6 +60,8 @@ export default function App() {
     if (running) {
       intervalId = setInterval(() => {
         // running ms = paused ms + diff since last paused
+        // since we are using performance.now(),
+        // accuracy is not compromised by high frequency refreshing
         setTimerMs(pausedMs + performance.now() - startMs);
       }, 1);
     }
@@ -68,7 +77,7 @@ export default function App() {
   const stopTimer = () => {
     console.debug("Stop");
     setRunning(false);
-    // update pausedMs
+    // update pausedMs for timer restart
     setPausedMs(timerMs);
   };
 
@@ -84,6 +93,7 @@ export default function App() {
 
   const addLap = () => {
     console.debug("Lap");
+    // add lap to top of array, so most recent laps are displayed first
     setLapTimes([runningTimeString, ...lapTimes]);
   };
 
@@ -100,7 +110,9 @@ export default function App() {
       <ul className="text-2xl" ref={lapsParent}>
         {lapTimes.map((lapTime, idx) => {
           return (
+            // reverse index keys so they stay stable when adding laps from the top
             <li key={lapTimes.length - idx}>
+              {/* use string index to generate lap number */}
               Lap {(lapTimes.length - idx).toString().padStart(2, "0")}:{" "}
               {lapTime}
             </li>
