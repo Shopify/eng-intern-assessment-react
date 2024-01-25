28bf8ce3dae678caf9cd9b69aef15b43f2ae4838
Daniel Tan <danieltan380@gmail.com>
2024-01-24 21:22:02 -0800

    Name values used in calculation for milliseconds, minutes, and seconds.

--- src/helpers/calculateTime.ts
+++ src/helpers/calculateTime.ts
@@ -9,10 +9,15 @@ const formatTime = (value: number, length: number): string => {
 }
 
 const calculateTime = (timeInSeconds: number): TimeFormat => {
+  const millisecondsPerInterval = 10;
+  const maxMilliseconds = 100;
+  const millisecondsInMinute = 60000;
+  const secondsInMinute = 60;
+  const millisecondsInSecond = 1000;
   
-  const milliseconds = ((timeInSeconds / 10) % 100);
-  const minutes = Math.floor((timeInSeconds / 60000) % 60);
-  const seconds = Math.floor((timeInSeconds / 1000) % 60);
+  const milliseconds = ((timeInSeconds / millisecondsPerInterval) % maxMilliseconds);
+  const minutes = Math.floor((timeInSeconds / millisecondsInMinute) % secondsInMinute);
+  const seconds = Math.floor((timeInSeconds / millisecondsInSecond) % secondsInMinute);
 
   const minutesFormatted = formatTime(minutes, 2);
   const secondsFormatted = formatTime(seconds, 2);
