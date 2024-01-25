e6a09669f6e3a11b17ab0404856c32135ec168b5
Daniel Tan <danieltan380@gmail.com>
2024-01-24 21:22:59 -0800

    Format document.

--- src/helpers/calculateTime.ts
+++ src/helpers/calculateTime.ts
@@ -2,11 +2,11 @@ export interface TimeFormat {
   minutesFormatted: string;
   secondsFormatted: string;
   millisecondsFormatted: string;
-};
+}
 
 const formatTime = (value: number, length: number): string => {
-  return String(value).padStart(length, '0');
-}
+  return String(value).padStart(length, "0");
+};
 
 const calculateTime = (timeInSeconds: number): TimeFormat => {
   const millisecondsPerInterval = 10;
@@ -14,16 +14,21 @@ const calculateTime = (timeInSeconds: number): TimeFormat => {
   const millisecondsInMinute = 60000;
   const secondsInMinute = 60;
   const millisecondsInSecond = 1000;
-  
-  const milliseconds = ((timeInSeconds / millisecondsPerInterval) % maxMilliseconds);
-  const minutes = Math.floor((timeInSeconds / millisecondsInMinute) % secondsInMinute);
-  const seconds = Math.floor((timeInSeconds / millisecondsInSecond) % secondsInMinute);
+
+  const milliseconds =
+    (timeInSeconds / millisecondsPerInterval) % maxMilliseconds;
+  const minutes = Math.floor(
+    (timeInSeconds / millisecondsInMinute) % secondsInMinute
+  );
+  const seconds = Math.floor(
+    (timeInSeconds / millisecondsInSecond) % secondsInMinute
+  );
 
   const minutesFormatted = formatTime(minutes, 2);
   const secondsFormatted = formatTime(seconds, 2);
-  const millisecondsFormatted = formatTime(milliseconds, 2)
+  const millisecondsFormatted = formatTime(milliseconds, 2);
 
   return { minutesFormatted, secondsFormatted, millisecondsFormatted };
-}
+};
 
 export default calculateTime;
