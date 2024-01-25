c7f35518539107d08096f30b1d8ab923efa20f49
Jas <raikjas@gmail.com>
2024-01-25 02:37:25 -0500

    timer and buttons in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,19 @@ import StopWatch from "./StopWatch";
 import "./styles.css";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="main-stopwatch">
+      <div className="timer-numbers">
+        <h1>00</h1>
+        <h1>00</h1>
+        <h1>00</h1>
+      </div>
+      <div className="btns">
+        <button className="btn btn-start">START</button>
+        <button className="btn btn-stop">STOP</button>
+        <button className="btn btn-reset">RESET</button>
+        <button className="btn btn-lap">LAP</button>
+      </div>
+    </div>
+  );
 }
