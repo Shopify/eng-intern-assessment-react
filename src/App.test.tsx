e81f373d0f249e35b447a6d913c33baffa0d65e5
Jason Huang <jasonhuang20035@gmail.com>
2024-01-25 21:29:20 +0000

    parameterise tests to reduce repetition

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,10 +9,9 @@ it("formatMs calculates 1000ms correctly", () => {
 it("renders all required buttons", async () => {
   const { queryByText } = render(<App />);
 
-  expect(queryByText("Start")).toBeTruthy();
-  expect(queryByText("Stop")).toBeTruthy();
-  expect(queryByText("Lap")).toBeTruthy();
-  expect(queryByText("Reset")).toBeTruthy();
+  ["Start", "Stop", "Lap", "Reset"]
+    .map((t) => queryByText(t))
+    .forEach((e) => expect(e).toBeTruthy());
 });
 
 it("measures 1 second correctly", async () => {
@@ -20,20 +19,36 @@ it("measures 1 second correctly", async () => {
 
   const { queryByText } = render(<App />);
 
-  expect(queryByText("Start")).toBeTruthy();
-  expect(queryByText("Stop")).toBeTruthy();
+  // buttons we need are rendering
+  const [start, stop] = ["Start", "Stop"].map((t) => queryByText(t));
+  [start, stop].forEach((e) => expect(e).toBeTruthy());
+
+  // timer starts at 0
+  expect(queryByText("00:00:000")).toBeTruthy();
 
   act(() => {
-    fireEvent.click(queryByText("Start"));
+    fireEvent.click(start);
   });
+
+  expect(queryByText("00:00:000")).toBeTruthy();
 
   act(() => {
     jest.advanceTimersByTime(1000);
   });
 
+  // after 1 second passes, the timer displays exactly 1 second
+  expect(queryByText("00:01:000")).toBeTruthy();
+
   act(() => {
-    fireEvent.click(queryByText("Stop"));
+    fireEvent.click(stop);
   });
 
+  expect(queryByText("00:01:000")).toBeTruthy();
+
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
+  // the timer does not advance when it's stopped
   expect(queryByText("00:01:000")).toBeTruthy();
 });
