a10ebf64242524e7c510fb9c4fb9677c8fb5488f
lucaspham098 <lucaspham098@gmail.com>
2024-01-24 23:24:36 -0500

    Change type for lap constant. Create functions to calculate lap times and record laps. Display laps on timer

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import StopWatchButton from './StopWatchButton'
 
+
 export default function StopWatch() {
+
+    type Lap = {
+        realMinutes: number,
+        realSeconds: number,
+        realCentiseconds: number,
+        calculatedMinutes: number
+        calculatedSeconds: number
+        calculatedCentiseconds: number
+    }
 
     const [timerOn, setTimerOn] = useState<boolean>(false)
     const [minutes, setMinutes] = useState<number>(0)
     const [seconds, setSeconds] = useState<number>(0)
     const [centiSeconds, setCentiSeconds] = useState<number>(0)
-    const [laps, setLaps] = useState<number>(0)
+    const [laps, setLaps] = useState<Lap[]>([])
 
 
     useEffect(() => {
@@ -38,6 +48,53 @@ export default function StopWatch() {
     }
 
 
+    const calculateLapTime = () => {
+        const lastLap = laps[laps.length - 1]
+        let calculatedCentiSeconds: number
+        let calculatedSeconds: number
+        let calculatedMinutes: number
+
+        calculatedMinutes = minutes - lastLap.realMinutes
+
+        if (seconds < lastLap.realSeconds) {
+            calculatedMinutes--
+            calculatedSeconds = 60 + seconds - lastLap.realSeconds
+        } else {
+            calculatedSeconds = seconds - lastLap.realSeconds
+        }
+
+        if (centiSeconds < lastLap.realCentiseconds) {
+            calculatedSeconds--
+            calculatedCentiSeconds = 100 + centiSeconds - lastLap.realCentiseconds
+        } else {
+            calculatedCentiSeconds = centiSeconds - lastLap.realCentiseconds
+        }
+
+        laps.push({
+            realMinutes: minutes,
+            realSeconds: seconds,
+            realCentiseconds: centiSeconds,
+            calculatedMinutes: calculatedMinutes,
+            calculatedSeconds: calculatedSeconds,
+            calculatedCentiseconds: calculatedCentiSeconds
+        })
+    }
+
+    const recordLap = () => {
+        if (laps.length === 0) {
+            laps.push({
+                realMinutes: minutes,
+                realSeconds: seconds,
+                realCentiseconds: centiSeconds,
+                calculatedMinutes: minutes,
+                calculatedSeconds: seconds,
+                calculatedCentiseconds: centiSeconds
+            })
+        } else {
+            calculateLapTime()
+        }
+    }
+
     const handleToggleTimer = () => {
         if (timerOn === false) {
             setTimerOn(true)
@@ -51,23 +108,33 @@ export default function StopWatch() {
         if (timerOn === false) {
             setMinutes(0)
             setSeconds(0)
-            setLaps(0)
+            setCentiSeconds(0)
+            setLaps([])
         } else {
-            setLaps(laps + 1)
+            recordLap()
         }
     }
 
 
 
     return (
-        <div>
-            <p className='timer__display'>{minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds}.{centiSeconds < 10 ? "0" + centiSeconds : centiSeconds}</p>
-            <p>{laps}</p>
+        <div className='timer'>
+            <p className='timer__time-display'>{minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds}.{centiSeconds < 10 ? "0" + centiSeconds : centiSeconds}</p>
             <StopWatchButton
                 timerOn={timerOn}
                 handleToggleTimer={handleToggleTimer}
                 handleLapResetClick={handleLapResetClick}
             />
+            <div className='timer__lap-display'>
+                {laps.map((lap, index) => {
+                    return (
+                        <div key={index} className='timer__lap-display__container'>
+                            <p className='timer__lap-display__text'>{index + 1}</p>
+                            <p className='timer__lap-display__text'>{lap.calculatedMinutes < 10 ? "0" + lap.calculatedMinutes : lap.calculatedMinutes}:{lap.calculatedSeconds < 10 ? "0" + lap.calculatedSeconds : lap.calculatedSeconds}.{lap.calculatedCentiseconds < 10 ? "0" + lap.calculatedCentiseconds : lap.calculatedCentiseconds}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
