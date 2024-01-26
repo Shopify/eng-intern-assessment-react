1e076d2a5c60d143ca295daa070f03c0e81295f8
AndrewTaoWen <andrewwen6@gmail.com>
2024-01-25 19:43:33 -0500

    added comments

--- src/StopWatchButton/StopWatchButton.tsx
+++ src/StopWatchButton/StopWatchButton.tsx
@@ -2,19 +2,23 @@
 import React from 'react';
 import "./StopWatchButton.css";
 
+// Props interface for StopWatchButton
 interface StopWatchButtonProps {
-    isRunning: boolean;
-    setIsRunning: React.Dispatch<React.SetStateAction<boolean>>;
+    isRunning: boolean; // Indicates if the stopwatch is currently running
+    setIsRunning: React.Dispatch<React.SetStateAction<boolean>>; // Function to change the running state
 }
 
 const StopWatchButton: React.FC<StopWatchButtonProps> = ({ isRunning, setIsRunning }) => {
+    // Determine button style and text based on whether the stopwatch is running
     const buttonStyle = !isRunning ? "button-start" : "button-stop";
     const buttonText = !isRunning ? "Start" : "Stop";
 
+    // Handler for click event on the button
     const handleClick = () => {
-        setIsRunning(!isRunning);
+        setIsRunning(!isRunning); // Toggle the running state
     }
 
+    // Render the button with the appropriate style and text
     return (
         <div>
             <button onClick={handleClick} className={buttonStyle}>{buttonText}</button>
