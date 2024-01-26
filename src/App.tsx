b03d231328b91139dea5e77d1a9198f45b875f90
omarelmalak28 <omar.elmalak@mail.utoronto.ca>
2024-01-26 02:15:10 -0500

    Set up and organized button files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import StopWatch from './components/StopWatch';
+import LapResetButton from './components/buttons/LapResetButton';
+import StartStopButton from './components/buttons/StartStopButton';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
-    return(
-        <div></div>
-    )
+  return (<div></div>
+  );
 }
---
+++ src/components/buttons/LapResetButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useState } from "react";
+
+export default function LapResetButton() {
+    return (
+        <div></div>
+    );
+}
