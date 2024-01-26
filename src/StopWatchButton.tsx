fab9c5771b3cd3c675eb8bca84da31239e95bc82
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2024-01-26 15:45:59 -0500

    added stopwatch buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './styles/StopWatchButton.css'
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
-}
+type StopwatchButtonProps = {
+    isRunning: boolean;
+    onStart: () => void;
+    onStop: () => void;
+    onReset: () => void;
+    onLap: () => void;
+};
+
+const StopwatchButton: React.FC<StopwatchButtonProps> = ({
+    isRunning,
+    onStart,
+    onStop,
+    onReset,
+    onLap
+}) => {
+    return (
+        <div className="stopwatch-buttons">
+            {!isRunning ? (
+                <button onClick={onStart}>Start</button>
+            ) : (
+                <button onClick={onStop}>Stop</button>
+            )}
+            <button onClick={onReset} disabled={isRunning}>Reset</button>
+            <button onClick={onLap} disabled={!isRunning}>Lap</button>
+        </div>
+    );
+};
+
+export default StopwatchButton;
