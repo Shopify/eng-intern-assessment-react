54eff3040b5d10be5d597e573c52e37acf14cf93
AkashRandhawa <akash.randhawa@designitcorp.ca>
2024-01-25 18:57:37 -0800

    Changing Watch Title

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -4,7 +4,7 @@ import './Title.css'
 const Title = () => {
 	// Can be used to implement a different header in the future
 	return (
-			<h1 className='title'>{"Akash's Timer"}</h1>
+			<h1 className='title'>{"Akash's StopWatch"}</h1>
 	)
 }
 
