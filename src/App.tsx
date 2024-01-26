33b8b9eacf98286103dd4b42173095b196cdab93
Shannon Budiman <shannon.budiman@mail.utoronto.ca>
2024-01-26 13:39:09 -0500

    feature- added useStates and basic functions to increment stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Stopwatch from './StopWatch'
+import StopwatchButton from './StopWatchButton';
 
 export default function App() {
+    const [time, setTime] = useState(0)
+    const [isRunning, setIsRunning] = useState(false)
+    const [laps, setLaps] = useState<number[]>([])
+
+    useEffect(() => {
+        if (isRunning){
+            const interval = setInterval(() => {
+                setTime(prevTime => prevTime + 10);
+            }, 10)
+        }
+    })
+
+    // Start the stopwatch
+    const handleStart = () => setIsRunning(true);
+
+    // Stop the stopwatch
+    const handleStop = () => setIsRunning(false);
+
+    // Reset the stopwatch
+    const handleReset = () => {
+        setIsRunning(false);
+        setTime(0);
+        setLaps([]);
+    };
+
+    // Record a lap
+    const handleLap = () => {
+        setLaps([...laps, time]);
+    };
     return(
-        <div></div>
+        <div>
+            <Stopwatch time={time} laps={laps} />
+            <StopwatchButton 
+                isRunning={isRunning} 
+                onStart={handleStart} 
+                onStop={handleStop} 
+                onReset={handleReset} 
+                onLap={handleLap} 
+            />
+        </div>
     )
 }
