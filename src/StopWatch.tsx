95dc6be7c780f22a80c0a7c00207f097d6bcc69c
ChristineWest <christinewwwest@gmail.com>
2024-01-25 19:23:28 -0500

    implemented lap functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -11,6 +11,9 @@ const StopWatch: React.FC<StopWatchProps> = ({isRunning, onStart, onStop}) => {
     // the state to store time
     const [time, setTime] = useState(0);
 
+    // the state to store lap times
+    const [laps, setLaps] = useState<number[]>([]);
+
 
     useEffect(() => {
         let intervalId: NodeJS.Timeout;
@@ -22,6 +25,14 @@ const StopWatch: React.FC<StopWatchProps> = ({isRunning, onStart, onStop}) => {
         }
         return () => clearInterval(intervalId);
     }, [isRunning]);
+
+    const formatTime = (milliseconds: number) => {
+        const formattedMinutes = Math.floor((milliseconds % 360000) / 6000);
+        const formattedSeconds = Math.floor((milliseconds % 6000) / 100);
+        const formattedMilliseconds = milliseconds % 100
+
+        return `${formattedMinutes.toString().padStart(2, '0')}:${formattedSeconds.toString().padStart(2, '0')}:${formattedMilliseconds.toString().padStart(2, '0')}`
+    }
 
     // calculating hours
     const hours = Math.floor(time / 360000);
@@ -46,16 +57,32 @@ const StopWatch: React.FC<StopWatchProps> = ({isRunning, onStart, onStop}) => {
 
     const reset = () => {
         setTime(0);
+        
+        // clear laps on reset
+        setLaps([]);
     }
 
     const lap = () => {
-        console.log('I may need another state to keep all the laps')
+        if (isRunning) {
+            // calculate lap time and add to laps array
+            const lapTime = time;
+            setLaps((prevLaps)=>[...prevLaps, lapTime])
+        }
     }
     return (
         <div>
             <p>
                 {hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}: {seconds.toString().padStart(2, '0')}:{milliseconds.toString().padStart(2, '0')}
             </p>
+            <div>
+                {laps.map((lapTime, index) => (
+                    <div key={index}>
+                        LAP NO. {laps.length - index}
+                        <span>Lap Time: {formatTime(lapTime)}</span>
+                        <span>Total Time: {formatTime(time)}</span>
+                    </div>
+                ))}
+            </div>
             <StopWatchButton
                 startTimer={startTimer}
                 stopTimer={stopTimer}
