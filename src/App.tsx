5dc76dbb18f6e29881f572ea43933b210316beea
Parin Parikh <parinparikh1@gmail.com>
2024-01-25 21:02:43 -0500

    add stopwatch func 1 comp 1 app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import StopWatch from './StopWatch';
 import StopWatchButton from './StopWatchButton';
 
 export default function App() {
+    const [time, setTime] = useState<number>(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    const lapsRef = useRef<number[]>([]);
+
+    const formatTime = (time: number): string => {
+        const seconds = ((time % 60000) / 1000).toFixed(2);
+        return `${+seconds < 10 ? "0" : ""}${seconds}`;
+    };
+
+    const startStopwatch = (): void => {
+        if (isRunning) {
+            setIsRunning(false);
+            clearInterval(timerRef.current);
+        } else {
+            setIsRunning(true);
+            timerRef.current = window.setInterval(() => {
+            setTime((prevTime) => prevTime + 10);
+            }, 10);
+        }
+    };
+
+    const resetStopwatch = (): void => {
+        setIsRunning(false);
+        clearInterval(timerRef.current);
+        setTime(0);
+        lapsRef.current = [];
+    };
+
+    const recordLap = (): void => {
+        lapsRef.current.push(time);
+    };
+    
+    const timerRef = useRef<number>();
 
     return(
-        <div>
-            <h1>Stop Watch</h1>
-            <StopWatch></StopWatch>
-            <StopWatchButton></StopWatchButton> 
+        <div className="App">
+            <h1>Stopwatch</h1>
+            <div className="stopwatch-display">{formatTime(time)}</div>
+                <div className="button-container">
+                    <StopWatchButton title={"Start"} onClick={startStopwatch} />
+                    <StopWatchButton title={"Stop"} onClick={startStopwatch} />
+                    <StopWatchButton title="Reset" onClick={resetStopwatch} />
+                    <StopWatchButton title="Lap" onClick={recordLap} />
+                </div>
+                <div className="laps-container">
+                    {lapsRef.current.map((lapTime, index) => (
+                    <div key={index} className="lap">
+                        Lap {index + 1}: {formatTime(lapTime)}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
-export default function StopWatchButton() {
+type ButtonProps = {
+    title: string;
+    onClick: () => void;
+  };
+
+export default function StopWatchButton({title, onClick}: ButtonProps) {
     return(
         <div>
-            <button id="button-start">Start</button>
+            <button className="button" onClick={onClick}>
+                {title}
+            </button>
+                {/* <button onClick={startStopwatch} >Start</button>
+                <button onClick={stopStopwatch} >Stop</button>
+                <button onClick={resetStopwatch}>Reset</button>
+                <button onClick={recordLap}>Lap</button> */}
+                
+            
+
+            {/* <button id="button-start">Start</button>
             <button id="button-stop">Stop</button>
             <button id="button-reset">Reset</button>
-            <button id="button-lap">Lap</button>
+            <button id="button-lap">Lap</button> */}
         </div>
     )
 }
