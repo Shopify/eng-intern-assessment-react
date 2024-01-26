51179649256b6b879454d266b0d080fdd21fd578
Meredith Jonatan <mjonatan@me.com>
2024-01-25 22:29:01 -0500

    update test arguments and add unit test for stop button -- all tests working

--- __test__/StopWatchButton.test.tsx
+++ __test__/StopWatchButton.test.tsx
@@ -6,7 +6,7 @@ import StopWatchButton from '../src/StopWatchButton'
 describe('StopWatchButton', () => {
 	// simple unit test to ensure tests are working
     test("this section contains a button", () => {
-		render(<StopWatchButton timeInSeconds={0} handleStartButton={Function} />);
+		render(<StopWatchButton timeInSeconds={0} handleStartButton={Function} handleStopButton={Function} />);
         const element = screen.getByRole('button', { name: 'Start' });
         expect(element).toBeInTheDocument();
 	});
@@ -15,7 +15,7 @@ describe('StopWatchButton', () => {
     test("stopwatch starts counting when start button is clicked", () => {
 		const mockHandleStart = jest.fn();
 
-        const {getByText} = render(<StopWatchButton timeInSeconds={0} handleStartButton={mockHandleStart} />);
+        const {getByText} = render(<StopWatchButton timeInSeconds={0} handleStartButton={mockHandleStart} handleStopButton={Function} />);
          
         const button = getByText("Start");
 
@@ -23,5 +23,18 @@ describe('StopWatchButton', () => {
         
         expect(mockHandleStart).toHaveBeenCalled();
 	});
+
+      // stop button unit test
+      test("stopwatch stops counting when stop button is clicked", () => {
+        const mockHandleStop = jest.fn();
+    
+            const {getByText} = render(<StopWatchButton timeInSeconds={0} handleStartButton={Function} handleStopButton={mockHandleStop} />);
+             
+            const button = getByText("Stop");
+    
+            fireEvent.click(button);
+            
+            expect(mockHandleStop).toHaveBeenCalled();
+      });
 
 });
