ef495b66194f56eff7a7a376844eb57e587ebc59
anyamittal08 <anyamittal08@gmail.com>
2024-01-26 12:14:37 -0500

    add laps

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+import StopWatchButton from "./StopWatchButton";
+
 const Stopwatch: React.FC = () => {
   const [totalTime, setTotalTime] = useState<number>(0);
   const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [laps, setLaps] = useState<string[]>([]);
 
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
@@ -31,29 +34,47 @@ const Stopwatch: React.FC = () => {
   const handleReset = () => {
     setTotalTime(0);
     setIsRunning(false);
+    setLaps([]);
   };
 
   const milliseconds = Math.floor((totalTime % 1000) / 10);
   const seconds = Math.floor((totalTime % 60000) / 1000);
   const minutes = Math.floor(totalTime / 60000);
 
-  const padZero = (value: number): string =>
-    value < 10 ? `0${value}` : `${value}`;
+  const format = (time: number): string => {
+    const padZero = (value: number): string =>
+      value < 10 ? `0${value}` : `${value}`;
+
+    return `${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds)}`;
+  };
+
+  const createNewLap = () => {
+    let newLap: string = format(totalTime);
+    setLaps((prevLaps) => [...prevLaps, newLap]);
+  };
 
   return (
     <div>
       <h1>Stopwatch</h1>
       <div>
-        <span>{`${padZero(minutes)}:${padZero(seconds)}.${padZero(
-          milliseconds
-        )}`}</span>
+        <span>{format(totalTime)}</span>
       </div>
       <div>
-        <button onClick={handleStartStop}>
-          {isRunning ? "Stop" : "Start"}
-        </button>
-        <button onClick={handleReset}>Reset</button>
+        <StopWatchButton
+          action={isRunning ? "Pause" : "Start"}
+          onClick={handleStartStop}
+        />
+        {isRunning ? (
+          <StopWatchButton action="Lap" onClick={createNewLap} />
+        ) : (
+          <StopWatchButton action="Reset" onClick={handleReset} />
+        )}
       </div>
+      <ul>
+        {laps.map((lap) => (
+          <li key={lap}>{lap}</li>
+        ))}
+      </ul>
     </div>
   );
 };
