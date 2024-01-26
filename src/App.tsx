dc76f181d46a988c4399f9d49a470ce4b10d52a8
Chirag Asrani <69314416+chiragasr@users.noreply.github.com>
2024-01-25 21:27:42 -0700

    laps and styling

--- src/App.tsx
+++ src/App.tsx
@@ -40,15 +40,15 @@ export default function App() {
 
     const handleLapClick = () => {
         setLapTimes((prevLapTimes) => [...prevLapTimes, time]);
-      };
+    };
 
-      const toggleModal = () => {
+    const toggleModal = () => {
         setShowModal(!showModal);
-      };
+    };
 
     return (
-        <div style={{ backgroundColor:"#000000", width: "100%", height: "100vh", textAlign: "center", color: "#EEF1F1"}}>
-            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)",  color: "#EEF1F1"}}>
+        <div style={{ backgroundColor: "#000000", width: "100%", height: "100vh", textAlign: "center", color: "#EEF1F1" }}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "#EEF1F1" }}>
                 <h5>Shopify Stopwatch</h5>
                 <div className='container'>
                     <div className='time-container'>
@@ -64,22 +64,22 @@ export default function App() {
                         onStartStopClick={handleStartStopClick}
                         onResetClick={handleResetClick}
                         onLapClick={handleLapClick}
-/>
+                    />
                 </div>
-                </div> 
-                {showModal && (
-                            <div className="modal">
-                            <div className="modal-content">
-                                <span className="close" onClick={toggleModal}>&times;</span>
-                                <h2>Lap Times</h2>
-                                <ul>
-                                {lapTimes.map((lapTime, index) => (
-                                    <li key={index}>{Time(lapTime)}</li>
-                                ))}
-                                </ul>
-                            </div>
-                            </div>
-                        )}
             </div>
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={toggleModal}>&times;</span>
+                        <h2>Lap Times</h2>
+                        <ul>
+                            {lapTimes.map((lapTime, index) => (
+                                <li key={index}>{Time(lapTime)}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
