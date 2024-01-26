ec0bb951748e82a21341a33cd21f5845dc683c79
omarelmalak28 <omar.elmalak@mail.utoronto.ca>
2024-01-26 18:53:50 -0500

    Implemented basic lap/reset button.

--- src/components/buttons/LapResetButton.tsx
+++ src/components/buttons/LapResetButton.tsx
@@ -2,7 +2,47 @@ import React from 'react';
 import { useState } from "react";
 
 export default function LapResetButton() {
+    const buttonStyleReset = {
+        borderRadius: '50px',
+        padding: '20px 20px',
+        width: '90px',
+        backgroundColor: '#999999',
+        color: '#fff',
+        cursor: 'pointer',
+        border: 'none',
+        outline: 'none',
+    };
+
+    const buttonStyleLap = {
+        borderRadius: '50px',
+        padding: '20px 20px',
+        width: '90px',
+        backgroundColor: '#7F7F7F',
+        color: '#fff',
+        cursor: 'pointer',
+        border: 'none',
+        outline: 'none',
+    };
+
+    const lapText = "Lap";
+    const resetText = "Reset";
+
+
+    const [style, setStyle] = useState(true);
+
+    const toggleStyle = () => {
+        setStyle((prevStyle) => !prevStyle);
+    };
+
+    const currentStyle = style ? buttonStyleLap : buttonStyleReset;
+    const currentText = style ? lapText : resetText;
+
     return (
-        <div></div>
+        <button
+            style={currentStyle}
+            onClick={toggleStyle}
+        >
+            {currentText}
+        </button>
     );
 }
