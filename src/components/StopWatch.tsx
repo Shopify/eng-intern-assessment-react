a5f5c5e467f88b55636b9fa0acd0a74d44816591
Nahor Yirgaalem <nahor21@hotmail.com>
2024-01-27 00:34:04 -0500

    added comments and minor changes

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -5,49 +5,25 @@ import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
 
+  // States which will be used to keep track of time, whether the stop watch is being ran and the laps selected by user.
   const [time, setTime] = useState<number>(0);
   const [timeList, setTimeList] = useState<(number|string)>(0);
   const [isTimeRunning, setIsTimeRunning] = useState<boolean>(false);
   const [lapsList, setLapsList] = useState<(number|string)[]>([]);
 
+  // Tells the page to re render when the time is changed, which is every second
   useEffect(() => {
     let tempTime: (number|string) = timeFormat(time);
     setTimeList(tempTime);
   }, [time]);
 
-  
   const [intervalID, setIntervalID] = useState<number>(0)
-
-  const runStartStop = () => {
-    if (!isTimeRunning) {
-      let interval: any = setInterval(() => {
-        setTime((prev: number) => prev + 1)
-      }, 1000);
-
-      setIntervalID(interval);
-    
-    } else {
-      clearInterval(intervalID);
-    }  
-
-    setIsTimeRunning(!isTimeRunning);
-  }
-
-  const runReset = () => {
-    clearInterval(intervalID);
-    setTime(0);
-
-    if (isTimeRunning) {
-      setIsTimeRunning(!isTimeRunning);
-    }
-
-    setLapsList([]);
-  }
-
-  const runLap = () => {
-   setLapsList(laps => [... laps, timeFormat(time)]);
-  }
   
+  // checks if the stop watch is running and if so, adds the current time to the lap list.
+  const runLap = () => {
+    isTimeRunning ? setLapsList(laps => [... laps, timeFormat(time)]) : null;
+  }
+
   return(
     <>
       <div className='timer-container'>
@@ -55,13 +31,21 @@ export default function StopWatch() {
       </div>
 
       <div className='buttons-container'>
-        <StopWatchButton onClick={runStartStop} label={isTimeRunning ? 'Stop' : 'Start'} />
-        <StopWatchButton onClick={runReset} label={'Reset'}/>
+        <StopWatchButton onClick={() => runStartStop(isTimeRunning,
+                                               setIsTimeRunning,
+                                               setTime,
+                                               intervalID,
+                                               setIntervalID)} label={isTimeRunning ? 'Stop' : 'Start'} />
+        <StopWatchButton onClick={() => runReset(setIsTimeRunning, 
+                                           setTime, 
+                                           intervalID, 
+                                           setLapsList)} label={'Reset'}/>
         <StopWatchButton onClick={runLap} label={'Lap'}/>
       </div>
 
       <div className='laps-container'>
         <div className='lapsTitle'>Laps</div>
+      {/* a ordered list, mapping each lap to a list item */}
           <ol className='lapsList'>
             {lapsList.map((lap) => (
               <li>{lap}</li>
@@ -73,15 +57,51 @@ export default function StopWatch() {
 
 }
 
+// A function that determines the number of hours, minutes and seconds, and returns a formatted time
 const timeFormat = (time: number): number|string => {
   
   let hours: number|string = Math.floor(time / 3600);
   let mins: number|string = Math.floor((time - (hours * 3600)) / 60);
   let secs: number|string = time - (hours * 3600) - (mins * 60);
 
+  // Adds a '0' before the integer if the integer is less then 10 (i.e., 9 -> 09). Common pratice among stop watches
   hours = hours < 10 ? `0${hours}` : hours;
   mins = mins < 10 ? `0${mins}` : mins;
   secs = secs < 10 ? `0${secs}` : secs;
   
   return `${hours}:${mins}:${secs}`;
+}
+
+// A function that is invoked when the start/stop button is pressed.
+const runStartStop = (isTimeRunning: boolean, 
+                      setIsTimeRunning: Function,
+                      setTime: Function,
+                      intervalID: number,
+                      setIntervalID: Function) => {
+
+  // Checks if the time is currently running, if it is NOT continuesly increments the time by 1 each second
+  if (!isTimeRunning) {
+    let interval: any = setInterval(() => {
+      setTime((prev: number) => prev + 1)
+    }, 1000);
+
+    setIntervalID(interval);
+  
+  } else { // If time is running, it means the stop button was pressed and we nee to pause the time.
+    clearInterval(intervalID);
+  }  
+
+  setIsTimeRunning(!isTimeRunning);
+}
+
+// A function that resets the time dispalyed to 0 as well as the laps 
+const runReset = (setIsTimeRunning: Function,
+                  setTime: Function,
+                  intervalID: number,
+                  setLapsList: Function) => {
+
+  clearInterval(intervalID);
+  setTime(0);
+  setIsTimeRunning(false);
+  setLapsList([]);
 }
