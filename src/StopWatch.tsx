4a79736344816ba4448e38d5154e0282848c3f23
Jeffrey Lu <jefflu288@gmail.com>
2024-01-27 17:44:26 -0500

    implement running and stopped states and ensure buttons are enabled/disabled correctly

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -7,17 +7,29 @@ export default function StopWatch() {
     const [isRunning, setIsRunning] = useState<boolean>(false);
     const [recordedLapTimes, setRecordedLapTimes] = useState<number[]>([]);
 
-    const startTimer = () => {};
-    const stopTimer = () => {};
-    const lapTimer =() => {};
-    const resetTimer = () => {};
+    const startTimer = () => {
+        setIsRunning(true);
+        console.log('Start Timer');
+    };
+    const stopTimer = () => {
+        setIsRunning(false);
+        console.log('Stop Timer');
+    };
+    const lapTimer =() => {
+        console.log('Lap Timer');
+    };
+    const resetTimer = () => {
+        setIsRunning(false);
+        setTime(0);
+        console.log('Reset Timer');
+    };
 
     return(
         <>
             <div>00:00:00.00</div>
-            <StopWatchButton disabled = {false} action = {startTimer} label = 'Start'/>
-            <StopWatchButton disabled = {true} action = {stopTimer} label ='Stop'/>
-            <StopWatchButton disabled = {true} action = {lapTimer} label = 'Lap'/>
+            <StopWatchButton disabled = {isRunning} action = {startTimer} label = 'Start'/>
+            <StopWatchButton disabled = {!isRunning} action = {stopTimer} label ='Stop'/>
+            <StopWatchButton disabled = {!isRunning} action = {lapTimer} label = 'Lap'/>
             <StopWatchButton disabled = {false} action = {resetTimer} label='Reset'/>
         </>
     )
