5697f185e01f7a02ff0bfe3478d7eaeaadd335bb
Jeffrey Lu <jefflu288@gmail.com>
2024-01-27 17:49:42 -0500

    create utility functions for displaying timer in digits format

---
+++ src/util.tsx
@@ -0,0 +1,9 @@
+export const getTimeDisplayValue = (timeInMs: number):string => {
+    const hundredthsOfASecond = digitize(Math.floor(timeInMs/10) % 100);
+    const seconds = digitize(Math.floor(timeInMs/1000) % 60);
+    const minutes = digitize(Math.floor(timeInMs/60000) % 60);
+    const hours = digitize(Math.floor(timeInMs/3600000) % 24);
+    return `${hours}:${minutes}:${seconds}.${hundredthsOfASecond}`;
+}
+
+export const digitize = (value: number): string => value < 10 ? `0${value}` : `${value}`;
