c31dc5e1e786a29203b9112c4333d2d81cc742d5
Nick <18398281+nicholasan98@users.noreply.github.com>
2024-01-27 15:47:23 -0500

    Update 1
    Learned some react functionality and finished StopWatchButton.tsx

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import StopWatchButton from './StopWatchButton'
 
 export default function StopWatch() {
     return(
         <div></div>
     )
 }
+
+// Implementation ideas
+// Use a counter for timer, start will add 1 to count each second (milliseconds will be added after basic functionality is cleared)
+// Stop will stop the counter
+// Reset, self-explanatory will reset the counter to 0
+// Lap button will display the difference in time from the previous lap button (or 0 if unpressed) to the current lap button press
+// Lap button implementation could be done with pointers? Start pointer at 0 and add a new pointer each time lap is pressed
+// Display could just take (time at lap button pressed) - (time at previous lap button pressed)
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,47 @@
 import React from 'react'
 
-export default function StopWatchButton() {
+export default function StopWatchButton(props) {
     return(
-        <div></div>
+        <div>
+            <button 
+                className="startButton" 
+                onClick={() => props.onStart()}
+            >
+                Start
+            </button>
+            
+            <button 
+                className="stopButton" 
+                onClick={() => props.onStop()}
+            >
+                Stop
+            </button>
+            
+            <button
+                className="resetButton" 
+                onClick={() => props.onReset()}
+            >
+                Reset
+            </button>
+            
+            <button 
+                className="lapButton" 
+                onClick={() => props.onLap()}
+            >
+                Lap
+            </button>
+        </div>
     )
 }
+
+// Four buttons are needed for this section,
+// Start, Stop, Reset and Lap
+// Steps were given in README.md
+// - The stopwatch should start counting when the user clicks the start button.
+// - The stopwatch should stop counting when the user clicks the stop button.
+// - The stopwatch should reset to zero when the user clicks the reset button.
+// - The stopwatch should record and display laps when user clicks the lap button.
+
+// Based on the react documentation that was given with onClick, I could make 4 different buttons with an onStart, onStop, onReset and onLap
+// Each of these buttons would respectively be tied to their functionality
+// The four buttons here are modifications of the react tutorial on the tictactoe functionality
