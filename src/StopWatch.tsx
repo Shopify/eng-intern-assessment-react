9eb91b38276550b9a634c29cd8dbce231db9b11f
Jeffrey Lu <jefflu288@gmail.com>
2024-01-27 17:40:25 -0500

    define stopwatch time, isRunning and recordedLapTimes states

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {useState} from 'react';
 import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
+    const [time, setTime] = useState<number>(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    const [recordedLapTimes, setRecordedLapTimes] = useState<number[]>([]);
+
     const startTimer = () => {};
     const stopTimer = () => {};
     const lapTimer =() => {};
@@ -10,8 +15,8 @@ export default function StopWatch() {
     return(
         <>
             <div>00:00:00.00</div>
-            <StopWatchButton disabled ={false} action = {startTimer} label = 'Start'/>
-            <StopWatchButton disabled={true} action = {stopTimer} label ='Stop'/>
+            <StopWatchButton disabled = {false} action = {startTimer} label = 'Start'/>
+            <StopWatchButton disabled = {true} action = {stopTimer} label ='Stop'/>
             <StopWatchButton disabled = {true} action = {lapTimer} label = 'Lap'/>
             <StopWatchButton disabled = {false} action = {resetTimer} label='Reset'/>
         </>
