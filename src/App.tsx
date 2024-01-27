f9f6d93096921d2be590fb585a71aca8c786bd53
Nathan Coulas <contact.nathancoulas@gmail.com>
2024-01-27 18:41:24 -0500

    Create interface for Laps and StopWatchProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import StopWatch from './StopWatch'
+import StopWatchButton from './StopWatchButton'
+
+export interface Lap {
+    startTime: number
+    duration: number
+}
 
 export default function App() {
+    const [laps, setLaps] = React.useState<Lap[]>([]);
+    const [isRunning, setIsRunning] = React.useState(false);
+    const [startTime, setStartTime] = React.useState(0);
+
     return(
-        <div></div>
+        <div className="app-container">
+            <StopWatch
+                laps={laps}
+                isRunning={isRunning}
+                startTime={startTime}
+            />
+            <StopWatchButton/>
+        </div>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Lap } from './App'
 
-export default function StopWatch() {
+interface StopWatchProps {
+    laps: Lap[]
+    isRunning: boolean
+    startTime: number
+}
+
+export default function StopWatch(props: StopWatchProps) {
     return(
         <div></div>
     )
