dcfaadf463b75de2f8b3f9ce99e5989b0971703e
fazoha <atefzohaf@gmail.com>
2024-01-27 19:45:12 -0330

    Hope you like it

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+import React, {Dispatch,SetStateAction} from 'react'
+import App from './App';
+import './Stopwatch.css';
+type StopWatchProps ={
+    timerArray: number[];
+    setTimerArray: Dispatch<SetStateAction<number[]>>;
 }
+const StopWatch: React.FC<StopWatchProps> = ({ timerArray, setTimerArray }) => {
+    return (
+        <div className='timestring'>
+            <p className="timertext">{timerArray[0]}</p>
+            <span>:</span>
+            <p className="timertext">{timerArray[1]}</p>
+            <span>:</span>
+            <p className="timertext">{timerArray[2]}</p>
+        </div>
+    );
+}
+export default StopWatch;
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './StopWatchButtons.css';
 
-export default function StopWatchButton() {
+
+
+
+type Props ={
+    setTimeinSeconds: Function
+    addLap: Function
+    resetLap: Function
+};
+
+export default function StopWatchButton(props:Props) {
+    const {setTimeinSeconds} = props;
+    const {addLap} = props;
+    const {resetLap} =props;
+    const [intervalId,setIntervalId] = useState<number>(0);
+    const handleStartButton = () =>{
+        if (intervalId!=0) return;
+        let interval:any = setInterval(() => {
+            setTimeinSeconds((previousState: number) => previousState+1);
+        }, 1000)
+        setIntervalId(interval);
+    }
+    const handleStopButton =() => {
+        clearInterval(intervalId);
+        setIntervalId(0);
+
+    }
+    const handleResetButton =() => {
+        clearInterval(intervalId);
+       setTimeinSeconds(0);
+        resetLap();
+        setIntervalId(0);
+
+    }
+    const handleLapButton=() =>{
+        addLap();
+
+    }
     return(
-        <div></div>
+        <div className='buttons'>
+            <button onClick={handleStartButton}>Start</button>
+            <button onClick={handleStopButton}>Stop</button>
+            <button onClick={handleResetButton}>Reset</button>
+            <button onClick={handleLapButton}>Lap</button>
+
+        </div>
     )
 }
