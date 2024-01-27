657b02a1e25ffcb71d1a7febe7de53b52163fcd7
jian-mahmoudi <jian.mahmoudi@shopify.com>
2024-01-27 00:05:57 -0500

    Add accessibility attributes to buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -10,18 +10,23 @@ type StopWatchButtonProps = {
   export default function StopWatchButton({ type, onClick, timerOn }: StopWatchButtonProps) {
     // Determine the button text based on the type
     let buttonText;
+    let tabIndex;
     switch(type) {
         case 'start':
             buttonText = 'Start';
+            tabIndex = 1;
             break;
         case 'stop':
             buttonText = 'Stop';
+            tabIndex = 2;
             break;
         case 'lap':
             buttonText = 'Record Lap';
+            tabIndex = 3;
             break;
         case 'reset':
             buttonText = 'Reset';
+            tabIndex = 4;
             break;
         default: buttonText = '';
     }
@@ -29,6 +34,8 @@ type StopWatchButtonProps = {
     return(
         <button 
             onClick={onClick} 
+            aria-label={type}
+            tabIndex={tabIndex}
             // Disable the lap button when the timer is stopped
             disabled={!timerOn && type === 'lap'}
             >
