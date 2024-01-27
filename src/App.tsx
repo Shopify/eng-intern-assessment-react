0292a4c889a6acb5d5544e8c6b2a9f71f7519837
Anita Yawson <anitayawson@gmail.com>
2024-01-27 16:37:46 -0500

    Define function to manage time format, add effect and states for stopwatch updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import StopWatch from "./components/StopWatch/StopWatch";
 import StopWatchButton from "./components/StopWatchButton/StopWatchButton";
 
 export default function App() {
+  // States to manage displayed time
+  const [timeInSeconds, setTimeInSeconds] = useState<number>(0);
+  const [displayTime, setDisplayTime] = useState<Array<number | string>>([]);
+
+  // Update the display time whenever timeInSeconds changes
+  useEffect(() => {
+    let timeArray: Array<number | string> = formatTime(timeInSeconds);
+    setDisplayTime(timeArray);
+  }, [timeInSeconds]);
+
+  // Format time in hours, minutes, and seconds
+  function formatTime(timeInSeconds: number): Array<number | string> {
+    const hours = String(Math.floor(timeInSeconds / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((timeInSeconds % 3600) / 60)).padStart(
+      2,
+      "0"
+    );
+    const seconds = String(Math.floor(timeInSeconds % 60)).padStart(2, "0");
+
+    return [hours, minutes, seconds];
+  }
+
   return (
     <main>
-      <StopWatch />
+      <StopWatch displayTime={displayTime} />
       <StopWatchButton />
     </main>
   );
--- src/components/StopWatch/StopWatch.tsx
+++ src/components/StopWatch/StopWatch.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "./StopWatch.css";
 
-export default function StopWatch() {
+type Props = {
+  // Array containing the display time values for hours, minutes, and seconds
+  displayTime: Array<number | string>;
+};
+
+export default function StopWatch({ displayTime }: Props) {
   return (
     <main className="stopwatch-container">
       <div className="stopwatch-numbers">
         <div>
-          <p className="number">0</p>
+          <p className="number">{displayTime[0]}</p>
           <p className="text">Hours</p>
         </div>
         <span>:</span>
         <div>
-          <p className="number">0</p>
+          <p className="number">{displayTime[1]}</p>
           <p className="text">Minutes</p>
         </div>
         <span>:</span>
         <div>
-          <p className="number">0</p>
+          <p className="number">{displayTime[2]}</p>
           <p className="text">Seconds</p>
         </div>
       </div>
