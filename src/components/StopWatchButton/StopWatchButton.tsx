3628c6909fa0a250f07fbb7d2b2db8236e3c6dcd
dani <118696363+dwleung@users.noreply.github.com>
2024-01-27 16:42:33 -0500

    Add button props

--- src/components/StopWatchButton/StopWatchButton.tsx
+++ src/components/StopWatchButton/StopWatchButton.tsx
@@ -1,27 +1,15 @@
-import React from "react";
+import React {useState, useEffect}from "react";
+import "./StopWatchButton.scss"; 
 
-export default function StopWatchButton() {
-	//Start button starts counting
-	// Click handler takes a function that:
-	// sets startTime to systemTime (or date.now?)
-	// Records date.now in a variable startTime
-	// updates state??
-	// Change button text to stop
-	// undefined if the stopwatch is not started yet or is reset
-	// TYPE: number | undefined
+interface ButtonProps {
+	text: string;
+	className: string;
+}
 
-	// Stop button stops counting
-	// Click handler stop counting (?) - stopsystemtime?
-	// Store time stopped in a variable stopTime
-	// Add time elapsed but subtracting startTime from Date.now()
-	// Store result in lap array
-	// Change button text to start
 
-	// Reset button resets to zero
-	// Reset counter to 0
+export default function StopWatchButton({text, className, clickHandler}) {
 
-	// Laps button records and displays laps
-	// Display lap array
-
-	return <div></div>;
+	return <div>
+		<button className={`button ${{buttonClassName}}`} onClick={clickHandler}>{buttonText}</button>
+	</div>;
 }
