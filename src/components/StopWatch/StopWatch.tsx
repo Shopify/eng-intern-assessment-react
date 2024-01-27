be5cd2c638cef304d030d54692bc7e44595c1596
Anita Yawson <anitayawson@gmail.com>
2024-01-27 16:20:51 -0500

    add basic styling for stopwatch & buttons

--- src/components/StopWatch/StopWatch.tsx
+++ src/components/StopWatch/StopWatch.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./StopWatch.css";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  return (
+    <main className="stopwatch-container">
+      <div className="stopwatch-numbers">
+        <div>
+          <p className="number">0</p>
+          <p className="text">Hours</p>
+        </div>
+        <span>:</span>
+        <div>
+          <p className="number">0</p>
+          <p className="text">Minutes</p>
+        </div>
+        <span>:</span>
+        <div>
+          <p className="number">0</p>
+          <p className="text">Seconds</p>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/components/StopWatchButton/StopWatchButton.tsx
+++ src/components/StopWatchButton/StopWatchButton.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./StopWatchButton.css";
 
 export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+  return (
+    <main className="controls">
+      <div className="buttons-container">
+        <button className="lap-button">Lap</button>
+        <button className="stop-button">Stop</button>
+        <button className="start-button">Start</button>
+        <button className="reset-button">Reset</button>
+      </div>
+    </main>
+  );
 }
