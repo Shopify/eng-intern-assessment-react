57e29f003636a130a757c9431edae8e6721a5a1b
Nahor Yirgaalem <nahor21@hotmail.com>
2024-01-26 22:34:38 -0500

    working on timer algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import StopWatch from './components/StopWatch'
+import './App.css';
 
 export default function App() {
     return(
--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import '../styles/StopWatch.css';
+import { useState } from 'react';
 
 export default function StopWatch() {
+
+  const [time, setTime] = useState<number>(0);
+  const [timeList, setTimeList] = useState<(number|string)[]>([]);
+
+  useEffect(() => {
+    
+    let tempTimeList: (number|string)[] = timeFormat(time);
+    setTimeList(tempTimeList);
+  }, [time]);
+
+  const hour: number|string = timeList[0];
+  const min: number|string = timeList[1];
+  const sec: number|string = timeList[2];
+
   return(
     <>
-      <div>Hello World</div>
+      <div className='hour'>{hour}</div>
+      <div className='min'>{min}</div>
+      <div className='sec'>{sec}</div>
     </>
   );
+
+}
+
+const timeFormat = (time: number): (number|string)[] => {
+  
+  const hours: number = Math.floor(time / 3600);
+  const mins: number = Math.floor(time - (hours * 3600) / 60);
+  const secs: number = Math.floor(time - (hours * 3600)- (mins * 60));
+
+  const hourFormat: number|string = hours < 10 ? `0${hours}` : hours;
+  const minsFormat: number|string = hours < 10 ? `0${mins}` : mins;
+  const secsFormat: number|string = hours < 10 ? `0${secs}` : secs;
+
+  return [hourFormat, minsFormat, secsFormat];
 }
