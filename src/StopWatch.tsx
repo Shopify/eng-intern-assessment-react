731610e3ba6e2843fdcfc3d2d2d6d6771fd36d9f
jnguyen164 <jnguyen164@myseneca.ca>
2024-01-27 23:36:06 -0500

    create stopwatch interface and functionality

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import StopWatchButton from './StopWatchButton';
 
 export default function StopWatch() {
-    return(
-        <div></div>
+
+    // state variables to track timer, lap time, and lap array
+    const [timer, setTimer] = useState<number>(0);
+    const [lapTime, setLapTime] = useState<number>(0);
+    const [laps, setLaps] = useState<string[]>([]);
+
+    // update laps array when lap time changes, skip initial render
+    useEffect(() => {
+        if (lapTime !== 0) {
+            setLaps((prevLaps) => [calculateTime(lapTime), ...prevLaps])
+        }
+    }, [lapTime]);
+
+    // helper function to convert timer into string representiing hours, minutes, and seconds
+    const calculateTime = (time: number) => {
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const seconds = time % 60;
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    //render stopwatch component
+    return (
+        <main>
+            <section className="timer-section">
+                <div className="timer-display">
+                    <p>{calculateTime(timer)}</p>
+                </div>
+                <StopWatchButton timer={timer} setTimer={setTimer} setLapTime={setLapTime} setLaps={setLaps} />
+            </section>
+
+            <section className="lap-section" style={{display: laps.length > 0 ? 'block' : 'none'}}>
+                <h2>Laps</h2>
+                <div className="lap-display">
+                    {laps.map((lap, index) => (
+                        <div className="laps-span">
+                            <span>lap {laps.length - index}</span>
+                            <span key={index}>{lap}</span>
+                        </div>
+
+                    ))}
+                </div>
+            </section>
+        </main>
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function StopWatchButton() {
+// props type definitions
+type Props = {
+    timer: number,
+    setTimer: Function,
+    setLapTime: Function,
+    setLaps: Function
+}
+
+export default function StopWatchButton(props: Props) {
+
+    // destructure props
+    const { timer, setTimer, setLapTime, setLaps } = props;
+
+    // state variables to keep track of if time is running, current time, and interval id
+    const [timeRunning, setTimeRunning] = useState<boolean>(false);
+    const [currentTime, setCurrentTime] = useState<number>(0);
+    const [intervalID, setIntervalID] = useState<number>(0);
+
+    // starts stopwatch timer
+    const startTimer = () => {
+        let interval:any = setInterval(() => {
+            setTimer((prevTimer:number) => prevTimer + 1)
+        }, 1000)
+
+        setIntervalID(interval);
+        setTimeRunning(true);
+    }
+
+    // stops stopwatch timer
+    const stopTimer = () => {
+        clearInterval(intervalID);
+        setTimeRunning(false);
+    }
+
+    // resets variable states
+    const resetTimer = () => {
+        clearInterval(intervalID);
+        setTimer(0);
+        setLapTime(0);
+        setCurrentTime(0);
+        setLaps([]);
+        setTimeRunning(false);
+    }
+
+    // records stopwatch lap time
+    const lapTimer = () => {
+        const prevTime = currentTime;
+        setCurrentTime(timer);
+        setLapTime(timer - prevTime);
+    };
+
+    //render stopwatch button component
     return(
-        <div></div>
+        <div className="stopwatch-btns">
+            <button className="start-btn" onClick={startTimer} style={{display: !timeRunning ? 'block' : 'none'}}>Start</button>
+            <button className="stop-btn" onClick={stopTimer} style={{display: !timeRunning ? 'none' : 'block'}}>Stop</button>
+            <button className="lap-btn" onClick={lapTimer} style={{display: !timeRunning ? 'none' : 'block'}}>Lap</button>
+            <button className="reset-btn" onClick={resetTimer}>Reset</button>
+        </div>
     )
 }
