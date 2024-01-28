f70747a7bbe401905ab10128f13de8d79d0a5b79
Nathan Kelly <ndavidkelly@gmail.com>
2024-01-28 12:25:02 -0500

    adding small optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
     const [laps, setLaps] = useState<number[]>([]);
 
     useEffect(() => {
-        let interval: any;
+        let interval: NodeJS.Timeout | null = null;
 
         if (isRunning) {
             interval = setInterval(() => setTime((time) => time + 10), 10);
@@ -25,9 +25,9 @@ const App: React.FC = () => {
     useEffect(() => {
         if (time) {
             const prevLaps = laps.slice(0, laps.length - 1);
-            const currentLaps: number =
+            const currentLap: number =
                 time - prevLaps.reduce((acc, val) => acc + val, 0);
-            setLaps([...prevLaps, currentLaps]);
+            setLaps([...prevLaps, currentLap]);
         } else {
             setLaps([]);
         }
