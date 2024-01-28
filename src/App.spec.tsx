babea945db42d5f0ba200b420e7712287842d57e
Nathan Coulas <contact.nathancoulas@gmail.com>
2024-01-28 17:11:13 -0500

    Added tests for adding laps and pressing the reset button

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -66,4 +66,47 @@ describe('App', () => {
             expect(stopStartButton.textContent).toBe('Start');
         })
     });
+
+    describe("lap related tests", () => {
+        // Setup for each test so there are 5 laps already created
+        const newLapCount = 5;
+        beforeEach(async () => {
+            render(<App />);
+            
+            const lapButton = screen.getByTestId('stopwatch-lap');
+            const startStopButton = screen.getByTestId('stopwatch-startstop');
+
+            await act(async () => {
+                fireEvent.click(startStopButton);
+            });
+
+            await waitFor(async () => {
+                // every 100 ms create a new lap
+                for(let i = 0; i < newLapCount; i++) {
+                    fireEvent.click(lapButton);
+                    await new Promise((resolve) => setTimeout(resolve, 100));
+                }
+            });
+        });
+
+        test('clicking the lap button adds a lap', async () => {
+            await waitFor(() => {
+                // Assert that we rendered the correct number of laps (newLapCount + 1 because of the preview lap)
+                expect(screen.getAllByTestId(/lap-/).length).toBe(newLapCount + 1);
+            });
+        });
+
+        test('clicking the reset button resets the laps and time counter', () => {
+            const resetButton = screen.getByTestId('stopwatch-reset');
+            const timeCounter = screen.getByTestId('stopwatch-display');
+
+            fireEvent.click(resetButton);
+
+            // Assert there are no more recorded laps displayed (only the preview lap)
+            expect(screen.getAllByTestId(/lap-/).length).toBe(1);
+
+            // Assert that the time counter is reset to 0 after clicking the button
+            expect(timeCounter.textContent).toBe(formatTime(0));
+        });
+    });
 });
