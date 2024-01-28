44f48a69971c79134696cef0494502e8ddd5c8aa
destocot <khurramcali@gmail.com>
2024-01-27 19:40:27 -0500

    initial stopwatch

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import StopWatchButton from "./StopWatchButton";
+
+type LapType = {
+  id: number;
+  lapTime: number;
+  timeElapsed: number;
+}[];
+
+/**
+ * formats a given time value (number), into a readable string MM:SS:CC (minutes:seconds:centiseconds)
+ *
+ * @param time
+ * @returns
+ */
+function formatTime(time: number) {
+  const minutes = Math.floor(time / (100 * 60));
+
+  time -= minutes * (100 * 60);
+  const seconds = Math.floor(time / 100);
+  // learned this word while researching about stopwatch - khurram
+  time -= seconds * 100;
+  const centisecond = time;
+
+  const strMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const strSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const strCentisecond = centisecond < 10 ? `0${centisecond}` : centisecond;
+
+  return `${strMinutes}:${strSeconds}:${strCentisecond}`;
+}
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [stopwatchIntervalId, setStopwatchIntervalId] =
+    useState<NodeJS.Timer>(null);
+  const [laps, setLaps] = useState<LapType>([]);
+
+  function start() {
+    const intervalId = setInterval(() => {
+      setTimeElapsed((state) => state + 1);
+    }, 10);
+    setStopwatchIntervalId(intervalId);
+  }
+
+  function stop() {
+    clearInterval(stopwatchIntervalId);
+    setStopwatchIntervalId(null);
+  }
+
+  function lap() {
+    const lastLap = laps[0];
+    const id = lastLap ? lastLap.id + 1 : 1;
+    const lapTime = timeElapsed - (lastLap?.timeElapsed ?? 0);
+
+    const newLap = {
+      id,
+      lapTime,
+      timeElapsed,
+    };
+
+    setLaps((state) => [newLap, ...state]);
+  }
+
+  function reset() {
+    stop();
+    setLaps([]);
+    setTimeElapsed(0);
+  }
+
+  return (
+    <div>
+      <div className="time">{formatTime(timeElapsed)}</div>
+      <StopWatchButton start={start} stop={stop} reset={reset} lap={lap} />
+      {laps.length > 0 && (
+        <div className="laps">
+          {laps.map(({ id, lapTime, timeElapsed }) => (
+            <div className="lap" key={id}>
+              <span>{`#${id}`}</span>
+              <time>{formatTime(lapTime)}</time>
+              <time>{formatTime(timeElapsed)}</time>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+interface StopWatchButtonProps {
+  start: () => void;
+  stop: () => void;
+  reset: () => void;
+  lap: () => void;
+}
+
+export default function StopWatchButton({
+  start,
+  stop,
+  lap,
+  reset,
+}: StopWatchButtonProps) {
+  return (
+    <div className="buttons">
+      <button className="btn btn-start" onClick={start}>
+        Start
+      </button>
+      <button className="btn btn-stop" onClick={stop}>
+        Stop
+      </button>
+      <button className="btn btn-lap" onClick={lap}>
+        Lap
+      </button>
+      <button className="btn btn-reset" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
 }
