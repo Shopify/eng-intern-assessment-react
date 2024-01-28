21ba5ce7056a5c770711159aaefa6e2b73de22cb
anyamittal08 <anyamittal08@gmail.com>
2024-01-27 19:21:26 -0500

    refactored formatTime utility function

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,19 +1,15 @@
-const formatTime = (
+const getTimeBreakdown = (
   time: number
 ): { minutes: string; seconds: string; centiseconds: string } => {
   const centiseconds = Math.floor((time % 1000) / 10);
   const seconds = Math.floor((time % 60000) / 1000);
   const minutes = Math.floor(time / 60000);
 
-  const padZero = (value: number): string =>
-    value < 10 ? `0${value}` : `${value}`;
-
-  // return `${padZero(minutes)}:${padZero(seconds)}.${padZero(centiseconds)}`;
   return {
-    minutes: `${padZero(minutes)}`,
-    seconds: `${padZero(seconds)}`,
-    centiseconds: `${padZero(centiseconds)}`,
+    minutes: minutes.toString().padStart(2, "0"),
+    seconds: seconds.toString().padStart(2, "0"),
+    centiseconds: centiseconds.toString().padStart(2, "0"),
   };
 };
 
-export { formatTime };
+export { getTimeBreakdown };
