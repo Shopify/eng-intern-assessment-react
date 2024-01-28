b6a232d47a0d3d0b4284f03f6b6b3b765757c948
Krish120003 <krish120003@gmail.com>
2024-01-28 02:20:48 -0500

    feat: body and button styles

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -3,5 +3,12 @@ import React, { ButtonHTMLAttributes } from "react";
 export default function StopWatchButton(
   props: ButtonHTMLAttributes<HTMLButtonElement>
 ) {
-  return <button {...props}>{props.children}</button>;
+  return (
+    <button
+      {...props}
+      className={`p-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-600 rounded active:translate-y-[0.1em] transition-transform ${props.className}`}
+    >
+      {props.children}
+    </button>
+  );
 }
