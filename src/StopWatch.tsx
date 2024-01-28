83b86ad24067e127bb3c4d918ed266ce91a980d5
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-01-27 19:29:07 -0500

    feat: added basic stopwatch functionality to StopWatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -2,12 +2,43 @@ import React from "react";
 import { StopWatchButton } from "./StopWatchButton";
 
 export default function StopWatch() {
+  // not specifying the type here for useState since typescript can infer this from the initial state value
+  const [time, setTime] = React.useState(0);
+  const [isTimerRunning, setTimerRunning] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval: NodeJS.Timer = null;
+
+    if (isTimerRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    // cleanup function
+    return () => clearInterval(interval);
+  }, [isTimerRunning]);
+
   return (
     <div>
-      <StopWatchButton>Start</StopWatchButton>
-      <StopWatchButton>Stop</StopWatchButton>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <StopWatchButton onClick={() => setTimerRunning(true)}>
+        Start
+      </StopWatchButton>
+      <StopWatchButton onClick={() => setTimerRunning(false)}>
+        Stop
+      </StopWatchButton>
+      <StopWatchButton onClick={() => setTimerRunning(true)}>
+        Resume
+      </StopWatchButton>
       <StopWatchButton>Lap</StopWatchButton>
-      <StopWatchButton>Reset</StopWatchButton>
+      <StopWatchButton onClick={() => setTime(0)}>Reset</StopWatchButton>
     </div>
   );
 }
