aff527114045ff1d3e41868306f58be58ea6ca72
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-01-28 18:35:44 -0500

    refactor
    moved formatTime() into StopWatch.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,22 +28,9 @@ function App() {
         setTimeElapsed(0);
     }
 
-    const formatTime = (timeInMilliseconds: number) => {
-        const minutes = Math.floor((timeInMilliseconds % 3600000) / 60000);
-        const seconds = Math.floor((timeInMilliseconds % 60000) / 1000);
-        const milliseconds = Math.floor((timeInMilliseconds % 1000) / 10);
-
-        const doubleDigits = (input: number) => {
-            const output = input < 10 ? `0${input}` : input;
-            return output;
-        };
-
-        return `${doubleDigits(minutes)}:${doubleDigits(seconds)}.${doubleDigits(milliseconds)}`;
-    }
-
     return (
         <div className='app'>
-            <StopWatch time={formatTime(timeElapsed)} />
+            <StopWatch time={timeElapsed} />
             <div className='buttonsContainer'>
                 <StopWatchButton 
                     label='start' 
