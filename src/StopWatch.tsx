2cb9173fb4a7f206a1580ccf484f569f71fff777
JP Grineau <jgrineau95@gmail.com>
2024-01-28 18:14:24 -0500

    StopWatch Displays and working correctly

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,107 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [lapTimes, setLapTimes] = useState<string[]>([]);
+  const [lastLapTime, setLastLapTime] = useState<number | null>(null);
+  const timerRef = useRef<number | null>(null);
+
+  // Set initial last lap time when the component mounts
+  useEffect(() => {
+    setLastLapTime(time);
+  }, []);
+
+  //Format the Time displayed on the screen
+  function formatTime(timeValue: number = 0): string {
+    const hours = Math.floor(time / (1000 * 60 * 60));
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}:${padZero(
+      milliseconds
+    )}`;
+  }
+
+  //Start times of '00'
+  function padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+
+  //Start the stopwatch hanling
+  const startStopwatch = () => {
+    if (!isRunning) {
+      timerRef.current = window.setInterval(() => {
+        setTime((prevTime) => prevTime + 10); // increment by 10 milliseconds
+      }, 10);
+    } else {
+      window.clearInterval(timerRef.current!);
+    }
+
+    setIsRunning(!isRunning);
+  };
+
+  //Stop handle
+  const stopStopwatch = () => {
+    window.clearInterval(timerRef.current!);
+    setIsRunning(false);
+  };
+
+  //Reset handle
+  const resetStopwatch = () => {
+    stopStopwatch();
+    setLastLapTime(0);
+    setTime(0);
+    setLapTimes([]);
+  };
+
+  //Lap handle
+  const handleLap = () => {
+    if (lastLapTime === null) {
+      setLastLapTime(time);
+      return;
+    }
+    const elapsed = time - lastLapTime;
+    const lapTime =
+      padZero(Math.floor(elapsed / (1000 * 60 * 60))) +
+      ":" +
+      padZero(Math.floor((elapsed / (1000 * 60)) % 60)) +
+      ":" +
+      padZero(Math.floor((elapsed / 1000) % 60)) +
+      "." +
+      padZero(Math.floor((elapsed % 1000) / 10));
+
+    setLapTimes((prevLaps) => [...prevLaps, lapTime]);
+    setLastLapTime(time);
+  };
+  return (
+    <>
+      <div>
+        <h1>{formatTime()}</h1>
+      </div>
+      <div>
+        <StopWatchButton
+          isRunning={isRunning}
+          start={startStopwatch}
+          stop={stopStopwatch}
+          reset={resetStopwatch}
+        />
+        {isRunning && (
+          <button className="btn" onClick={handleLap}>
+            Lap
+          </button>
+        )}
+      </div>
+      <div>
+        <h2>Lap Times</h2>
+        <ul>
+          {lapTimes.map((lap, index) => (
+            <li key={index}>{lap}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
 
-export default function StopWatchButton() {
-    return(
-        <div></div>
-    )
+export default function StopWatchButton(props: any) {
+  //Handle the start Click using props
+  const handleStartClick = () => {
+    if (!props.isRunning) {
+      return props.start();
+    } else {
+      return alert("Timer is already running");
+    }
+  };
+
+  //Handle the stop Click using props
+  const handleStopClick = () => {
+    if (props.isRunning) {
+      return props.stop();
+    } else {
+      return alert("Timer is not running");
+    }
+  };
+
+  //Handle the Reset Click using props
+  const handleResetClick = () => {
+    return props.reset();
+  };
+
+  return (
+    <>
+      <button onClick={handleStartClick}>Start</button>
+      <button onClick={handleStopClick}>Stop</button>
+      <button onClick={handleResetClick}>Reset</button>
+    </>
+  );
 }
