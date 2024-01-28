c610e8a957ef4107a6faae6c9520aa345d6b5b86
omarelmalak28 <omar.elmalak@mail.utoronto.ca>
2024-01-28 01:27:41 -0500

    STO3: Implemented a placeholder UI

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import LapResetButton from './buttons/LapResetButton';
+import StartStopButton from './buttons/StartStopButton';
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 export default function StopWatch() {
-    return(
-        <div></div>
+    const lightModeBackground = {
+        fontFamily: 'Tahoma',
+        fontSize: '100px',
+        fontWeight: 'bold',
+        width: '30vw',
+
+
+        // linear-gradient(to top, transparent, #9bce39, #87ceeb)
+        backgroundColor: '',
+        color: '#000000',
+        outline: 'none',
+
+    }
+
+    const currentMode = lightModeBackground;
+
+    const [lapReset, setLapReset] = useState(true);
+
+    const handleStartStop = (lapReset: boolean) => {
+        setLapReset(lapReset);
+    };
+
+    return (
+        <div className="container text-center" style={currentMode}>
+            <h1 style={{ fontSize: '100px', fontWeight: 'bold', padding: '20px' }}>
+                hr.min.sec
+            </h1>
+
+            <div className="row" style={{}}>
+                <LapResetButton lapReset={lapReset}></LapResetButton>
+                <div className="container" style={{ width: '1px' }}></div>
+                <StartStopButton setLapResetButtonType={handleStartStop} ></StartStopButton>
+            </div>
+
+
+        </div>
     )
 }
