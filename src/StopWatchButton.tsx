39e86f104a9225c91253f7e26d37ac637f33694e
Nathan Coulas <contact.nathancoulas@gmail.com>
2024-01-27 19:29:47 -0500

    Create styling and layout for StopWatchButton component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import './StopWatchButton.css'
 
-export default function StopWatchButton() {
+interface StopWatchButtonProps {
+    isRunning: boolean
+    stopStart: () => void
+    lap: () => void
+    reset: () => void
+}
+
+export default function StopWatchButton(props: StopWatchButtonProps) {
     return(
-        <div></div>
+        <div
+            className="stopwatch-button-row"
+        >
+            <button
+                onClick={props.stopStart}
+                className="stopwatch-button"
+            >
+                {props.isRunning ? 'Stop' : 'Start'}
+            </button>
+            <button
+                onClick={props.lap}
+                className="stopwatch-button"
+            >
+                Lap
+            </button>
+            <button
+                onClick={props.reset}
+                className="stopwatch-button"
+            >
+                Reset
+            </button>
+        </div>
     )
 }
