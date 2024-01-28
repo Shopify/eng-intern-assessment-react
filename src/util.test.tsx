7c210b7fabb3f837e85dc0fcebaa13899ee32b61
Jeffrey Lu <jefflu288@gmail.com>
2024-01-27 23:45:51 -0500

    create tests for util.tsx

---
+++ src/util.test.tsx
@@ -0,0 +1,27 @@
+import { getTimeDisplayValue, digitize } from "./util";
+
+describe('util tests', () => {
+    describe('getTimeDisplay tests', () => {
+        it('getTimeDIsplayValue should produce the expected result for 0', () => {
+            expect(getTimeDisplayValue(0)).toEqual('00:00:00.00');
+        });
+
+        it('getTimeDisplayValue should produce the expected result for 2510090', () => {
+            expect(getTimeDisplayValue(2510090)).toEqual('00:41:50.09');
+        });
+    });
+
+    describe ('digitize tests', () => {
+        it('digitize should produce a leading zero when input number <10', () => {
+            expect(digitize(5)).toEqual('05');
+        });
+
+        it('digitize should leave the digits alone when the value is 10', () => {
+            expect(digitize(10)).toEqual('10');
+        });
+
+        it('digitize should leave the digits alone when the value is > 10', () => {
+            expect(digitize(25)).toEqual('25');
+        });
+    });
+});
