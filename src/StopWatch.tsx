f7d0c7e65a080c142f9d0bf56d9f1b6a0d9811f4
AnnyZ08 <97325065+AnnyZ08@users.noreply.github.com>
2024-01-28 18:03:38 -0500

    basic format

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import './StopWatch.css'
+import { time } from 'console';
+import StopWatchButton from './StopWatchButton';
+
+// function calculateTimer(timeInSec:number): Array<number> {
+//     let hours:number = Math.floor(timeInSec/3600);
+//     let minutes:number = Math.floor((timeInSec%3600)/60);
+//     let seconds:number = Math.floor((timeInSec%60));
+
+//     //show two digit
+//     let hoursFormat = hours < 10 ? '0${hours}':hours;
+
+
+//     return [
+//         hoursFormat,
+//         minutes,
+//         seconds,
+//     ];
+// }
+
 
 export default function StopWatch() {
+    const[timeInSec, setTimeInSec] = useState<number>(0);
+    const [timerArr, setTimerArr] = useState<Array<number>>([]);
+
+    //calculating the hour, minutes, seconds from seconds
+    const hours:number = Math.floor(timeInSec/3600);
+    const minutes:number = Math.floor((timeInSec%3600)/60);
+    const seconds:number = Math.floor((timeInSec%60));
+    
+    useEffect(() => {
+        //increase time every 10 milisecond
+        //let intervalId = setInterval(() => setTimeInSec(timeInSec+1),10);
+    },[timeInSec]);
+
     return(
-        <div></div>
+        <main>
+            <section className='time-container'>
+                <p className = "timer-text">{hours.toString().padStart(2,"0")}</p>
+                <span>:</span>
+                <p className = "timer-text">{minutes.toString().padStart(2,"0")}</p>
+                <span>:</span>
+                <p className = "timer-text">{seconds.toString().padStart(2,"0")}</p>
+            </section>
+            <StopWatchButton setTimeInSec={setTimeInSec} />
+        </main>
+
     )
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './StopWatchButton.css'
+type Props = {
+    setTimeInSec: Function
+};
 
-export default function StopWatchButton() {
+export default function StopWatchButton(props:Props) {
+    const {setTimeInSec} = props;
+    const [intervalId, setIntervalId] = useState<number>(0);
+    
+    const handlePlayButton = () => {
+        let interval:any = setInterval(() => {
+            setTimeInSec((previousState:number) => (previousState + 1))
+        },1000);
+
+        setIntervalId(interval);
+    }
+
+    const handleStopButton = () => {
+       clearInterval(intervalId);
+    }
+
+    const handleResetButton = () => {
+        clearInterval(intervalId);
+        setTimeInSec(0);
+    }
+    
     return(
-        <div></div>
+        <section className='buttons-container'>
+            <button onClick={handlePlayButton}>Play</button>
+            <button onClick={handleStopButton}>Stop</button>
+            <button onClick={handleResetButton}>Reset</button>
+        </section>
     )
 }
