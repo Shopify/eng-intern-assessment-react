18079c2b6051ecaae5ff8bdd478816b35a963564
Nathan Coulas <contact.nathancoulas@gmail.com>
2024-01-28 17:25:55 -0500

    Added documentation for StopWatchButton

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './StopWatchButton.css'
 
+/**
+ * Props for the StopWatchButton component
+ * @param isRunning true if the stopwatch is running, false otherwise
+ * @param stopStart a void function that toggles the running state
+ * @param lap a void function that records a lap
+ * @param reset a void function that resets the stopwatch
+ */
 interface StopWatchButtonProps {
     isRunning: boolean
     stopStart: () => void
@@ -8,6 +15,12 @@ interface StopWatchButtonProps {
     reset: () => void
 }
 
+/**
+ * A component that displays the buttons for the stopwatch with no default button click behaviour
+ * Provide functions for the buttons to call as props
+ * @param props StopWatchButtonProps
+ * @returns a div element that contains the buttons in a row
+ */
 export default function StopWatchButton(props: StopWatchButtonProps) {
     return(
         <div
