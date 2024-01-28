8b601b68a3abb61b82b5033bab5155024c41cdc9
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-01-27 21:08:36 -0500

    feat: record and display lap times along with the number

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { StopWatchButton } from "./StopWatchButton";
 
+let lapTimeId = 0;
+
 const StopWatch: React.FC = () => {
   // not specifying the type here for useState since typescript can infer this from the initial state value
   const [time, setTime] = React.useState(0);
   const [isTimerRunning, setTimerRunning] = React.useState(false);
+  const [lapTimes, setLapTimes] = React.useState<
+    { id: number; time: number }[]
+  >([]);
 
   React.useEffect(() => {
     let interval: NodeJS.Timer = null;
@@ -38,8 +43,37 @@ const StopWatch: React.FC = () => {
         <StopWatchButton onClick={() => setTimerRunning(true)}>
           Resume
         </StopWatchButton>
-        <StopWatchButton>Lap</StopWatchButton>
+        <StopWatchButton
+          onClick={() => setLapTimes([...lapTimes, { id: lapTimeId++, time }])}
+        >
+          Lap
+        </StopWatchButton>
         <StopWatchButton onClick={() => setTime(0)}>Reset</StopWatchButton>
+        <StopWatchButton onClick={() => console.log(lapTimes)}>
+          Reset
+        </StopWatchButton>
+      </div>
+      <div className="flex justify-center items-center flex-col mt-6">
+        <h2 className="text-2xl text-primary-100">Laps Recorded</h2>
+        {lapTimes.map((lapTime) => {
+          return (
+            <div
+              id={lapTime.id.toString()}
+              className="flex justify-between text-lg text-primary-200 border border-white w-[700px]"
+            >
+              <span>{`Lap ${lapTime.id + 1}`}</span>
+              <div>
+                <span>
+                  {("0" + Math.floor((lapTime.time / 60000) % 60)).slice(-2)}:
+                </span>
+                <span>
+                  {("0" + Math.floor((lapTime.time / 1000) % 60)).slice(-2)}:
+                </span>
+                <span>{("0" + ((lapTime.time / 10) % 100)).slice(-2)}</span>{" "}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
