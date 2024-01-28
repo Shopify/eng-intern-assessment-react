2203e343b23bd12c3c42635c065d41514177c879
Jacob Artuso <hahkobeh@gmail.com>
2024-01-28 15:50:59 -0700

    moved reset and styled main stopwatch area

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import StopWatch from './StopWatch';
 import './styles/App.css';
-import './styles/CssReset.css';
 
 export default function App() {
 	return (
 		<main>
-			<h1>My Stopwatch app</h1>
+			<h1 className='title'>My Stopwatch app</h1>
 			<StopWatch />
 		</main>
 	);
