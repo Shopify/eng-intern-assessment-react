98d5ca179003daa1840ef33a9e2047a8054269b8
Luke Dasios <lukedasios@321@gmail.com>
2024-01-28 12:52:48 -0500

    checkkpoint

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import StopwatchButton from "./StopWatchButton";
+
+interface StopwatchProps {
+  elapsedTime: number;
+  isRunning: boolean;
+  laps: Array<{ id: number; time: number }>;
+  startStop: () => void;
+  reset: () => void;
+  recordLap: () => void;
+}
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  return <div></div>;
 }
