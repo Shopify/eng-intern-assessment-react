a4b0d86f52a40b09141731f78c23a0a5d4e8e87a
Kwongz <kyle.wong917@gmail.com>
2024-01-28 00:50:14 -0500

    added hover and focus styling

--- src/StopWatch.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render, screen, cleanup } from "@testing-library/react";
-import StopWatch from "./StopWatch";
-
-test("should render component", () => {
-  render(<StopWatch />);
-});
