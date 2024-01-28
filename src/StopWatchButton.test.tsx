a0bd3200da09f1a1d4d4b5f50dd066153dff0123
Jeffrey Lu <jefflu288@gmail.com>
2024-01-27 23:32:39 -0500

    create testcases for stopwatchbutton.tsx

---
+++ src/StopWatchButton.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render, screen, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import StopWatchButton from './StopWatchButton';
+
+describe('StopWatchButton tests', () => {
+    it('render should match snapshot file', () => {
+        const tree = renderer.create(<StopWatchButton label = 'STOP' disabled={false} action={jest.fn()}/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should invoke the action function when the button is clicked', () => {
+        const action = jest.fn();
+        render(<StopWatchButton label = 'GO' disabled = {false} action={action}/>);
+        expect(screen.getByText('GO')).toBeInTheDocument();
+        fireEvent.click(screen.getByText('GO'));
+        expect(action).toHaveBeenCalledTimes(1);
+    })
+
+})
