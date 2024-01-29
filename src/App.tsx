8acdddd779a7eb680866693697639f6fd70fbe7f
SachinKumar43 <96299849+SachinKumar43@users.noreply.github.com>
2024-01-29 11:38:27 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import './App.css'
-import StopWatch from './StopWatch'
+import React from "react";
+import "./App.css";
+import StopWatch from "./StopWatch";
+import StopWatchButton from "./StopWatchButton";
 
 export default function App() {
-    return(
-        <StopWatch />
-    )
+  return (
+    <>
+      <div>
+        <h3>hi</h3>
+      </div>
+      <StopWatch />
+      <StopWatchButton
+        type={"start"}
+        onClick={() => console.log("Start clicked")}
+      />
+      <StopWatchButton
+        type={"stop"}
+        onClick={() => console.log("Stop clicked")}
+      />
+      <StopWatchButton
+        type={"reset"}
+        onClick={() => console.log("Reset clicked")}
+      />
+    </>
+  );
 }
