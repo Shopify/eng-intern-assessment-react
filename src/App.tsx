e8ec2f0c9775f6bf725a88585345078a642539d1
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-01-29 03:29:32 -0700

    Move functionality logic to Stopwatch file

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import StopWatch from './StopWatch'
-import StopWatchButton from './StopWatchButton'
 import './App.css'
 
 export default function App() {
-    const [isTimeRunning, setIsTimeRunning] = useState(false);
-    const [elapsedMilliSecond, setElapsedMilliSecond] = useState(0);
-    const [isLapClicked, setIsLapClicked] = useState(false);
-    const [lapElaspedMilliSecond, setLapElapsedMilliSecond] = useState(0);
-    const [lapList, setLapList] = useState([]);
-    
-    useEffect(() => {
-        let interval: NodeJS.Timeout | null = null;
-        let lapInterval: NodeJS.Timeout | null = null;
-
-        if(isTimeRunning == true) {
-        interval = setInterval(() => {
-            setElapsedMilliSecond(prevElapsedMilliSec => prevElapsedMilliSec + 10);
-        }, 10);
-
-        if(isLapClicked == true) {
-            console.log(formatMilliSeconds(lapElaspedMilliSecond));
-            setLapList(currLapList => {
-            return [formatMilliSeconds(lapElaspedMilliSecond), ...currLapList]
-            })
-            setLapElapsedMilliSecond(0);
-            setIsLapClicked(false);
-        }
-
-        lapInterval = setInterval (() => {
-            setLapElapsedMilliSecond(prevElapsedMilliSec => prevElapsedMilliSec + 10);
-        }, 10)
-        } 
-
-        return () => {
-        clearInterval(interval);
-        clearInterval(lapInterval);
-        }
-
-    }, [isTimeRunning, isLapClicked]);
-
-    
-    useEffect(() => {
-        console.log(lapList);
-    }, [lapList]);
-
-    function formatMilliSeconds(milliSecond: number) {
-        let second = Math.floor(milliSecond/1000);
-        let minute = Math.floor(second/60);
-        let hour = Math.floor(minute/60);
-        let hundrethSecond = (milliSecond/10)%100; 
-        let hundrethSecondFormat = hundrethSecond < 10? `0${hundrethSecond}` : hundrethSecond;
-
-        return `${hour}h : ${minute % 60}m : ${second % 60}s.${hundrethSecondFormat}`;
-    }
-
-    function displayLapTable() {
-        return lapList.map((lap, i) => {
-          return ( 
-            <tr>
-              <td>{lapList.length-i}</td>
-              <td>{lap}</td>
-            </tr>)
-        })
-    }
-    
-    function handleReset() {
-        setElapsedMilliSecond(0);
-        setIsTimeRunning(false);
-        setLapElapsedMilliSecond(0);
-        setLapList([]);
-    }
-
+   
     return(
-        <div>
-            <div className="stopwatchContainer">
-                <StopWatch formattedElapsedMilliSecond={formatMilliSeconds(elapsedMilliSecond)}/>
-                <div className="buttonContainer">
-                    {!isTimeRunning && <StopWatchButton className="startButton" onClick={() => setIsTimeRunning(true)} label="Start"/>}
-                    {<StopWatchButton className="resetButton" onClick={() => handleReset()} label="Reset"/>}
-                    {isTimeRunning && <StopWatchButton className="stopButton" onClick={() => setIsTimeRunning(false)} label="Stop"/>}
-                    {isTimeRunning && <StopWatchButton className="lapButton" onClick={() => isTimeRunning && setIsLapClicked(true)} label="Lap"/>}
-                </div>
-            </div>
-
-            <div className="lapTableContainer">
-                {lapList.length > 0 && <table className="lapTable">
-                <thead>
-                    <tr>
-                    <th>Lap No.</th>
-                    <th>Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {displayLapTable()}
-                </tbody>
-                </table>}
-            </div>                   
-        </div>
+       <StopWatch/>
     )
 }
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,14 +1,98 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import StopWatchButton from './StopWatchButton'
 
-interface DisplayProps {
-    formattedElapsedMilliSecond: string
-}
+export default function StopWatch() {
+    const [isTimeRunning, setIsTimeRunning] = useState(false);
+    const [elapsedMilliSecond, setElapsedMilliSecond] = useState(0);
+    const [isLapClicked, setIsLapClicked] = useState(false);
+    const [lapElaspedMilliSecond, setLapElapsedMilliSecond] = useState(0);
+    const [lapList, setLapList] = useState([]);
+    
+    useEffect(() => {
+        let interval: NodeJS.Timeout | null = null;
+        let lapInterval: NodeJS.Timeout | null = null;
 
+        if(isTimeRunning == true) {
+        interval = setInterval(() => {
+            setElapsedMilliSecond(prevElapsedMilliSec => prevElapsedMilliSec + 10);
+        }, 10);
 
-export default function StopWatch({formattedElapsedMilliSecond} : DisplayProps) {
+        if(isLapClicked == true) {
+            console.log(formatMilliSeconds(lapElaspedMilliSecond));
+            setLapList(currLapList => {
+            return [formatMilliSeconds(lapElaspedMilliSecond), ...currLapList]
+            })
+            setLapElapsedMilliSecond(0);
+            setIsLapClicked(false);
+        }
+
+        lapInterval = setInterval (() => {
+            setLapElapsedMilliSecond(prevElapsedMilliSec => prevElapsedMilliSec + 10);
+        }, 10)
+        } 
+
+        return () => {
+        clearInterval(interval);
+        clearInterval(lapInterval);
+        }
+
+    }, [isTimeRunning, isLapClicked]);
+
+    function formatMilliSeconds(milliSecond: number) {
+        let second = Math.floor(milliSecond/1000);
+        let minute = Math.floor(second/60);
+        let hour = Math.floor(minute/60);
+        let hundrethSecond = (milliSecond/10)%100; 
+        let hundrethSecondFormat = hundrethSecond < 10? `0${hundrethSecond}` : hundrethSecond;
+
+        return `${hour}h : ${minute % 60}m : ${second % 60}s.${hundrethSecondFormat}`;
+    }
+
+    function displayLapTable() {
+        return lapList.map((lap, i) => {
+          return ( 
+            <tr>
+              <td>{lapList.length-i}</td>
+              <td>{lap}</td>
+            </tr>)
+        })
+    }
+    
+    function handleReset() {
+        setElapsedMilliSecond(0);
+        setIsTimeRunning(false);
+        setLapElapsedMilliSecond(0);
+        setLapList([]);
+    }
+
     return(
-        <div className="displayContainer"> 
-          <h1>{formattedElapsedMilliSecond}</h1> 
+        <div>
+            <div className="stopwatchContainer">
+                <div className="displayContainer"> 
+                    <h1>{formatMilliSeconds(elapsedMilliSecond)}</h1> 
+                </div>
+                <div className="lapDisplay">Lap: {formatMilliSeconds(lapElaspedMilliSecond)}</div>
+                <div className="buttonContainer">
+                    {!isTimeRunning && <StopWatchButton className="startButton" onClick={() => setIsTimeRunning(true)} label={elapsedMilliSecond == 0 ? "Start" : "Resume"}/>}
+                    {isTimeRunning && <StopWatchButton className="lapButton" onClick={() => isTimeRunning && setIsLapClicked(true)} label="Lap"/>}
+                    {isTimeRunning && <StopWatchButton className="stopButton" onClick={() => setIsTimeRunning(false)} label="Stop"/>}
+                    {<StopWatchButton className="resetButton" onClick={() => handleReset()} label="Reset"/>}
+                </div>
+            </div>
+
+            <div className="lapTableContainer">
+                {lapList.length > 0 && <table className="lapTable">
+                <thead>
+                    <tr>
+                    <th>Lap No.</th>
+                    <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayLapTable()}
+                </tbody>
+                </table>}
+            </div>                   
         </div>
     )
 }
