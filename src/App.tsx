0fe4c6eb1b7696b39c096fdf85fdd60e9e79a960
qureshe00 <qureshe@mcmaster.ca>
2024-01-29 02:38:30 -0500

    add time display and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ export default function App() {
         if(isStartPressed){
         //every 1000 miliseconds (i.e every second), calculate the elapsed time until interval is cleared
         intervalId = setInterval(()=>{
-            setCurrentTime(new Date().valueOf() - startTime.valueOf())
+            setCurrentTime(currentTime + 1)
+            console.log(currentTime)
         }, 1000)}
         return()=>{
             clearInterval(intervalId)
         }
         }
-    ,[isStartPressed])
+    ,[isStartPressed, currentTime])
     
 
     function onClickStart(){
@@ -27,11 +28,24 @@ export default function App() {
         setIsStartPressed(false)
     }
 
+    function secondsMinutesHours(value: number){
+        //calculate hours
+        const hours = Math.trunc(value/(60*60))
+
+        //calculate minutes
+        const minutes = Math.trunc((value - hours*60*60)/60)
+
+        //calculate seconds
+        const seconds = Math.trunc(value - hours*60*60 - minutes*60)
+
+        return hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0")
+    }
+
     return(
         <div>
             <button onClick={() => onClickStart()}>Start</button>
             <button onClick={() => onClickStop()}>Stop</button>
-            <div>{currentTime}</div>
+            <div>{secondsMinutesHours(currentTime)}</div>
         </div>
     )
 }
