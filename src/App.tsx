7a87d06d868fcd5f46324877a167640e69af8114
Nicole Cevey <nicole.cevey@gmail.com>
2024-01-29 18:28:06 -0500

    Adding basic ui and start, stop, and reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './App.css'
 import StopWatch from './StopWatch'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(fas);
+
 
 export default function App() {
     return(
-        <StopWatch />
+        <div className='app'>
+            <span className='heading-wrapper'>
+                <span className='icon-wrapper'>
+                    <FontAwesomeIcon icon={['fas', 'stopwatch']} size='xl'/>
+                </span>
+                <h1 className='heading'>Shopify Stopwatch</h1>
+            </span>
+            <StopWatch />
+        </div>
     )
 }
