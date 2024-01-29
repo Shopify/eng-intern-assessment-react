6c1c0ee798d8e09e59c246ccbafdc0aa94981ff3
Akshen Jasikumar <akshen.jasikumar@gmail.com>
2024-01-28 20:24:37 -0500

    Fixed type error

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
