d997eadc97d75f955d85c7cc07f32fe2ffb83bbc
Deniz Jasarbasic <denizj2828@gmail.com>
2024-01-29 14:02:23 -0500

    added a footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Made with ❤️ by <a href="https://denizjasarbasic.com" target="_blank" rel="noopener noreferrer">Deniz Jasarbasic</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
