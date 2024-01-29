231803c592bef4eacdb907d514ea727a0b8d9043
Amaan Farooqi <amaan.farooqi2020@gmail.com>
2024-01-29 18:07:33 -0500

    Add Stopwatch component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// App.tsx
+import React from 'react';
+import StopWatch from './StopWatch';
+
+export default function App() {
+    return(
+        <div>
+            <StopWatch />
+        </div>
+    );
+}
