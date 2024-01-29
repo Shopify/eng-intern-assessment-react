36542ff623628eafd9eb39f70de6b638dce896d1
gdavidcho <david@innovibe.ca>
2024-01-28 16:29:39 -0800

    feat: added logic for start/stop and reset time

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  // state for time
+  const [time, setTime] = useState(0);
+  // state to check if stopwatch is running
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval: number;
+    if (isRunning) {
+      interval = window.setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning, time]);
+
+  // time caclulation for hours, minutes, seconds, and milliseconds
+  const hours = Math.floor(time / 360000);
+  const minutes = Math.floor((time % 360000) / 6000);
+  const seconds = Math.floor((time % 6000) / 100);
+  const milliseconds = time % 100;
+
+  // start and stop time
+  const startAndStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  // reset timer to 0
+  const reset = () => {
+    if (isRunning) {
+      setTime(0);
+      setIsRunning(!isRunning);
+    } else {
+      setTime(0);
+      setIsRunning(isRunning);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {hours.toString().padStart(2, "0")}:
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </div>
+      <div>
+        <button onClick={startAndStop}>{isRunning ? "Stop" : "Start"}</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
 }
