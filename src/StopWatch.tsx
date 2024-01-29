693946668d657956b930c45a760fd2a50bffbe81
Meredith Jonatan <mjonatan@me.com>
2024-01-29 15:17:52 -0500

    added icon to index.html, made sure formatting was correct for StopWatch component

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 interface timeInSecondsProps {
     timeInSeconds: number;
