438e26ea48b41a395c00b01879be34f54a923522
beatuano <beatuano@gmail.com>
2024-01-29 11:37:38 -0500

    components complete, installed testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import StopWatchButton from './StopWatchButton'
+import React from "react";
+import { useState, useEffect } from "react";
+import StopWatch from "./StopWatch";
+import StopWatchButton from "./StopWatchButton";
 
 export default function App() {
-    return(
-        <div>
-            <StopWatchButton />
-        </div>
-    )
+  const [timer, setTimer] = useState<number>(0);
+  const [toggle, setToggle] = useState<boolean>(false);
+  const [laps, setLaps] = useState<number[]>([]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    // when toggle is true, start timer
+    if (toggle) {
+      intervalId = setInterval(() => {
+        setTimer((prevTime) => prevTime + 10);
+      }, 10);
+
+      // when toggle is false, stop & reset timer
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [toggle]);
+
+  console.log(toggle);
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <StopWatch timer={timer} laps={laps} />
+      <StopWatchButton
+        setToggle={setToggle}
+        setTimer={setTimer}
+        setLaps={setLaps}
+        timer={timer}
+      />
+    </div>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,30 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-
-export default function StopWatchButton() {
-    const [timer, setTimer] = useState<number>(0)
-    const [counting, setCounting] = useState<boolean>(false);
-    const [lap, setLap] = useState<number>(0);
-
- const handleClick = () => {setLap(lap + 1);};
- 
-  return (
-    <div>
-      <h1>Stopwatch</h1>
-      <div>
-        <span>{"0" + Math.floor((timer / 60000) % 60)};</span>
-        <span>{"0" + Math.floor((timer / 1000) % 60)};</span>
-        <span>{"0" + ((timer / 10) % 100)};</span>
-
-        <button onClick={() =>{setCounting(true)}}>Start</button>
-        <button onClick={() =>{setCounting(false)}}>Stop</button>
-        <button onClick={() =>{setTimer(0)}}>Reset</button>
-        <button onClick={handleClick}>Lap</button>
-        <button onClick={() => {setLap(0)}}>Reset Laps</button>
-      </div>
-      <p>Laps: {lap}</p>
-    </div>
-  );
+interface StopWatchButtonProps {
+  setToggle: React.Dispatch<React.SetStateAction<boolean>>;
+  timer: number;
+  setTimer: React.Dispatch<React.SetStateAction<number>>;
+  setLaps: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
+const StopWatchButton: React.FC<StopWatchButtonProps> = ({
+  setToggle,
+  setTimer,
+  setLaps,
+  timer,
+}) => {
+  const handleReset = () => {
+    setToggle(false);
+    setTimer(0);
+    setLaps([]);
+  };
+  return (
+    <div>
+      <button onClick={() => setToggle(true)}>Start</button>
+      <button onClick={() => setToggle(false)}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={() => setLaps((prevLaps) => [...prevLaps, timer])}>
+        Lap
+      </button>
+    </div>
+  );
+};
+
+export default StopWatchButton;
