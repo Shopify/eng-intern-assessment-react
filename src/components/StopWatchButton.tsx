f535b1896516e6e7ae8b295891ff11fb63955e78
Chelsea Dwarika <chelseakristina@live.com>
2024-01-29 18:01:01 -0500

    btn-selected styles

--- src/components/StopWatchButton.tsx
+++ src/components/StopWatchButton.tsx
@@ -1,5 +1,4 @@
-// StopWatchButton.tsx
-import React from 'react';
+import React, { useState } from 'react';
 
 interface StopWatchButtonProps {
   onStart: () => void;
@@ -14,16 +13,33 @@ const StopWatchButton: React.FC<StopWatchButtonProps> = ({
   onReset,
   isRunning,
 }) => {
-    
+  const [selectedButton, setSelectedButton] = useState<string | null>(null);
+
+  const handleButtonClick = (action: string, callback: () => void) => {
+    setSelectedButton(action);
+    callback();
+  };
+
   return (
     <div>
-      <button className='btn' onClick={onStart} disabled={isRunning}>
+      <button
+        className={`btn ${selectedButton === 'start' ? 'selected' : ''}`}
+        onClick={() => handleButtonClick('start', onStart)}
+        disabled={isRunning}
+      >
         Start
       </button>
-      <button className='btn' onClick={onStop} disabled={!isRunning}>
+      <button
+        className={`btn ${selectedButton === 'stop' ? 'selected' : ''}`}
+        onClick={() => handleButtonClick('stop', onStop)}
+        disabled={!isRunning}
+      >
         Stop
       </button>
-      <button className='btn' onClick={onReset}>
+      <button
+        className={`btn ${selectedButton === 'reset' ? 'selected' : ''}`}
+        onClick={() => handleButtonClick('reset', onReset)}
+      >
         Reset
       </button>
     </div>
