20f8983b3e0fb4329a4849e6edb015d5f1a9ea17
Krish120003 <krish120003@gmail.com>
2024-01-29 04:20:44 -0500

    style: bigger button

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -6,7 +6,7 @@ export default function StopWatchButton(
   return (
     <button
       {...props}
-      className={`p-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-600 rounded active:translate-y-[0.1em] transition-transform ${props.className}`}
+      className={`p-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-600 rounded active:translate-y-[0.1em] transition-transform text-2xl ${props.className}`}
     >
       {props.children}
     </button>
