aa1452fe0bba6a80b912368aa29609e094873f7d
anyamittal08 <anyamittal08@gmail.com>
2024-01-28 22:08:53 -0500

    add data-testids

--- src/components/Lap.tsx
+++ src/components/Lap.tsx
@@ -19,20 +19,32 @@ const Lap: React.FC<LapProps> = ({
     <div className="flex justify-between text-xl items-center">
       <div className=" min-w-48 ">{index ? `${index}` : "Current"}</div>
       <div className="flex justify-center text-xl py-5 -space-x-1 min-w-48">
-        <div className=" min-w-14">{`${relativeLapTimeBreakdown.minutes}`}</div>
+        <div
+          className=" min-w-14"
+          data-testid={`rel-lap-${index}-minutes`}
+        >{`${relativeLapTimeBreakdown.minutes}`}</div>
         <div>:</div>
-        <div className=" min-w-14">{`${relativeLapTimeBreakdown.seconds}`}</div>
+        <div
+          className=" min-w-14"
+          data-testid={`rel-lap-${index}-seconds`}
+        >{`${relativeLapTimeBreakdown.seconds}`}</div>
         <div>.</div>
-        <div className="min-w-14">
+        <div className="min-w-14" data-testid={`rel-lap-${index}-ms`}>
           {`${relativeLapTimeBreakdown.milliseconds.slice(0, 2)}`}
         </div>
       </div>
       <div className="flex justify-center text-xl py-5 -space-x-1 min-w-48">
-        <div className=" min-w-14">{`${totalLapTimeBreakdown.minutes}`}</div>
+        <div
+          className=" min-w-14"
+          data-testid={`abs-lap-${index}-minutes`}
+        >{`${totalLapTimeBreakdown.minutes}`}</div>
         <div>:</div>
-        <div className=" min-w-14">{`${totalLapTimeBreakdown.seconds}`}</div>
+        <div
+          className=" min-w-14"
+          data-testid={`abs-lap-${index}-seconds`}
+        >{`${totalLapTimeBreakdown.seconds}`}</div>
         <div>.</div>
-        <div className="min-w-14">
+        <div className="min-w-14" data-testid={`abs-lap-${index}-ms`}>
           {`${totalLapTimeBreakdown.milliseconds.slice(0, 2)}`}
         </div>
       </div>
