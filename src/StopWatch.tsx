babdb117af48b2e8ce5d77668d88257b694778e1
Brahmpreet Mondair <brahm.mondair@outlook.com>
2024-01-29 18:58:58 -0500

    added more comments

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,13 +1,19 @@
+//Imports for all necessary files
 import React from 'react'
-import './CSS Files/App.css'; // Ensure this file exists and contains your styles
+import './CSS Files/App.css';
 import App from './App';
 import StopWatchButton from './StopWatchButton';
 import calculateTimer from './Helper/CalculateTimer';
 
+// Props type definition for the Stopwatch component.
 type Props = {
     timerArray: Array<number | string>;
+
   };
+// The Stopwatch functional component displays the timer.
+// It expects an array with the current time split into hours, minutes, and seconds.
   const Stopwatch: React.FC<Props> = ({ timerArray }) => {
+// Render the stopwatch display using the timerArray values.
     return (
       <section className='time-container'>
         <p className='timer-text'>{timerArray[0]}</p>
