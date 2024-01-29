f421645627a7f1e6b96569c725250eb85a696eed
gdcho <david@innovibe.ca>
2024-01-28 20:10:38 -0800

    feat: configured tailwind css for styling

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -26,36 +26,38 @@ export default function StopWatchButton({
   return (
     // button group for start, stop, reset, and lap buttons
     // button group from polaris
-    <ButtonGroup variant="segmented">
-      {/* start stop button */}
-      <Button
-        onClick={onStartStop}
-        accessibilityLabel={isRunning ? "Stop" : "Start"}
-        textAlign="center"
-        size="large"
-        tone={isRunning ? "critical" : "success"}
-      >
-        {isRunning ? stop : start}
-      </Button>
-      {/* reset button */}
-      <Button
-        onClick={onReset}
-        accessibilityLabel="Reset"
-        textAlign="center"
-        size="large"
-      >
-        {reset}
-      </Button>
-      {/* lap button */}
-      <Button
-        onClick={onLap}
-        accessibilityLabel="Lap"
-        disabled={!isRunning}
-        textAlign="center"
-        size="large"
-      >
-        {lap}
-      </Button>
-    </ButtonGroup>
+    <div>
+      <ButtonGroup variant="segmented">
+        {/* start stop button */}
+        <Button
+          onClick={onStartStop}
+          accessibilityLabel={isRunning ? "Stop" : "Start"}
+          textAlign="center"
+          size="large"
+          tone={isRunning ? "critical" : "success"}
+        >
+          {isRunning ? stop : start}
+        </Button>
+        {/* reset button */}
+        <Button
+          onClick={onReset}
+          accessibilityLabel="Reset"
+          textAlign="center"
+          size="large"
+        >
+          {reset}
+        </Button>
+        {/* lap button */}
+        <Button
+          onClick={onLap}
+          accessibilityLabel="Lap"
+          disabled={!isRunning}
+          textAlign="center"
+          size="large"
+        >
+          {lap}
+        </Button>
+      </ButtonGroup>
+    </div>
   );
 }
