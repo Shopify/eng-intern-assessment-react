48b098e85717042d16a45202ac9bb9a6664db2a7
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-01-29 12:55:34 -0500

    Added comments

--- src/components/StopWatchButton.tsx
+++ src/components/StopWatchButton.tsx
@@ -7,6 +7,11 @@ interface buttonProps {
 }
 
 const StopWatchButton = ({ name, stopwatchState, handleButtonClick }: buttonProps) => {
+    // certain buttons should be disabled depending on the stopwatch state
+    // disabled button conditions:
+        // disable all buttons except the start button when the stopwatch has been reset (default)
+        // disable only the start button when the stopwatch is running
+        // disable only the stop button when the stopwatch is stopped/paused
     if (
         (stopwatchState === 'reset' && name !== 'start')
         || (stopwatchState === 'started' && name === 'start')
@@ -15,6 +20,7 @@ const StopWatchButton = ({ name, stopwatchState, handleButtonClick }: buttonProp
         return (
             <button className='digital' onClick={handleButtonClick} disabled>{name}</button>
         )
+    // if the none of the above conditions are met, render a normal/non-disabled button
     } else return (
         <button className='digital' onClick={handleButtonClick}>{name}</button>
     )
--- src/utils/formatTime.tsx
+++ src/utils/formatTime.tsx
@@ -1,13 +1,19 @@
 const formatTime = (timeInMilliseconds: number): string => {
+    // converting the recorded time from a value in milliseconds
+    // into separate minutes, seconds, and milliseconds values
     const minutes = Math.floor((timeInMilliseconds % 3600000) / 60000);
     const seconds = Math.floor((timeInMilliseconds % 60000) / 1000);
     const milliseconds = Math.floor((timeInMilliseconds % 1000) / 10);
 
+    // ensuring that each time value is displayed in two digits
+    // so that the html element has consistent dimensions.
+    // if the input value is less than 10, output it with a zero in front of it
     const doubleDigits = (input: number) => {
         const output: string|number = input < 10 ? `0${input}` : input;
         return output;
     };
 
+    // combining the separate time values into a user-friendly format
     return `${doubleDigits(minutes)}:${doubleDigits(seconds)}.${doubleDigits(milliseconds)}`;
 }
 
