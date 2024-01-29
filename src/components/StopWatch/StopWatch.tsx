049c0282298868859baae635b86eec614fc45755
Jordan Na <jordster02@gmail.com>
2024-01-29 17:41:44 -0500

    Added logic for stopwatch and laps

--- src/components/StopWatch/StopWatch.tsx
+++ src/components/StopWatch/StopWatch.tsx
@@ -1,20 +1,21 @@
-import React, { useCallback } from 'react'
-import './StopWatch.css'
-import StopWatchButton from '../StopWatchButton/StopWatchButton'
-import getFormattedTime from '../../utils/time-formatter'
+import React, { useCallback, useEffect, useRef } from "react";
+import "./StopWatch.css";
+import StopWatchButton from "../StopWatchButton/StopWatchButton";
+import { getFormattedTime, getFormattedTimeObject } from "../../utils/time-formatter";
 
 export default function StopWatch() {
-
-   const [time, setTime] = React.useState(0)
-   const [isRunning, setIsRunning] = React.useState(false)
+   const [time, setTime] = React.useState(0);
+   const [isRunning, setIsRunning] = React.useState(false);
    const [laps, setLaps] = React.useState([]);
 
-   const {hours, minutes, seconds, milliseconds} = getFormattedTime(time);
+   const { hours, minutes, seconds, milliseconds } = getFormattedTimeObject(time);
+
+   const lapsRef = useRef(null);
 
    const resetTimer = useCallback(() => {
-      setTime(0)
-      setIsRunning(false)
-      setLaps([])
+      setTime(0);
+      setIsRunning(false);
+      setLaps([]);
    }, []);
 
    const startTimer = useCallback(() => {
@@ -26,24 +27,71 @@ export default function StopWatch() {
    }, []);
 
    const addLap = useCallback(() => {
-      setLaps((prevLaps) => [...prevLaps, time]);
+      setLaps((prevLaps) => {
+         const lapTime = time - prevLaps.reduce((acc, curr) => acc + curr.lapTime, 0);
+         const overallTime = time;
+         return [...prevLaps, { lapTime, overallTime }];
+      });
    }, [time]);
 
-    return(
-        <div className='stopwatch'>
-            <div className='time'>
-               <p className='hour'>00</p>
-               <p>:</p>
-               <p className='seconds'>00</p>
-               <p>.</p>
-               <p className='milliseconds'>00</p>
+   useEffect(() => {
+      if (lapsRef.current) {
+         const { scrollHeight, clientHeight } = lapsRef.current;
+         lapsRef.current.scrollTop = scrollHeight - clientHeight;
+      }
+   }, [laps]);
+
+   useEffect(() => {
+      let interval: ReturnType<typeof setInterval> | null = null;
+      if (isRunning) {
+         interval = setInterval(() => setTime((time) => time + 1), 10);
+      }
+      return () => {
+         clearInterval(interval);
+      };
+   }, [isRunning]);
+
+   return (
+      <div className="stopwatch">
+         <div className="time">
+            {hours !== "00" && (
+               <>
+                  <p className="numbers hours">{hours}</p>
+                  <p>:</p>
+               </>
+            )}
+            <p className="number minutes">{minutes}</p>
+            <p>:</p>
+            <p className="number seconds">{seconds}</p>
+            <p>.</p>
+            <p className="number milliseconds">{milliseconds}</p>
+         </div>
+         <div className="buttons">
+            <StopWatchButton type="start" onClick={startTimer} isRunning={isRunning} />
+            <StopWatchButton type="stop" onClick={stopTimer} isRunning={isRunning} />
+            <StopWatchButton type="lap" onClick={addLap} isRunning={isRunning} />
+            <StopWatchButton type="reset" onClick={resetTimer} isRunning={isRunning} />
+         </div>
+         {laps.length > 0 && <div className="laps-container">
+            <div className="columns">
+               <div>Lap</div>
+               <div>Lap Times</div>
+               <div>Overall Time</div>
             </div>
-            <div className='buttons'>
-               <StopWatchButton type='start'onClick={startTimer}/>
-               <StopWatchButton type='stop'onClick={stopTimer}/>
-               <StopWatchButton type='lap'onClick={addLap}/>
-               <StopWatchButton type='reset'onClick={resetTimer}/>
-            </div>
-        </div>
-    )
+            <ul className="laps" ref={lapsRef}>
+               {laps.map((lap, index) => {
+                  const formattedLapTime = getFormattedTime(lap.lapTime);
+                  const formattedOverallTime = getFormattedTime(lap.overallTime);
+                  return (
+                     <li className="lap-item" key={`${index}${lap}`}>
+                        <div>{index + 1}</div>
+                        <div>{formattedLapTime}</div>
+                        <div>{formattedOverallTime}</div>
+                     </li>
+                  );
+               })}
+            </ul>
+         </div>}
+      </div>
+   );
 }
