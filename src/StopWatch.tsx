7706b9b9e196123df1836401e57579f8e4b41c1d
prach19 <prachi19@my.yorku.ca>
2024-01-29 10:11:28 -0500

    formatting and comments

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -3,43 +3,52 @@ import StopWatchButton from "./StopWatchButton";
 //import { time } from "console";
 
 export default function StopWatch() {
+
+    //defining variables that will change states
     const [seconds, setSeconds] = useState<number>(0);
     const [minutes, setMinutes] = useState<number>(0);
     const [hours, setHours] = useState<number>(0);
-    const [laps, setLaps] = useState<Array<string>>(["00:00:00"]);
-    const [totalTime, setTotalTime] = useState<Array<string>>(["00:00:00"]);
-    const [run, setRun] = useState<boolean>(false);
+    const [laps, setLaps] = useState<Array<string>>(["00:00:00"]); //array that records/stores the laps
+    const [totalTime, setTotalTime] = useState<Array<string>>(["00:00:00"]); //array that records/stores the total time elapsed when lap was recorded
+    const [run, setRun] = useState<boolean>(false); //tracks start/stop
 
+    //function to format single digit numbers with 0s, making the numbers easier to read for the user
     const formatZero = (num: number): string => {
         if (num < 10) {
             return "0" + num;
         } else return num.toString();
     };
 
+    //function to record laps, takes a boolean as a parameter to track whether or not the array should be empty on reset
     const recordLap = (reset: boolean) => {
         if (reset) {
             setLaps([]);
         } else {
-            let lastLap: string = totalTime.slice(-1).toString();
-            let newLap: string = formatZero(hours) + ":" + formatZero(minutes) + ":" + formatZero(seconds);
+            let lastLap: string = totalTime.slice(-1).toString(); //take the time for the last lap
+            let newLap: string = formatZero(hours) + ":" + formatZero(minutes) + ":" + formatZero(seconds); //take the current time
 
+            //turn the last lap and current time into Date objects for further calculation
             let time1 = new Date(`2011-10-10T${lastLap}`);
             let time2 = new Date(`2011-10-10T${newLap}`);
 
+            //calculate the difference between the last lap and current time
             let secDiff = Math.abs(time2.getSeconds() - time1.getSeconds());
             let minDiff = Math.abs(time2.getMinutes() - time1.getMinutes());
             let hrsDiff = Math.abs(time2.getHours() - time1.getHours());
 
+            //create a string with the difference in time
             let resultLap: string = `${formatZero(hrsDiff)}:${formatZero(minDiff)}:${formatZero(secDiff)}`;
 
-            setLaps([...laps, resultLap]);
-            setTotalTime([...totalTime, newLap]);
+            setLaps([...laps, resultLap]);  //adding the difference as a lap in the lap array
+            setTotalTime([...totalTime, newLap]); //adding the current time to the totalTime array
         }
     };
 
+    //using the useEffect hook for the timer with window.setInterval as a timer
     useEffect(() => {
         let interval: number;
 
+        //if the start button is pressed, start the interval and calculate the seconds, minutes, hours from that interval at every second
         if (run) {
             interval = window.setInterval(() => {
                 setSeconds((seconds: number) => {
@@ -62,6 +71,8 @@ export default function StopWatch() {
                 return 0;
             }, 1000);
         }
+
+        //if not running, then stop the timer where it is
         return () => clearInterval(interval);
     }, [run]);
 
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,50 +1,57 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
+
+//Using props to 'link' or help componenets communicate with each other
+//Using an interface to contain all the functions because the StopwatchButton function can only take at most 2 arguments but more is required for button functionality
 interface timerProps {
-    setRun: Function;
-    setSeconds: Function;
-    setMinutes: Function;
-    setHours: Function;
-    recordLap: Function;
-        
+  setRun: Function;
+  setSeconds: Function;
+  setMinutes: Function;
+  setHours: Function;
+  recordLap: Function;
 }
 
-export default function StopWatchButton(props:timerProps) {
-    const setRun = props.setRun
-    const setSeconds = props.setSeconds
-    const setMinutes = props.setMinutes
-   const setHours = props.setHours
-   const setLaps = props.recordLap
+//button functionality
+//uses an object of the timerProps interface to communicate with the Stopwatch component
+export default function StopWatchButton(props: timerProps) {
+  const setRun = props.setRun;
+  const setSeconds = props.setSeconds;
+  const setMinutes = props.setMinutes;
+  const setHours = props.setHours;
+  const setLaps = props.recordLap;
 
-    const startButton = () => {
-        setRun(true);
-    }
-
-    const stopButton = () => {
-        setRun(false);
-    }
-
-    const resetButton = () => {
-        setRun(false);
-        setSeconds(0)
-        setMinutes(0)
-        setHours(0)
-        setLaps(true)
-        
-    }
-
-    const lapButton = () => {
-        setLaps(false);
-        
-    }
+  //if start button is pressed, then set the run property to true in the Stopwatch component
+  const startButton = () => {
+    setRun(true);
+  };
 
 
-    return(
-        <div>
-              <button onClick={startButton}> start </button> 
-                <button onClick={stopButton}> stop </button> 
-                <button onClick={lapButton}> lap </button> 
-                <button onClick={resetButton}> reset </button> 
-        </div>
-    )
+  //if stop button is pressed, then set the run property to false in the Stopwatch component
+  const stopButton = () => {
+    setRun(false);
+  };
+
+  //if reset button is pressed, then reset timer to 0, turn timer off, and empty the laps table
+  const resetButton = () => {
+    setRun(false);
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+    setLaps(true);
+  };
+
+  //record a lap when button is clicked
+  const lapButton = () => {
+    setLaps(false);
+  };
+
+  //return the buttons
+  return (
+    <div>
+      <button onClick={startButton}> start </button>
+      <button onClick={stopButton}> stop </button>
+      <button onClick={lapButton}> lap </button>
+      <button onClick={resetButton}> reset </button>
+    </div>
+  );
 }
