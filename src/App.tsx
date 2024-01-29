b383745c581a7154bbd36b416c2cbb5d5a944c7e
Hans Lam <hans.kt.lam@gmail.com>
2024-01-28 23:54:29 -0800

    Basic Functionality Done
    Basic functionality for each of the components is done:
    Created states in parent component App to store values
    Added props in StopWatch and StopWatchButton components
    Added buttonMapping for buttons in StopWatchButton, which all set state values in App appropriately
    Added time display to StopWatch
    Added temporary laps display to StopWatch

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import StopWatch from './StopWatch'
 import StopWatchButton from './StopWatchButton'
 
 export default function App() {
+    const [isRunning, setRunning] = useState(false);
+    const [time, setTime] = useState(0);
+    const [laps, setLaps] = useState<number[]>([]);
+
     return(
         <div>
             <h1>Stopwatch</h1>
-            <StopWatch/>
+            <StopWatch isRunning={isRunning} time={time} setTime={setTime} laps={laps}/>
             <div>
-                <StopWatchButton text='Start'/>
-                <StopWatchButton text='Stop'/>
-                <StopWatchButton text='Reset'/>
-                <StopWatchButton text='Lap'/>
+                <StopWatchButton text='Start' setValue={setRunning}/>
+                <StopWatchButton text='Stop'setValue={setRunning}/>
+                <StopWatchButton text='Reset'setValue={setTime}/>
+                <StopWatchButton text='Lap' time={time} setLaps={setLaps}/>
             </div>
         </div>
     );
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,11 +1,48 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 type Props = {
     text: string;
-} 
+    setValue?: React.Dispatch<React.SetStateAction<boolean|number>>;
+    time?: number;
+    setLaps?: React.Dispatch<React.SetStateAction<number[]>>;
+};
+
+type StrToFuncMap = {
+    [key: string]: () => void;
+};
 
 export default function StopWatchButton(props: Props) {
+    const maxLaps = 5;
+
+    const buttonMap: StrToFuncMap = {
+        "Start": () => {
+            props.setValue(true);
+        }, 
+        "Stop": () => {
+            props.setValue(false);
+        }, 
+        "Reset": () => {
+            props.setValue(0); 
+        }, 
+        "Lap": () => {
+            // props.setLaps((prevLaps) => [...prevLaps, props.time]);
+            props.setLaps((prevLaps) => {
+                if (prevLaps.length < maxLaps) {
+                  // Add lap if there is space
+                  return [...prevLaps, props.time];
+                } else {
+                  // Replace the earliest lap with the newest lap
+                  const updatedLaps = [...prevLaps];
+                  updatedLaps.shift();
+                  updatedLaps.push(props.time);
+                  return updatedLaps;
+                }
+            });
+        }
+    };
+
     let onClick = function() {
+        buttonMap[props.text]();
     };
 
     return(
