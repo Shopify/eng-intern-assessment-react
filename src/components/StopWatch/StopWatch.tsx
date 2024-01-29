94ddb4d36690379e03bb417cffb5f3b3efe054cd
Jordan Na <jordster02@gmail.com>
2024-01-29 15:52:33 -0500

    Added stopwatch states

--- src/components/StopWatch/StopWatch.tsx
+++ src/components/StopWatch/StopWatch.tsx
@@ -1,25 +1,49 @@
-import React, { useCallback } from "react";
-import "./StopWatch.css";
-import StopWatchButton from "../StopWatchButton/StopWatchButton";
+import React, { useCallback } from 'react'
+import './StopWatch.css'
+import StopWatchButton from '../StopWatchButton/StopWatchButton'
+import getFormattedTime from '../../utils/time-formatter'
 
 export default function StopWatch() {
 
+   const [time, setTime] = React.useState(0)
+   const [isRunning, setIsRunning] = React.useState(false)
+   const [laps, setLaps] = React.useState([]);
 
-   return (
-      <div className="stopwatch">
-         <div className="time">
-            <p className="hour">00</p>
-            <p>:</p>
-            <p className="seconds">00</p>
-            <p>.</p>
-            <p className="milliseconds">00</p>
-         </div>
-         <div className="buttons">
-            <StopWatchButton type="start" onClick={null} />
-            <StopWatchButton type="stop" onClick={null} />
-            <StopWatchButton type="lap" onClick={null} />
-            <StopWatchButton type="reset" onClick={null} />
-         </div>
-      </div>
-   );
+   const {hours, minutes, seconds, milliseconds} = getFormattedTime(time);
+
+   const resetTimer = useCallback(() => {
+      setTime(0)
+      setIsRunning(false)
+      setLaps([])
+   }, []);
+
+   const startTimer = useCallback(() => {
+      setIsRunning(true);
+   }, []);
+
+   const stopTimer = useCallback(() => {
+      setIsRunning(false);
+   }, []);
+
+   const addLap = useCallback(() => {
+      setLaps((prevLaps) => [...prevLaps, time]);
+   }, [time]);
+
+    return(
+        <div className='stopwatch'>
+            <div className='time'>
+               <p className='hour'>00</p>
+               <p>:</p>
+               <p className='seconds'>00</p>
+               <p>.</p>
+               <p className='milliseconds'>00</p>
+            </div>
+            <div className='buttons'>
+               <StopWatchButton type='start'onClick={startTimer}/>
+               <StopWatchButton type='stop'onClick={stopTimer}/>
+               <StopWatchButton type='lap'onClick={addLap}/>
+               <StopWatchButton type='reset'onClick={resetTimer}/>
+            </div>
+        </div>
+    )
 }
