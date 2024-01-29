7fb4d1db2bfce48a180a3af56db5ca89dba62355
anyamittal08 <anyamittal08@gmail.com>
2024-01-28 22:21:39 -0500

    refactor lapTime

--- src/components/Lap.tsx
+++ src/components/Lap.tsx
@@ -8,6 +8,31 @@ interface LapProps {
   index?: number;
 }
 
+interface LapTimeProps {
+  time: { minutes: string; seconds: string; milliseconds: string };
+}
+
+const LapTime: React.FC<LapTimeProps> = ({ time }) => {
+  return (
+    <>
+      <div
+        className=" min-w-14"
+        data-testid="lap-minutes"
+      >{`${time.minutes}`}</div>
+      <div>:</div>
+      <div
+        className=" min-w-14"
+        data-testid="lap-seconds"
+      >{`${time.seconds}`}</div>
+      <div>.</div>
+      <div
+        className="min-w-14"
+        data-testid="lap-ms"
+      >{`${time.milliseconds.slice(0, 2)}`}</div>
+    </>
+  );
+};
+
 const Lap: React.FC<LapProps> = ({
   totalLapTime: totalLapTime,
   relativeLapTime,
@@ -19,34 +44,10 @@ const Lap: React.FC<LapProps> = ({
     <div className="flex justify-between text-xl items-center">
       <div className=" min-w-48 ">{index ? `${index}` : "Current"}</div>
       <div className="flex justify-center text-xl py-5 -space-x-1 min-w-48">
-        <div
-          className=" min-w-14"
-          data-testid={`rel-lap-${index}-minutes`}
-        >{`${relativeLapTimeBreakdown.minutes}`}</div>
-        <div>:</div>
-        <div
-          className=" min-w-14"
-          data-testid={`rel-lap-${index}-seconds`}
-        >{`${relativeLapTimeBreakdown.seconds}`}</div>
-        <div>.</div>
-        <div className="min-w-14" data-testid={`rel-lap-${index}-ms`}>
-          {`${relativeLapTimeBreakdown.milliseconds.slice(0, 2)}`}
-        </div>
+        <LapTime time={relativeLapTimeBreakdown} />
       </div>
       <div className="flex justify-center text-xl py-5 -space-x-1 min-w-48">
-        <div
-          className=" min-w-14"
-          data-testid={`abs-lap-${index}-minutes`}
-        >{`${totalLapTimeBreakdown.minutes}`}</div>
-        <div>:</div>
-        <div
-          className=" min-w-14"
-          data-testid={`abs-lap-${index}-seconds`}
-        >{`${totalLapTimeBreakdown.seconds}`}</div>
-        <div>.</div>
-        <div className="min-w-14" data-testid={`abs-lap-${index}-ms`}>
-          {`${totalLapTimeBreakdown.milliseconds.slice(0, 2)}`}
-        </div>
+        <LapTime time={totalLapTimeBreakdown} />
       </div>
     </div>
   );
