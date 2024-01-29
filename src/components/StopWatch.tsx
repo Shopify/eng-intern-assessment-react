3e119c13d095d9c9dcab3ca25c782580b8954445
Jake Spodek <68516673+jakespodek@users.noreply.github.com>
2024-01-28 19:04:04 -0500

    Formatting
    Added some semicolons

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import formatTime from '../utils/formatTime'
+import React from 'react';
+import formatTime from '../utils/formatTime';
 
 interface stopWatchProps {
-    time: number
+    time: number;
 }
 
 const StopWatch = ({time} : stopWatchProps) => {
