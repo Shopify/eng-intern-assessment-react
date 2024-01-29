aeb70cd96d375a789869d46581297cd7a7827f31
Hara Hordofa <hhordofa1@gmail.com>
2024-01-29 00:35:16 -0500

    setup the StopWatch component, added states to track time, added function to start time and onclick function on start button

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+  // state to keep track of time
+  const [time, setTime] = useState<number>(0);
+
+  // state to track if the time has started or stopped
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  // function for starting the stopwatch
+  const startTime = () => {
+    setIsRunning(true);
+    // time to increase by an interval of one second
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    // clear interval
+    return () => {
+      clearInterval(interval);
+      setIsRunning(false);
+    };
+  };
+
+  return (
+    <div>
+      <div>{`${time} seconds`}</div>
+      <StopWatchButton title="Start" onClick={startTime} />
+      {/* <StopWatchButton title="Stop" />
+      <StopWatchButton title="Reset" />
+      <StopWatchButton title="Lap" /> */}
+    </div>
+  );
 }
