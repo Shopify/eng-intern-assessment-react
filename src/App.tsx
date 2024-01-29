8a449fc5009739fc97d4454c7c8e71c0a98e04d2
Chelsea Dwarika <chelseakristina@live.com>
2024-01-29 13:52:27 -0500

    initial CSS styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function App() {
     return(
         <div></div>
--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './StopWatch.css';
 
 export default function StopWatch() {
-    return(
-        <div></div>
-    )
+    return (
+        <section className='container'>
+            <h1 className='text'>0</h1>
+            <span>:</span>
+            <h1 className='text'>0</h1>
+            <span>:</span>
+            <h1 className='text'>0</h1>
+        </section>
+            )
 }
