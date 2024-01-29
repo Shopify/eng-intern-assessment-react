000a70f9f3f0a084aceef713cc82e532e51f0962
Jianghui-Lai <laijianghui@gmail.com>
2024-01-28 21:01:41 -0700

    add start stop and time

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from "react";
+import DisplayComponent from "./Components/DisplayComponent";
+import BtnComponent from "./Components/BtnComponent";
+import "./StopWatch.css";
 
-export default function StopWatch() {
-    return(
-        <div></div>
-    )
+interface Time {
+  ms: number;
+  s: number;
+  m: number;
+  h: number;
 }
+
+function StopWatch() {
+  const [time, setTime] = useState<Time>({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interv, setInterv] = useState<NodeJS.Timeout | undefined>();
+  const [status, setStatus] = useState<number>(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    if (!interv) {
+      setInterv(setInterval(run, 10));
+    }
+  };
+
+  let updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  };
+
+  const stop = () => {
+    if (interv) {
+      clearInterval(interv);
+      setInterv(undefined);
+    }
+    setStatus(2);
+  };
+
+  const reset = () => {
+    if (interv) {
+      clearInterval(interv);
+      setInterv(undefined);
+    }
+    setStatus(0);
+    setTime({ ms: 0, s: 0, m: 0, h: 0 });
+  };
+
+  const resume = () => start();
+
+  useEffect(() => {
+    return () => {
+      if (interv) {
+        clearInterval(interv);
+        setInterv(undefined);
+      }
+    };
+  }, [interv]);
+
+  return (
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent
+            status={status}
+            resume={resume}
+            reset={reset}
+            stop={stop}
+            start={start}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StopWatch;
