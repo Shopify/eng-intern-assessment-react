16e0a09662cf79814da7b4da1ca08cc15db69761
Alethea Kramer <aletheakramer@outlook.com>
2024-01-29 16:01:45 -0800

    reset files to remove previous solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import './App.css'
-import StopWatch from './StopWatch'
 
 export default function App() {
     return(
-        <StopWatch />
+        <div></div>
     )
 }
