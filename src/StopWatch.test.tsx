73a7ced2f12f56eb6e9f2291825e81d4dbc36438
JP Grineau <jgrineau95@gmail.com>
2024-01-29 23:19:19 -0500

    Styling updated

--- src/StopWatch.test.tsx
+++ src/StopWatch.test.tsx
@@ -1,24 +1,6 @@
 import StopWatch from "./StopWatch";
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
-
-// describe("Start timer function", () => {
-//   test("timer starts when start is called", () => {
-//     const stopWatchDom = render(<StopWatch />);
-//     const setIntervalSpy = jest.spyOn(global, "setInterval");
-//     const startButton = stopWatchDom.container.querySelector("#startButton");
-//   });
-// });
-
-// test('starts timer when start button is clicked', () => {
-//   const setIntervalSpy = jest.spyOn(global, 'setInterval');
-//   render(<StopWatch />);
 
 describe("StopWatch component", () => {
   beforeEach(() => {
