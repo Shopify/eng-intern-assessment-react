b3753279c2850bbb60294e0b9e4aaf8b201c5b81
Sangita Dutta <sangita7dutta@gmail.com>
2024-01-29 18:03:07 -0800

    added timer logic

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,49 +1,50 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import StopWatchButton from './StopWatchButton'
+import React, { useState, useEffect, useCallback } from "react";
+import StopWatchButton from "./StopWatchButton";
 
-export function timeCalulator(timeInSeconds: number): Array<number|string> {
-    let hours: number = Math.floor(timeInSeconds / 3600);
-    let minutes: number = Math.floor((timeInSeconds - (hours * 3600)) / 60);
-    let seconds: number = timeInSeconds - (hours * 3600) - (minutes * 60);
-    let milliseconds: number = Math.round((timeInSeconds - Math.floor(timeInSeconds)) * 1000);
+export function timeCalulator(time: number): Array<number | string> {
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const milliseconds = time % 100;
 
-    let hoursFormatted = hours < 10?  `0${hours}` : hours;
-    let minutesFormatted = minutes < 10?  `0${minutes}` : minutes;
-    let secondsFormatted = seconds < 10?  `0${seconds}` : seconds;
-    let millisecondsFormatted = milliseconds < 10? `0${milliseconds}` : milliseconds;
-
-
-    return [hoursFormatted, minutesFormatted, secondsFormatted, millisecondsFormatted]
-
+  let hoursFormatted = hours < 10 ? `0${hours}` : hours;
+  let minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+  let secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
+  let millisecondsFormatted =
+    milliseconds < 10
+      ? `0${milliseconds}`
+      : `${milliseconds}`
+  
+  return [
+    hoursFormatted,
+    minutesFormatted,
+    secondsFormatted,
+    millisecondsFormatted,
+  ];
 }
 
-
-
 export default function StopWatch() {
-    const[timeInSeconds, setTimeInSections]=useState(0);
-    const[timerArray, setTimerArray]=useState<Array<number|string>>([]);
+  const [time, setTime] = useState(0);
+  const [timerArray, setTimerArray] = useState<Array<number | string>>([]);
 
-    useEffect(() => {
-        let timeArray: Array<number|string> =timeCalulator(timeInSeconds);
-        setTimerArray(timeArray);
-    },[timeInSeconds]);
+  useEffect(() => {
+    setTimerArray(timeCalulator(time));
+  }, [time]);
 
-    return(
-       <div>
-       <div className='stopwatch-container'>
-        <p className='timer-text'>{timerArray[0]}</p>
+  return (
+    <div>
+      <div className="stopwatch-container">
+        <p className="timer-text">{timerArray[0]}</p>
         <span>:</span>
-        <p className='timer-text'>{timerArray[1]}</p>
+        <p className="timer-text">{timerArray[1]}</p>
         <span>:</span>
-        <p className='timer-text'>{timerArray[2]}</p>
+        <p className="timer-text">{timerArray[2]}</p>
         <span>:</span>
-        <p className='timer-text'>{timerArray[3]}</p> 
-       </div>
-       <div>
-        <StopWatchButton></StopWatchButton>
-       </div>
-       </div>
-       
-       
-    )
+        <p className="timer-text">{timerArray[3]}</p>
+      </div>
+      <div>
+        <StopWatchButton setTime={setTime}></StopWatchButton>
+      </div>
+    </div>
+  );
 }
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 // Maximum number of laps that can be recorded
 const maxLaps = 25;
 
 
+type Props = { 
+    setTime: Function 
+}
+  export default function StopWatchButton(props:Props) {
+
+    const {setTime} = props;
+    const [intervalId, setIntervalId]=useState<number>(0);
+
+    const handleStartButton = (e: object) => {
+        let interval:any = setInterval(() => {
+            setTime((prev:number) => prev + 1);
+        }, 10);
+
+        setIntervalId(interval);
+    }
 
   
-  export default function StopWatchButton() {
+
+    const handleStopButton = () => {
+        clearInterval(intervalId);
+
+    }
+
+    const handleLapsButton = () => {
+
+
+    }
+
+    const handleResetButton = () => {
+        clearInterval(intervalId);
+        setTime(0);
+        }
+
+
+
     return (
-        <div>
-            <button>Start</button>
-            <button>Stop</button>
+        <div className='stopwatchbutton-container'>
+            <button onClick={handleStartButton}>Start</button>
+            <button onClick={handleStopButton}>Stop</button>
             <button>Laps</button>
-            <button>Reset</button>
+            <button onClick={handleResetButton}>Reset</button>
         </div>
     )
   }
