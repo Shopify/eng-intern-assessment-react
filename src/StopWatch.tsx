6cd6e55ed5148f145c4ed8d091127d46176a7df2
AnnyZ08 <97325065+AnnyZ08@users.noreply.github.com>
2024-01-29 19:30:10 -0500

    add lap function

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -4,35 +4,20 @@ import { time } from 'console';
 import StopWatchButton from './StopWatchButton';
 import video from './cloud.mp4'
 
-// function calculateTimer(timeInSec:number): Array<number> {
-//     let hours:number = Math.floor(timeInSec/3600);
-//     let minutes:number = Math.floor((timeInSec%3600)/60);
-//     let seconds:number = Math.floor((timeInSec%60));
-
-//     //show two digit
-//     let hoursFormat = hours < 10 ? '0${hours}':hours;
-
-
-//     return [
-//         hoursFormat,
-//         minutes,
-//         seconds,
-//     ];
-// }
-
 
 export default function StopWatch() {
     const[timeInSec, setTimeInSec] = useState<number>(0);
-    const [timerArr, setTimerArr] = useState<Array<number>>([]);
 
     //calculating the hour, minutes, seconds from seconds
     const hours:number = Math.floor(timeInSec/3600);
     const minutes:number = Math.floor((timeInSec%3600)/60);
     const seconds:number = Math.floor((timeInSec%60));
+    const [laps, setLaps] = useState<Array<number>>([]);
+
     useEffect(() => {
-        //increase time every 10 milisecond
-        //let intervalId = setInterval(() => setTimeInSec(timeInSec+1),10);
+        
     },[timeInSec]);
+
 
     return(
         <div className='container'>
@@ -49,7 +34,11 @@ export default function StopWatch() {
                 <span>:</span>
                 <p className = "timer-text">{seconds.toString().padStart(2,"0")}</p>
             </div>
-            <StopWatchButton setTimeInSec={setTimeInSec} />
+            
+            <StopWatchButton setTimeInSec={setTimeInSec} timeInSec={timeInSec}/>
+            
+            
+
             
         </div>
         
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,35 +1,77 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './StopWatchButton.css'
 type Props = {
     setTimeInSec: Function
+    timeInSec: number;
 };
 
-export default function StopWatchButton(props:Props) {
-    const {setTimeInSec} = props;
-    const [intervalId, setIntervalId] = useState<number>(0);
-    
-    const handlePlayButton = () => {
-        let interval:any = setInterval(() => {
-            setTimeInSec((previousState:number) => (previousState + 1))
-        },1000);
+export default function StopWatchButton(props: Props) {
+   const {setTimeInSec,timeInSec} = props;
+   // const [intervalId, setIntervalId] = useState<number>(0);
+    const [laps, setLaps] = useState([]);
+    const [isRunning, setIsRunning] = useState(false);
 
-        setIntervalId(interval);
+    useEffect(()=> {
+        let interval:any;
+
+        if(isRunning) {
+            interval = setInterval(()=> {
+                setTimeInSec((previousState:number) => (previousState + 1))
+            },1000)
+        }
+        return () => clearInterval(interval);
+    },[isRunning])
+
+    const handleStartButton = () => {
+        setIsRunning(true);
     }
 
     const handleStopButton = () => {
-       clearInterval(intervalId);
+       setIsRunning(false);
     }
 
     const handleResetButton = () => {
-        clearInterval(intervalId);
+        setIsRunning(false);
         setTimeInSec(0);
+        setLaps([]);
     }
-    
+
+    const handleLapButton = () => {
+        const lapTime = timeInSec;
+        setLaps((prevLaps) => [...prevLaps, lapTime]);
+      };
+
+    const timeFormat = (sec:number) => {
+        const hours:number = Math.floor(sec/3600);
+        const minutes:number = Math.floor((sec%3600)/60);
+        const seconds:number = Math.floor((sec%60));
+        
+        return {hours,minutes,seconds};
+    };
+   
     return(
-        <section className='buttons-container'>
-            <button onClick={handlePlayButton}>Play</button>
+        <div className='buttons-container'>
+            <button onClick={handleStartButton}>Start</button>
             <button onClick={handleStopButton}>Stop</button>
             <button onClick={handleResetButton}>Reset</button>
-        </section>
+            <button onClick={handleLapButton}>Lap</button>
+            
+            <div>
+                <ul>
+                {laps.map((lap, index) => (
+                    <div>
+                       <p key={index}> Lap {index + 1}:
+                       {timeFormat(lap).hours.toString().padStart(2,"0")}:
+                       {timeFormat(lap).minutes.toString().padStart(2,"0")}:
+                       {timeFormat(lap).seconds.toString().padStart(2,"0")}
+                       </p>
+                    </div>
+                    
+                ))}
+                </ul>
+        
+            </div>
+        </div>
+        
     )
 }
