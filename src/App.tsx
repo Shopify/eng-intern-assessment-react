cae085b8735019fd0cf8f19dfa0e7752f9a12738
Jamiu Abdsalami <jamiuabdsalami@outlook.com>
2024-01-29 23:36:51 -0500

    divides app to components and adds scss design files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import './App.scss'
+import StopWatch from './components/Functionalities/StopWatch'
 
 export default function App() {
     return(
-        <div></div>
+        <StopWatch />
     )
 }
---
+++ src/components/Button/StopWatchButton.tsx
@@ -0,0 +1,10 @@
+import './Button.scss';
+import React from 'react'
+
+function StopWatchButton() {
+    return(
+        <button type="submit" className="button">
+            <div></div>
+        </button>
+    )
+}
