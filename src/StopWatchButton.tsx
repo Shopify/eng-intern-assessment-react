712819c573f05c471ce5385c17e0b468f060a53a
Mehedi Hossain <mhoss107@uottawa.ca>
2024-01-30 10:15:04 -0500

    Added comments to stopWatchButton component

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+// Props interface for the StopWatchButton component
 interface StopWatchButtonProps {
     type: string;
     onClick: () => void;
     disabled?: boolean;
 }
 
+//StopWatchButton component
 export default function StopWatchButton({ type, onClick, disabled = false }: StopWatchButtonProps) {
+  // Function to determine the text content of the button based on its type
     const getButtonText = (): string => {
         switch (type) {
           case 'start':
