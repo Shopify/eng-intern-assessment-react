a3550fb61760c3e860ee944f770adc9c1b9f4823
megh-mm <megh.gandhi@dal.ca>
2024-02-03 00:24:59 -0400

    feat(app) add theme selector feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import StopWatch from "./StopWatch";
+import ThemeSelector from "./ThemeSelector";
+import "./styles/app.css"
+
 export default function App() {
-    return(
-        <StopWatch />
-    )
+
+  const [theme, setTheme] = useState<string>('');
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--main-accent-color', theme);
+  }, [theme]);
+
+  return(
+    <>
+      <StopWatch />
+      <ThemeSelector theme={theme} themeSelect={setTheme} />
+    </>
+  )
 }
